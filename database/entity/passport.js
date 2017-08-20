746027fa51ea289fab7b06b40cb713f83d0b9249
yunxu1019 <yunxu1019@qq.com>
2017-08-20 12:14:55 +0800

    数据库初步

---
+++ database/entity/passport.js
@@ -0,0 +1,23 @@
+/**
+ * 账号密码表
+ * 与可登录表(如User)呈多对一关系
+ */
+Passport.prototype = {
+    /**
+     * 登录名，手机号，邮箱等
+     */
+    passport_id: "253",
+    /**
+     * 关联的可登录表
+     */
+    login_as: "64",
+    /**
+     * 关联的可登录表的id
+     */
+    login_id: 15,
+    passwordc:"62",
+    passwordd:"62",
+};
+
+function Passport() {}
+module.exports = Passport;
---
+++ database/entity/userinfo.js
@@ -0,0 +1,12 @@
+/**
+ * 用户的详细信息
+ */
+Userinfo.prototype = {
+    username: "64",
+    id: 12,
+    password: "63",
+    account: "63",
+};
+
+function Userinfo() {}
+module.exports = Userinfo;
