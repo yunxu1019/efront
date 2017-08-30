d6f897ba3f158f4b171fc3dbcbbfbe57d2f8fbf0
yunxu1019 <yunxu1019@qq.com>
2017-08-30 22:45:24 +0800

    移动鼠标之后将单击操作取消

--- coms/zimoli/onclick.js
+++ coms/zimoli/onclick.js
@@ -1,1 +1,14 @@
-var onclick=on("click");
+var _onclick = on("click");
+var preventClick = false;
+onmousedown(window, function (event) {
+    preventClick = false;
+});
+onmousemove(window, function (event) {
+    preventClick = true;
+});
+var onclick = function (target, handler) {
+    return _onclick(target, function (event) {
+        if (preventClick) return;
+        return handler.call(this, event);
+    });
+}
