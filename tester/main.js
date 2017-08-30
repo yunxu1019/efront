9711bd78c1bca631b9267f21dc96ef7dc3841e2e
yunxu1019 <yunxu1019@qq.com>
2017-08-30 22:44:01 +0800

    测试环境入口

--- tester/main.js
+++ tester/main.js
@@ -1,6 +1,4 @@
-var tester=function(){
-    
-    return tester;
-};
-
-module.exports=tester;
+process.stdin.resume();
+require("../process/setupenv");
+process.env.IN_TEST_MODE = true;
+return require("../main");
