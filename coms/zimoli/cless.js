a0fdb8b27c6d812723f17775c5fbbb78c5189eb5
yunxu1019 <yunxu1019@qq.com>
2017-09-09 20:46:12 +0800

    兼容ie7,ie8

---
+++ coms/zimoli/cless.js
@@ -0,0 +1,18 @@
+function cless(commFactory, styleSheet, className) {
+    var style = createElement("style");
+    style.type = "text/css";
+    if (style.styleSheet) {
+        style.styleSheet.cssText = styleSheet;
+    } else {
+        style.innerHTML = styleSheet
+    }
+    appendChild(document.getElementsByTagName("head")[0], style);
+    if (commFactory) {
+        try {
+            commFactory.className = className;
+        } catch (e) {
+            console.error(e, "bindClassNameError");
+        }
+    }
+    return style;
+}
