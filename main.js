d7aa68844eb331f901397cfddccab724244d947e
yunxu1019 <yunxu1019@qq.com>
2017-09-11 00:51:04 +0800

    中文编码

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+// 中文编码 utf-8
 require("./process/setupenv");
 require("./process/console");
 var server=require(/*sdf*/"./server/main");
+// var tester=require("./tester/main");
+// tester(server);
 // console.info('你好!');
