4bbbbe40f57b1eebd0e873d35166dbf0f628b99c
yunxu1019 <yunxu1019@qq.com>
2017-09-11 00:51:54 +0800

    组件加载工具

---
+++ apps/docs/main.js
@@ -0,0 +1,32 @@
+"use strict";
+// var document=this.document;
+// document["body"].appendChild(this.document.createElement("input"));
+titlebar("组件加载工具", false, false);
+var page = createElement(div);
+var outerbox = createElement(input);
+outerbox.value = state();
+appendChild(page, outerbox);
+onkeydown(outerbox, function (event) {
+    var {
+        keyCode
+    } = event;
+    if (keyCode === 13) {
+        state(this.value);
+        load();
+    }
+});
+var load=function(){
+    component && remove(component);
+    if(!outerbox.value)return;
+    console.info(`load ${outerbox.value}!`);
+    init(outerbox.value, function (comm) {
+        component = createElement(comm);
+        appendChild(page, component);
+    });
+}
+var component;
+
+function main() {
+    load();
+    return page;
+}
