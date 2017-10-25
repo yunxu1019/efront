99926b7d2484abe8b594d4bfbe8fd2feb6f9cc2c
unknown <>
2017-10-26 01:21:36 +0800

    速度计算算法

---
+++ coms/zimoli/speed.js
@@ -0,0 +1,13 @@
+function speed(time_splitter = 1) {
+    var lastmoveTime = new Date, speed = 0;
+    return function (deltay) {
+        if (arguments.length) {
+            var now = new Date;
+            var deltat = now - lastmoveTime;
+            lastmoveTime = now;
+            if (deltay) speed = speed ? (speed + deltay * time_splitter / deltat) / 2 : deltay * time_splitter / deltat;
+            else if (deltay === 0) speed = 0;
+        }
+        return speed;
+    };
+}
