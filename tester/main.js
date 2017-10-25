c9be48da7e84429775b2a24dad605f024fd4b8ef
unknown <>
2017-10-26 00:17:53 +0800

    测试环境显示本地地址

--- tester/main.js
+++ tester/main.js
@@ -1,5 +1,7 @@
 process.stdin.resume();
 require("../process/setupenv");
+process.title = `服务器地址：${require("../process/getLocalIP")()}`;
+
 process.env.IN_TEST_MODE = true;
 require("../main");
 if (process.argv.indexOf("--electron") >= 0) require("./deploy/electron");
