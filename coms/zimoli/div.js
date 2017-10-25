a2c26afe1f2807182253d8fab848330507db507a
unknown <>
2017-10-26 01:33:18 +0800

    去除点击时的阴影

--- coms/zimoli/div.js
+++ coms/zimoli/div.js
@@ -1,5 +1,5 @@
 var block = createElement("div");
-css(block, "box-sizing:border-box;")
+css(block, "box-sizing:border-box;-webkit-tap-highlight-color:rgba(255,255,255,0);")
 
 function div() {
     return createElement(block);
