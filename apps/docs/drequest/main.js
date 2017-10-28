f4df717f05aa21117d3294d65c654c9f30fd1dd7
unknown <>
2017-10-29 02:16:50 +0800

    请求计时工具

---
+++ apps/docs/drequest/main.js
@@ -0,0 +1,53 @@
+titlebar("请求计时工具", [], false);
+var _input = createElement(input);
+css(_input, "width:100%;height:50px;margin:10px 0;");
+var request_time_count = createElement(_input);
+request_time_count.setAttribute("placeholder", "输入请求次数");
+var request_time_url = createElement(_input);
+request_time_url.setAttribute("placeholder", "输入url");
+var request_time_label = createElement(div);
+var request_time_button = button("请求");
+var request_stop_button = button("停止");
+css(request_time_button, "background-color:#f34;width:100%;height:50px;margin:10px 0;color:#fff;")
+css(request_stop_button, "background-color:#999;color:#fff;height:50px;width:100%;");
+var saveState = function () {
+    state({ count: request_time_count.value, url: request_time_url.value });
+};
+var lastState = state();
+if (lastState) {
+    if (lastState.count) request_time_count.value = lastState.count;
+    if (lastState.url) request_time_url.value = lastState.url;
+}
+oninput(request_time_count, saveState);
+oninput(request_time_url, saveState);
+var stoped = false;
+var request = function () {
+    var url = request_time_url.value.trim();
+    var count = +request_time_count.value;
+    var _count = 0, startTime = new Date;
+    stoped = false;
+    var request = function () {
+        if (count <= _count || stoped) return;
+        new Promise(function (ok, oh) {
+            var xhr = new XMLHttpRequest;
+            xhr.open("get", url);
+            xhr.onload = e => ok();
+            xhr.onerror = e => oh();
+            xhr.onabort = e => oh();
+            xhr.send();
+            _count++;
+            request_time_label.innerText = `count:${_count},time:${new Date - startTime}`;
+        }).then(request).catch(request);
+    };
+    return request();
+}
+onclick(request_time_button, request)
+onclick(request_stop_button, function () {
+    stoped = true;
+});
+var page = createElement(div);
+css(page, "padding-top:50px;");
+appendChild(page, request_time_count, request_time_label, request_time_url, request_time_button, request_stop_button);
+function main() {
+    return page;
+}
