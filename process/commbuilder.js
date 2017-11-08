32f4d020abf0666249ae373b12de1516b268dc9f
不枝雀 <yunxu1019@qq.com>
2017-11-09 06:51:16 +0800

    修改编码替换的范围

--- process/commbuilder.js
+++ process/commbuilder.js
@@ -150,6 +150,6 @@ module.exports = function commbuilder(buffer, filename, fullpath, watchurls) {
         length = "0" + length;
     }
 
-    data = (_arguments.length ? length + _arguments : "") + data.replace(/[\u1000-\uffff]/g, m => "\\u" + m.charCodeAt(0).toString(16));
+    data = (_arguments.length ? length + _arguments : "") + data.replace(/[\u0100-\uffff]/g, m => "\\u" + m.charCodeAt(0).toString(16));
     return data;
 }
