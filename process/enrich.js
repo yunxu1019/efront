03864ab267323c6fb5ad90f25f58b37cf459bb48
不枝雀 <yunxu1019@qq.com>
2017-11-09 22:56:12 +0800

    提升enrich性能

--- process/enrich.js
+++ process/enrich.js
@@ -1,35 +1,36 @@
-function enrich(_obj) {
-    var _enrich = function _enrich(obj) {
-        var promise = obj;
-        var that = _obj;
-        if (promise._enrich) {
-            return promise;
-        }
-        for (var cx = 0, dx = ["catch", "then"], k; k = dx[cx++];)
-            promise[k] = function () {
-                var _orig = promise[k];
-                return function () {
-                    var subobj = _orig.apply(this, arguments);
-                    return this._enrich(subobj);
-                };
-            }();
-        for (var k in that) {
-            promise[k] = function () {
-                var method = that[k];
-                return function () {
-                    var args = arguments;
-                    return this.then(function (res) {
-                        return method.apply(this, args);
-                    });
-                };
-            }();
-        }
-        promise._enrich = function (subobj) {
-            return that._enrich(subobj);
-        };
-        return obj;
-    };
-    _obj._enrich = _enrich;
-    return _obj._enrich(Promise.resolve());
+var _preich = function _preich(obj) {
+    function EnrichedPromise(){}
+    var _obj = {};
+    for (var k in obj) {
+        _obj[k] = function () {
+            var method = obj[k];
+            return function () {
+                var args = arguments;
+                return this.then(function (res) {
+                    return method.apply(this, args);
+                });
+            };
+        }();
+    }
+    for (var cx = 0, dx = ["catch", "then"], k; k = dx[cx++];)
+        _obj[k] = function () {
+            var _orig = Promise.prototype[k];
+            return function () {
+                var subobj = _orig.apply(this, arguments);
+                return _enrich(subobj, _obj);
+            };
+        }();
+    return _obj;
+};
+var _enrich = function _enrich(promise, _obj) {
+    if (!promise._enrich) {
+        promise._enrich = _obj;
+        for (var k in _obj) promise[k] = _obj[k];
+    }
+    return promise;
+};
+function enrich(obj) {
+    var _obj = _preich(obj);
+    return _enrich(Promise.resolve(), _obj);
 }
 module.exports = enrich;
--- process/enrich_test.js
+++ process/enrich_test.js
@@ -1,5 +1,5 @@
 describe("enrich_test", function () {
-    var enrich=require("./enrich");
+    var enrich = require("./enrich");
     it("enrich_onstep", function () {
         var enriched = enrich({
             a() {
@@ -9,17 +9,50 @@ describe("enrich_test", function () {
                 return this.a + 1;
             },
         });
-        enriched.a().b().then(a=>require("chai").expect(a).to.be.equal(2))
-        ;
+        return enriched.a().b().then(a => require("chai").expect(a).to.be.equal(2))
+            ;
     });
-    it("catch_throw",function(){
-        var enriched=enrich({
-            throw(){
+    it("catch_throw", function () {
+        var enriched = enrich({
+            throw() {
                 throw a;
             },
         });
-        return enriched.throw().catch(function(a){
+        return enriched.throw().catch(function (a) {
             console.log(a);
-        }).catch(b=>b);
-    })
+        }).catch(b => b);
+    });
+    it("堆栈", function () {
+        this.timeout(200000);
+        var count = 0;
+        var stack = function () {
+            count++;
+            stack();
+        };
+        try { stack(); } catch (e) { }
+        console.log(count);
+        var enriched = enrich({
+            stack() { }
+        });
+        for (var cx = 0, dx = count * 20; cx < dx; cx++) {
+            enriched = enriched.stack();
+        }
+    });
+    it("GC", function () {
+        this.timeout(200000);
+        var count = 0;
+        var stack = function () {
+            count++;
+            stack();
+        };
+        try { stack(); } catch (e) { }
+        var enriched = enrich({
+            stack() { }
+        });
+        for (var cx = 0, dx = count * 2000; cx < dx; cx++) {
+            !(cx % 103390) && console.error(cx);
+            (cx>2079700)&&console.error(cx);
+            enriched = enriched.stack();
+        }
+    });
 });
