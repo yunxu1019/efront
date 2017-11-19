af807b8ec2bbb30bc4c1990920c86b5cbf491836
不枝雀 <yunxu1019@qq.com>
2017-11-19 19:21:21 +0800

    登录按钮样式修改

--- apps/zimoli/login.js
+++ apps/zimoli/login.js
@@ -8,16 +8,16 @@ _password.type = "password";
 _password.placeholder = "密码";
 var _username = createElement(_input);
 _username.placeholder = "用户";
-var _loginBtn = button("登录");
+var _loginBtn = button("登录", "dark");
 var _logoArea = icon("QQ");
 css(_logoArea, "width:80px;height:80px;margin:60px auto 40px auto;")
-css(_loginBtn, "width:200px;height:50px;background-color:rgba(255,255,255,.3);margin:20px auto;");
+css(_loginBtn, "width:200px;height:50px;margin:20px auto;");
 var _pageCount = createElement(div);
-css(_pageCount,"white-space:nowrap;right:0;bottom:0;color:#888;right:0;position:absolute;height:50px;line-height:50px;padding:0 16px;");
+css(_pageCount, "white-space:nowrap;right:0;bottom:0;color:#888;right:0;position:absolute;height:50px;line-height:50px;padding:0 16px;");
 appendChild(_login, _logoArea, _username, _password, _loginBtn, _pageCount);
 function login(dst_url, src_url) {
     api("/server/count", { path: "/" }).success(function (response) {
-        text(_pageCount, "累计访问量："+response.result);
+        text(_pageCount, "累计访问量：" + response.result);
     });
     return _login;
 }
