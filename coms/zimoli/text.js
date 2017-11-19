2b7f7d9033f93a1d1e0818de9e27228b301a1411
不枝雀 <yunxu1019@qq.com>
2017-11-19 19:20:20 +0800

    text支持读取文本

--- coms/zimoli/text.js
+++ coms/zimoli/text.js
@@ -1,11 +1,14 @@
 function text(node, text) {
-    if (isString(node)) {
-        var _node = div();
-        appendChild(_node, document.createTextNode(node));
-        css(_node, text);
-        return _node;
+    if (arguments.length === 2) {
+        if (isString(node)) {
+            var _node = div();
+            appendChild(_node, document.createTextNode(node));
+            css(_node, text);
+            return _node;
+        }
+        node.innerHTML = "";
+        isFunction(text) && (text = text());
+        isFunction(node.text) ? node.text(text) : node.appendChild(document.createTextNode(text));
     }
-    node.innerHTML = "";
-    isFunction(text) && (text = text());
-    isFunction(node.text) ? node.text(text) : node.appendChild(document.createTextNode(text));
+    return node.innerHTML;
 }
