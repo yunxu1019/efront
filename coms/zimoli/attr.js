146022521a49cd429529b62d3a3432b03285a669
不枝雀 <yunxu1019@qq.com>
2017-11-22 22:39:34 +0800

    只要传入attr的第三个参数，就修改元素的属性

--- coms/zimoli/attr.js
+++ coms/zimoli/attr.js
@@ -1,8 +1,7 @@
 function attr(target, key, value) {
-    if (value) {
-        target.setAttribute(key, value);
-    } else if (value === null) {
-        target.removeAttribute(key, value);
+    if (arguments.length === 3) {
+        if (value === null) target.removeAttribute(key, value);
+        else target.setAttribute(key, value);
     } else if (key instanceof Object) {
         for (var k in key) {
             target.setAttribute(k, key[k]);
