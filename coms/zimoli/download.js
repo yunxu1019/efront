a68729261a517c2cce55befeab2a1d942f43100c
不枝雀 <yunxu1019@qq.com>
2017-11-30 21:17:46 +0800

    chrome下载文件

---
+++ coms/zimoli/download.js
@@ -0,0 +1,9 @@
+function download(dataurl, filename, mime = "text/plain") {
+    var a = createElement("a");
+    a.href = "data:" + mime + ";base64," + btoa(dataurl);
+    a.target = '_blank';
+    a.download = filename;
+    appendChild(document.body, a);
+    a.click();
+    remove(a);
+}
