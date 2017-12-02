34856b3f50e9e0d2c8a9dcf0e4c86c490f26140c
不枝雀 <yunxu1019@qq.com>
2017-12-02 14:31:58 +0800

    测试内存回收

---
+++ tools/empty.js
@@ -0,0 +1,11 @@
+global.promise = new Promise(function (ok, oh) {
+    require("fs").readFile(__filename, function (error, buffer) {
+        if (error) return oh(error);
+        else ok(buffer);
+    });
+}).then(function (result) {
+    console.log(result);
+    require("../process/commbuilder")(result, "", __filename, []);
+}).catch(function (result) {
+    console.log(result);
+});
