34e21936d6c5c06a8daf2cb341fb5389222cec0a
不枝雀 <yunxu1019@qq.com>
2018-01-13 18:45:39 +0800

    box-sizing兼容安卓2.3

--- coms/zimoli/div.js
+++ coms/zimoli/div.js
@@ -1,5 +1,5 @@
 var block = createElement("div");
-css("div", "box-sizing:border-box;-webkit-tap-highlight-color:rgba(255,255,255,0);")
+css("div", "box-sizing:border-box;-webkit-tap-highlight-color:rgba(255,255,255,0);-moz-box-sizing: border-box;-webkit-box-sizing: border-box;-o-box-sizing: border-box;-ms-box-sizing: border-box;")
 
 function div() {
     return createElement(block);
