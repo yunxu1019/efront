d3e9691180648677583381f01c470906026ebef9
不枝雀 <yunxu1019@qq.com>
2018-01-14 23:01:23 +0800

    实现播放暂停

--- apps/kugou/song/list.js
+++ apps/kugou/song/list.js
@@ -27,7 +27,7 @@ cross("get", "http://m.kugou.com/").done(function (xhr) {
         appendChild(block, _singer, _song);
         block.hash = id.replace(/^songs_(.*?)$/g, "$1");
         onclick(block, function () {
-            play(this.hash);
+            player.play(this.hash);
         });
         return block;
     });
@@ -46,26 +46,76 @@ var getSingerAvatar = function () {
     `http://tools.mobile.kugou.com/api/v1/singer_header/get_by_hash?hash=77AFF2715498A86AA28AC2DAA29C3FEB&size=200&format=jsonp&callback=kgJSONP857041635`;
 };
 var oncanplay = on("canplay"), ondataloaded = on("loadeddata");
-var play = function (hash) {
-    /**
-     * ios 只能由用户创建audio，所以请在用户触发的事件中调用play方法
-     */
-    var audio = document.createElement("audio");
 
-    getMusicInfo(hash).done(function (xhr) {
-        var data = JSON.parse(xhr.responseText);
-        ondataloaded(audio, function () {
+var createControls = function () {
+    var box = createWithClass(div, "player-box");
+    var play = createWithClass(div, "play");
+    var next = createWithClass(div, "next");
+    var prev = createWithClass(div, "prev");
+    var song = createWithClass(div, "song");
+    var singer = createWithClass(div, "singer");
+    var info = createWithClass(div, "info");
+    appendChild(info, song, singer);
+    var avatar = createWithClass(div, "avatar");
+    appendChild(box, avatar, info, play, next);
+    onclick(play, function () {
+        if (box.playing) {
+            removeClass(play, "pause");
+            addClass(play, "play");
+            box.pause();
+        } else {
+            removeClass(play, "play");
+            addClass(play, "pause");
+            box.play();
+        }
+    })
+    box.apply = function (data) {
+        text(singer, data.choricSinger);
+        text(song, data.songName)
+        css(avatar, {
+            backgroundImage: `url('${data.imgUrl.replace(/\{size\}/g, 200)}')`
         });
-        oncanplay(audio, function () {
-            audio.play();
-        })
-        audio.src = data.url;
-        audio.load();
-    });
-    // audio.autoplay = true;
-    audio.play();
-    // appendChild(document.body, audio);
+    };
+    box.reset = function () {
+        removeClass(play, "play");
+        addClass(play, "pause");
+        if (!box.parentNode) {
+            appendChild(page, box);
+        }
+    };
+    return box;
 };
+var player = function (box = div()) {
+
+    box.playing = false;
+    box.play = function (hash = this.hash) {
+        if (hash === this.hash) {
+            if (this.playing) return;
+            this.audio.play();
+            this.playing = true;
+            return;
+        }
+        box.pause();
+        box.reset();
+        /**
+         * ios 只能由用户创建audio，所以请在用户触发的事件中调用play方法
+         */
+        box.playing = true;
+        var audio = document.createElement("audio");
+        getMusicInfo(hash).done(function (xhr) {
+            var data = JSON.parse(xhr.responseText);
+            box.apply(data);
+            audio.src = data.url;
+        });
+        audio.play();//安卓4以上的播放功能要在用户事件中调用;
+        this.audio = audio;
+    };
+    box.pause = function () {
+        this.audio && this.audio.pause();
+        this.playing = false;
+    };
+    return box;
+}(createControls());
 function main() {
     return page;
 }
