f26728f210ede82074784c82d46cbe0284eb437f
不枝雀 <yunxu1019@qq.com>
2018-01-16 20:06:11 +0800

    歌手页

--- apps/kugou/singer/keywords.js
+++ apps/kugou/singer/keywords.js
@@ -1,5 +1,36 @@
-var page = createElement(div);
-text(page, "歌手");
+var page = createVboxWithState(state);
+var createItem = function (img_src, str) {
+    var item = createWithClass(div, "item");
+    var image = createWithClass(div, "img");
+    var label = createWithClass(div, "text");
+    css(image, {
+        backgroundImage: `url('${img_src}')`
+    });
+    text(label, str);
+    appendChild(item, image, label);
+    return item;
+};
+cross("get", "http://m.kugou.com/singer/class").done(function (xhr) {
+    var bodyHTML = String(xhr.responseText || xhr.responseText || "").replace(RegBodyExp, "$1").replace(RegScriptExp, "").replace(/\son/ig, " no").replace(/\s(src|href)/g, " s$1");
+    console.log(bodyHTML);
+    var sandbox = createElement(div);
+    sandbox.innerHTML = bodyHTML;
+    var groups = [].map.call(sandbox.getElementsByClassName("bd")[0].children, function (child) {
+        var anchors = child.getElementsByTagName("a");
+        var items = [].map.call(anchors, function (anchor) {
+            var href = anchor.getAttribute("shref");
+            var innerText = anchor.innerText.replace(/^\s*|\s*$/g, "");
+            var item = createElement(div);
+            text(item, innerText);
+            return item;
+        });
+        var group = createWithClass(div,"group");
+        appendChild(group,items);
+        return group;
+    });
+    appendChild(page, groups);
+});
+text(page);
 function main() {
     return page;
 }
