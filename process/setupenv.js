ce3947fea8207192e6bd69ca9c02a7c49b1fcc3b
不枝雀 <yunxu1019@qq.com>
2018-01-17 22:29:09 +0800

    私有环境无法加载的bug修复

--- process/setupenv.js
+++ process/setupenv.js
@@ -97,7 +97,7 @@ var get = function (text) {
     var match = text.match(reg_set);
     if (match) {
         var k = match[1],
-            v = process.env[k] || match[3];
+            v = env[k] || match[3] || process.env[k];
         if (v)
             return env[k.toUpperCase()] = /^path\.|path$/i.test(k) ? path.normalize(v.replace(/[\\]+/g, "/")) : v;
         else
