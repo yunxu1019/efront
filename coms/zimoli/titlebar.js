ca14e616e76ed9c049b0aafcb264bce0daacb8d5
不枝雀 <yunxu1019@qq.com>
2018-01-17 21:15:44 +0800

    格式化

--- coms/zimoli/titlebar.js
+++ coms/zimoli/titlebar.js
@@ -6,12 +6,12 @@ function btn(element) {
     css(opt, "position:absolute;background-color:#333336;top:0;bottom:0;height:100%;width:60px;");
     return opt;
 }
-function back(){
-    var _icon=icon("back",0xf0f0f0);
-    css(_icon,"width:30px;height:30px;top:50%;margin-top:-15px;left:10px;position:absolute;");
-    var _btn=btn(_icon);
-    css(_btn,"left:0;")
-    onclick(_btn,function(){
+function back() {
+    var _icon = icon("back", 0xf0f0f0);
+    css(_icon, "width:30px;height:30px;top:50%;margin-top:-15px;left:10px;position:absolute;");
+    var _btn = btn(_icon);
+    css(_btn, "left:0;")
+    onclick(_btn, function () {
         history.back();
     })
     return _btn;
@@ -19,14 +19,14 @@ function back(){
 
 
 function titlebar(page_title, option_buttons, use_back) {
-    use_back=use_back!==false;
+    use_back = use_back !== false;
     var bar = createElement(nav);
     var title = createElement(label);
-    var _back=createElement(back);
+    var _back = createElement(back);
     text(title, page_title || document.title);
     appendChild(bar, title);
-    use_back&&appendChild(bar,_back);
-    option_buttons&&appendChild(bar,option_buttons);
+    use_back && appendChild(bar, _back);
+    option_buttons && appendChild(bar, option_buttons);
     return bar;
 }
 titlebar.button = btn;
