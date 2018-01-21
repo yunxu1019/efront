0fac603403502eb37310db067ed1d526241c6516
不枝雀 <yunxu1019@qq.com>
2018-01-21 20:28:26 +0800

    支持事件名

--- coms/zimoli/dispatch.js
+++ coms/zimoli/dispatch.js
@@ -8,3 +8,17 @@ var dispatch = "dispatchEvent" in document ? function dispatchEvent(target, even
         return target[fire] && target[fire](event);
     }
 };
+function main() {
+    var target, event;
+    for (var cx = 0, dx = arguments.length; cx < dx; cx++) {
+        var arg = arguments[cx];
+        if (isString(arg)) {
+            event = createEvent(arg);
+        } else if (isNode(arg)) {
+            target = arg;
+        } else {
+            event = arg;
+        }
+    }
+    dispatch(target || window, event);
+}
