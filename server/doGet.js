91f686cc08d264626f75864628cf29e469125c69
不枝雀 <yunxu1019@qq.com>
2018-02-19 17:19:51 +0800

    访问文件夹的抛错问题

--- server/doGet.js
+++ server/doGet.js
@@ -5,7 +5,13 @@ var mimes = require("../process/mime");
 var message = require("../process/message");
 var referer_proxy = require("./proxy");
 var URL = require("url");
-
+/**
+ * 返回数据
+ * @param {Buffer} data 
+ * @param {string} url 
+ * @param {httpRequest} req 
+ * @param {httpResponse} res 
+ */
 var response = function (data, url, req, res) {
     message.count({ path: url, update: true });
     var requiredVersion = req.headers["if-modified-since"];
@@ -14,25 +20,63 @@ var response = function (data, url, req, res) {
     }
     else {
         var extend = url.match(/\.(.*?)$/);
+        var headers = {
+            "Content-Length": data.length
+        };
         if (extend) {
             var mime = mimes[extend[1]];
             if (mime) {
-                var headers = {
-                    'Content-Type': mime,
-                    "Content-Length": data.length
-                };
-                if (data.stat) {
-                    headers["Last-Modified"] = data.stat.mtime.toUTCString()
-                }
-                res.writeHead(200, headers);
+                headers['Content-Type'] = mime;
             }
         }
+        if (data.stat) {
+            headers["Last-Modified"] = data.stat.mtime.toUTCString()
+        }
+        res.writeHead(200, headers);
         res.write(data);
     }
     return res.end();
+};
+/**
+ * 根据数据类型进行不同的响应处理
+ * @param {Buffer|Object|string} data 
+ * @param {string} url 
+ * @param {httpRequest} req 
+ * @param {httpResponse} res 
+ */
+var adapter = function (data, url, req, res) {
+    if (data instanceof Buffer) {
+        return response(data, url, req, res);
+    }
+    if (data instanceof Object) {
+        if (!data["index.html"]) {
+            if (url[url.length - 1] !== "/") data = getfile(path.join(process.env.APP, url));
+            else data = "index.html" in data ? getfile(url + "/index.html") : getfile(process.env.APP + url + "index.html");
+        } else {
+            data = data["index.html"];
+        }
+        if (data instanceof Buffer) {
+            return response(data, "index.html", req, res);
+        } else if (data instanceof Promise) {
+            return data.then(function (data) {
+                adapter(data, "index.html", req, res);
+            }).catch(function (error) {
+                res.writeHead(500, {});
+                res.end(String(error));
+            });
+        }
+    }
+    if (typeof data === "string") {
+        res.writeHead(301, {
+            'Location': data[0] === "/" ? data : "/" + data
+        });
+        return res.end();
+    }
+    res.writeHead(403, {})
+    res.end();
 }
 /**
- * 
+ * doGet
  */
 module.exports = function (req, res) {
 
@@ -55,40 +99,14 @@ module.exports = function (req, res) {
             return res.end();
         }
     }
-    if (data instanceof Buffer) {
-        return response(data, url, req, res);
-    } else if (data instanceof Promise) {
+    if (data instanceof Promise) {
         return data.then(function (data) {
-            response(data, url, req, res);
+            adapter(data, url, req, res);
         }).catch(function (error) {
             res.writeHead(500, {});
             res.end(String(error));
         });
-    } else if (data instanceof Object) {
-        if (!data["index.html"]) {
-            if (url[url.length - 1] !== "/") data = getfile(path.join(process.env.APP, url));
-            else data = "index.html" in data ? getfile(url + "/index.html") : getfile(process.env.APP + url + "index.html");
-        } else {
-            data = data["index.html"];
-        }
-        if (data instanceof Buffer) {
-            return response(data, "index.html", req, res);
-        } else if (data instanceof Promise) {
-            return data.then(function (data) {
-                response(data, "index.html", req, res);
-            }).catch(function (error) {
-                res.writeHead(500, {});
-                res.end(String(error));
-            });
-        }
-    } else if (typeof data === "string") {
-        res.writeHead(301, {
-            'Location': data[0] === "/" ? data : "/" + data
-        });
-        return res.end();
     } else {
-
+        adapter(data, url, req, res);
     }
-    res.writeHead(403, {})
-    res.end();
 };
