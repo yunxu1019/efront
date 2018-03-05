d7825a3a14dc0be7f32c10e6623ea17c1ccfcd2e
不枝雀 <yunxu1019@qq.com>
2018-03-05 11:40:00 +0800

    阻止非人为点击，防止误操作

--- coms/zimoli/onclick.js
+++ coms/zimoli/onclick.js
@@ -1,5 +1,5 @@
 var _onclick = on("click");
-var preventClick = false, saved_x, saved_y;
+var preventClick = false, saved_x, saved_y, lasttime_click;
 onmousedown(window, function (event) {
     saved_x = event.clientX, saved_y = event.clientY;
     onclick.preventClick = preventClick = false;
@@ -10,7 +10,15 @@ onmousemove(window, function (event) {
         onclick.preventClick = preventClick = true;
 });
 var onclick = function (target, handler) {
+
     return _onclick(target, function (event) {
+        saved_time = lasttime_click;
+        lasttime_click = +new Date;
+        if (lasttime_click - saved_time < 20) {
+            // 阻止非人为点击，防止误操作
+            return;
+        }
+
         if (preventClick) return;
         return handler.call(this, event);
     });
