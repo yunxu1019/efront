c35998b969037ce9d53533dce342a978bed26bb2
不枝雀 <yunxu1019@qq.com>
2018-03-23 22:28:39 +0800

    服务器调用方式修改

--- tester/main.js
+++ tester/main.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 process.stdin.resume();
 require("../process/setupenv");
-
+require("../process/console");
 process.env.IN_TEST_MODE = true;
-require("../main");
+require("../server/main");
 if (process.argv.indexOf("--electron") >= 0) require("./deploy/electron");
