4a66308a372b43c7f23bf1f732cf12b327185473
不枝雀 <yunxu1019@qq.com>
2018-03-29 22:54:08 +0800

    支持弹出dom元素

--- coms/zimoli/popup.js
+++ coms/zimoli/popup.js
@@ -30,7 +30,30 @@ put("/popup/window", function () {
  * @param {object} style 自定义样式，主要是width,height等位置参数
  * @returns {Element} 给调用弹窗的命令暴露的可选方法
  */
-var popup = function (path = "", parameters, style) {
+var popup = function (path) {
+    if (isString(path)) {
+        return popup_path.apply(null, arguments);
+    }
+    if (isNode(path)) {
+        return popup_extra.apply(null, arguments);
+    }
+};
+var popup_extra = function (element, target) {
+    var position = getScreenPosition(target);
+    var maxHeight = Math.min(position.top, window.innerHeight - position.top - position.height);
+    css(element, `position:absolute;left:${position.left}px;min-width:${position.width}px`);
+    zimoli.global(element, true);
+    var height = element.offsetHeight;
+    if (height > maxHeight) {
+        css(element, `height:${maxHeight}px`);
+    }
+    if (position.top + height + position.height > window.innerHeight) {
+        css(element, `bottom:${position.top}px`);
+    } else {
+        css(element, `top:${position.top + position.height}px`);
+    }
+};
+var popup_path = function (path = "", parameters, style) {
     var viewsPath = "views";
     if (path.indexOf(".") >= 0) {
         path = path.replace(/\./g, '');
@@ -41,7 +64,7 @@ var popup = function (path = "", parameters, style) {
     css(mask, `position:absolute;position:fixed;z-index${zIndex()};left:0;right:0;bottom:0;top:0;width:auto;height:auto;background-color:rgba(0,0,0,0.2)`);
     mask.className = "popup-factory mask";
     var saved_position;
-    setTimeout(go, 0, "/popup/window",{}, mask);
+    // setTimeout(go, 0, "/popup/window",{}, mask);
     onappend(mask, addMask);
     onremove(mask, escMask);
     zimoli.global(mask, true);
