9d399e8539f1ddb9a759366a9725ee32155f9198
不枝雀 <yunxu1019@qq.com>
2018-04-08 23:39:30 +0800

    自动处理跨域请求中的cookie

--- coms/zimoli/cross.js
+++ coms/zimoli/cross.js
@@ -3,6 +3,7 @@ var base = /^https?:\/\//.test(location.href) ? "/?" : "http://efront.cc/?";
 function cross(method, url, headers) {
     var _cookies = cookies_map[url];
     if (_cookies) {
+        if (!headers) headers = {};
         headers.cookie = _cookies;
     }
     var xhr = new XHR;
@@ -13,6 +14,18 @@ function cross(method, url, headers) {
     })));
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
+            if (xhr.getResponseHeader) {
+                var cookie = xhr.getResponseHeader("cross-cookie");
+                if (cookie) {
+                    var _saved_cookie_map = parseKV((cookies_map[url] || "") + cookie
+                        .replace(/(^|;|,)\s*(expires)=(\w*),([^=]*)(;|$)/ig, "$1$2=$3.$4")
+                        .split(/,\s*/).map(function (cookie) {
+                            var result = cookie.split(/;\s*/);
+                            return result[0];
+                        }).join(";"), ";");
+                    cookies_map[url] = serialize(_saved_cookie_map, ";");
+                }
+            }
             var status = xhr.status;
             if (status === 0 || status === 200 || status === 304) {
                 onload instanceof Function && onload(xhr);
