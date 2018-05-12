4c47fb9e23a0ea95ddaa0bee9501db4bd5d30fff
不枝雀 <yunxu1019@qq.com>
2018-05-12 20:15:42 +0800

    按字母分组

--- apis/docs/getAllComponents.js
+++ apis/docs/getAllComponents.js
@@ -1,15 +1,30 @@
-var fs=require("fs");
-var path=require("path");
-var commspath=path.join(__dirname,"../../coms/zimoli");
-var test_file_reg=/_test\.[tj]sx?$/i;
-var comm_file_reg=/\.[tj]sx?$/i;
+var fs = require("fs");
+var path = require("path");
+var commspath = path.join(__dirname, "../../coms/zimoli");
+var test_file_reg = /_test\.[tj]sx?$/i;
+var comm_file_reg = /\.[tj]sx?$/i;
 function getAllcomponents() {
-    return new Promise(function(ok,oh){
-        fs.readdir(commspath,function(error,names){
-            if(error){
+    return new Promise(function (ok, oh) {
+        fs.readdir(commspath, function (error, names) {
+            if (error) {
                 return oh(error);
             }
-            ok(names.filter(a=>!test_file_reg.test(a)&&comm_file_reg.test(a)));
+            var result = [];
+            var current_name_prefix;
+            names.filter(a => !test_file_reg.test(a) && comm_file_reg.test(a)).forEach(function (name) {
+                if (current_name_prefix !== name.charAt(0).toUpperCase()) {
+                    current_name_prefix = name.charAt(0).toUpperCase();
+                    result.push({
+                        name: current_name_prefix,
+                        tab: 1
+                    });
+                }
+                result.push({
+                    name,
+                    tab: 2
+                });
+            });
+            ok(result);
         });
     });
 }
