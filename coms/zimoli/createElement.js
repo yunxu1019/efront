c4f41358d62b788f28eda60270605cd46cf55b3e
不枝雀 <yunxu1019@qq.com>
2018-05-27 00:15:30 +0800

    增加几个类似jQuery的语法

--- coms/zimoli/createElement.js
+++ coms/zimoli/createElement.js
@@ -4,11 +4,29 @@
  */
 
 var slice = [].slice;
-
+var prototype = {
+    addClass(clazz) {
+        addclass(this, clazz);
+        return this;
+    },
+    removeClass(clazz) {
+        removeClass(this, clazz);
+        return this;
+    },
+    append() {
+        appendChild(this, [].slice.call(arguments, 0));
+        return this;
+    },
+    appendTo(target) {
+        appendChild(target, this);
+        return this;
+    }
+}
 
 function createElement(name) {
     var node = isNode(name) ? name.cloneNode() : isFunction(name) ? name() : document.createElement(name);
     if (name.className) node.className = name.className;
     appendChild(node, slice.call(arguments, 1));
+    extend(node, prototype);
     return node;
 }
