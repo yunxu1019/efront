759e94aacadb7f4b15e843126aabd43b1189f64a
不枝雀 <yunxu1019@qq.com>
2018-05-27 18:47:48 +0800

    阻止环形引用

--- coms/zimoli/text.js
+++ coms/zimoli/text.js
@@ -1,11 +1,5 @@
 function text(node, text) {
     if (arguments.length === 2) {
-        if (isString(node)) {
-            var _node = div();
-            appendChild(_node, document.createTextNode(node));
-            css(_node, text);
-            return _node;
-        }
         node.innerHTML = "";
         isFunction(text) && (text = text());
         isFunction(node.text) ? node.text(text) : node.appendChild(document.createTextNode(text));
