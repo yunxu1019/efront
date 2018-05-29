290593c8d18d56549011ca01ca969c5534ad7732
不枝雀 <yunxu1019@qq.com>
2018-05-29 23:32:44 +0800

    encodeURIComponent

--- coms/zimoli/serialize.js
+++ coms/zimoli/serialize.js
@@ -1,7 +1,7 @@
 function serialize(map, spliter = "&", equals = "=") {
     var result = [];
     for (var k in map) {
-        result.push(k + equals + map[k]);
+        result.push(k + equals + encodeURIComponent(map[k]));
     }
     return result.join(spliter);
 }
