a115419d4ed54cec9317c9feb3eded912114d6ec
不枝雀 <yunxu1019@qq.com>
2018-06-15 21:08:26 +0800

    读取并返回磁盘文件信息

---
+++ apis/zimoli/file/info.js
@@ -0,0 +1,60 @@
+var fs = require("fs");
+var path = require("path");
+var rootDirectorys = [
+    "E:\\77"
+]
+function infos({ files = rootDirectorys, folder = "." }) {
+    return new Promise(function (ok, oh) {
+        if (!files.length) return ok([]);
+        var results = [];
+        function resolve(a) {
+            results.push(a);
+            if (results.length >= files.length) {
+                ok(results);
+            }
+        }
+        files.map(function (pathname) {
+            var name = pathname;
+            pathname = path.join(folder, pathname);
+            fs.exists(pathname, function (exists) {
+                if (!exists) {
+                    return resolve({
+                        name,
+                        pathname,
+                        exists
+                    });
+                }
+                fs.stat(pathname, function (error, stats) {
+                    if (error) {
+                        resolve({
+                            name,
+                            pathname,
+                            error
+                        });
+                    } else {
+                        var result = {
+                            name,
+                            pathname,
+                            atimeMs: stats.atimeMs,
+                            ctimeMs: stats.ctimeMs,
+                            mtimeMs: stats.ctimeMs,
+                            birthtimeMs: stats.birthtimeMs,
+                            isFolder: stats.isDirectory(),
+                            isFile: stats.isFile(),
+                            size: stats.size
+                        };
+                        if (result.isFolder) {
+                            fs.readdir(pathname, function (error, children) {
+                                result.children = error || children;
+                                resolve(result);
+                            });
+                        } else {
+                            resolve(result);
+                        }
+                    }
+                })
+            });
+        });
+    });
+}
+module.exports = infos;
