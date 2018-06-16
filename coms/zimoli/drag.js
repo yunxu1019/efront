4b756983ce6fddb73670b0aebc5cf30696ccb648
不枝雀 <yunxu1019@qq.com>
2018-06-17 04:03:48 +0800

    拖动后的窗口可以适应窗口大小调整，仍然留在可视区

--- coms/zimoli/drag.js
+++ coms/zimoli/drag.js
@@ -1,26 +1,45 @@
+function getMarginLeft(offsetLeft, offsetWidth, innerWidth) {
+    // -marginLeft/offsetWidth=(-marginLeft+offsetLeft)/innerWidth
+    // marginLeft/innerWidth-marginLeft/offsetWidth=offsetLeft/innerWidth
+    // marginLeft*offsetWidth-marginLeft*innerWidth=offsetLeft*offsetWidth
+    // marginLeft(offsetWidth-innerWidth)=offsetLeft*offsetWidth
+    // marginLeft=offsetLeft*offsetWidth/(offsetWidth-innerWidth)
+    if (offsetWidth === innerWidth) return -offsetWidth / 2;
+    return offsetLeft * offsetWidth / (offsetWidth - innerWidth);
+}
 function drag(target, event, overflow = false) {
     var saved_delta = { x: target.offsetLeft - event.clientX, y: target.offsetTop - event.clientY };
     var cancelmousemove = onmousemove(window, function (event) {
+        var { innerHeight, innerWidth } = window;
         event.preventDefault();
-        var left = saved_delta.x + event.clientX;
-        var top = saved_delta.y + event.clientY;
+        var offsetLeft = saved_delta.x + event.clientX;
+        var offsetTop = saved_delta.y + event.clientY;
+        var {
+            offsetHeight,
+            offsetWidth
+        } = target;
         if (!overflow) {
-            if (left + target.offsetWidth > innerWidth) {
-                left = innerWidth - target.offsetWidth;
+            if (offsetLeft + offsetWidth > innerWidth) {
+                offsetLeft = innerWidth - offsetWidth;
             }
-            if (top + target.offsetHeight > innerHeight) {
-                top = innerHeight - target.offsetHeight;
+            if (offsetTop + offsetHeight > innerHeight) {
+                offsetTop = innerHeight - offsetHeight;
             }
-            if (left < 0) {
-                left = 0;
+            if (offsetLeft < 0) {
+                offsetLeft = 0;
             }
-            if (top < 0) {
-                top = 0;
+            if (offsetTop < 0) {
+                offsetTop = 0;
             }
         }
-        css(target, `left:${left}px;top:${top}px;`);
+        var marginLeft = getMarginLeft(offsetLeft, offsetWidth, innerWidth);
+        var marginTop = getMarginLeft(offsetTop, offsetHeight, innerHeight);
+        var left = offsetLeft - marginLeft;
+        var top = offsetTop - marginTop;
+        css(target, `left:${left * 100 / innerWidth}%;top:${top * 100 / innerHeight}%;margin-left:${marginLeft}px;margin-top:${marginTop}px`);
     });
     var cancelmouseup = onmouseup(window, function (event) {
+        saved_delta = null;
         cancelmousemove();
         cancelmouseup();
     });
