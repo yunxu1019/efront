78d1aed50ea301cbfd58ae3b9b52fca5944a0ae6
不枝雀 <yunxu1019@qq.com>
2018-06-25 12:51:41 +0800

    仅对类似searchString的值做encodeURI和decodeURI

--- coms/zimoli/parseKV.js
+++ coms/zimoli/parseKV.js
@@ -1,11 +1,12 @@
 function parseKV(string, spliter = "&", equals = "=") {
     var object = {};
+    var decode = spliter === "&" && equals === "=" ? decodeURIComponent : a => a;
     if (isString(string)) {
         var kvs = string.split(spliter)
         for (var cx = 0, dx = kvs.length; cx < dx; cx++) {
             var kv = kvs[cx];
             var index = kv.indexOf(equals);
-            object[kv.slice(0, index)] = kv.slice(index + 1);
+            object[decode(kv.slice(0, index))] = decode(kv.slice(index + 1));
         }
     }
     return object;
--- coms/zimoli/serialize.js
+++ coms/zimoli/serialize.js
@@ -1,7 +1,8 @@
 function serialize(map, spliter = "&", equals = "=") {
     var result = [];
+    var encode = spliter === "&" && equals === "=" ? encodeURIComponent : a => a;
     for (var k in map) {
-        result.push(k + equals + encodeURIComponent(map[k]));
+        result.push(encode(k) + equals + encode(map[k]));
     }
     return result.join(spliter);
 }
