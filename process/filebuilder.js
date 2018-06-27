fdb7c86fd61a2243f8ab162157e723a6f9ac29ef
不枝雀 <yunxu1019@qq.com>
2018-06-27 19:21:37 +0800

    仅对非public_path中的文件进行压缩处理

--- process/filebuilder.js
+++ process/filebuilder.js
@@ -97,24 +97,30 @@ var config = {
     quoteCharacter: "",
 }
 var builder;
+var path = require("path");
 if (process.env.IN_TEST_MODE) builder = function (buff, name) {
     if (/\b(index|default)\.html$/i.test(name) || /\.html?$/.test(name) && /^\s*!<Doctype/i.test(buff.slice(0, 100).toString())) {
         buff = Buffer.from(String(buff).replace(/<script.*?>([\s\S]*?)<\/script>/i, `<script>-function(){var xhr=new XMLHttpRequest;xhr.open("post","/reload");xhr.timeout=0;xhr.onerror=function(){console.error("reload",new Date);location.reload();};xhr.onreadystatechange=function(){if(xhr.readyState===4)location.reload()|console.warn("reload..",new Date);};xhr.send("haha");}();\r\n$1</script>`));
     }
     return buff;
 };
+
 else builder = function (buff, name, fullpath) {
     try {
-        if (/\.html$/i.test(name)) {
-            buff = Buffer.from(htmlMinifier.minify(buff.toString(), config));
-        } else if (/\.js$/i.test(name)) {
-            buff = Buffer.from(htmlMinifier.minify("<script><!--\r\n- function (document, window) {" + typescript.transpile(buff.toString()) + "}.call(this, document, this);\r\n</script>", config).replace(/<script>|<\/script>/g, ""));
-        } else if (/\.css$/i.test(name)) {
-            buff = Buffer.from(htmlMinifier.minify("<style>" + buff.toString() + "</style>", config).replace(/<style>|<\/style>/g, ""));
+        var relativePath = path.relative(fullpath, process.env.PUBLIC_PATH);
+        if (!/^\.\./.test(relativePath)) {
+            if (/\.html$/i.test(name)) {
+                buff = Buffer.from(htmlMinifier.minify(buff.toString(), config));
+            } else if (/\.js$/i.test(name)) {
+                buff = Buffer.from(htmlMinifier.minify("<script><!--\r\n- function (document, window) {" + typescript.transpile(buff.toString()) + "}.call(this, document, this);\r\n</script>", config).replace(/<script>|<\/script>/g, ""));
+            } else if (/\.css$/i.test(name)) {
+                buff = Buffer.from(htmlMinifier.minify("<style>" + buff.toString() + "</style>", config).replace(/<style>|<\/style>/g, ""));
+            }
         }
     } catch (e) {
         console.warn(`${name}压缩失败！`);
     }
     return buff;
 };
+
 module.exports = builder;
