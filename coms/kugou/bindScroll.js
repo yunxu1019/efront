963dede5f7d6bcb51232245ff26eec38ca66c001
不枝雀 <yunxu1019@qq.com>
2018-07-02 22:24:13 +0800

    通过render更新UI的绑定关系

--- coms/kugou/bindScroll.js
+++ coms/kugou/bindScroll.js
@@ -1,12 +1,16 @@
+cross.digest = render.digest;
 function bindScroll(titlebar, page) {
-    var labelarea = titlebar.children[0];
-    css(labelarea, "height:100%;width:100%;position:absolute;left:0;top:0;background-color:rgb(44,162,249);");
-    css(titlebar.children[1], "height:50px;box-shadow:none;");
+    var labelarea = div();
+    var [label, back] = titlebar.children;
+    css(back, "height:50px;");
+    appendChild.after(label, labelarea);
+    css(labelarea, "height:100%;width:100%;position:absolute;background:#000;left:0;top:0;z-index:-1");
     onappend(page, function () {
-        css(titlebar, "background:transparent;min-height:50px;box-shadow:none");
+        css(titlebar, "min-height:50px;");
         opacity(labelarea, 0);
+        opacity(label, 0);
     });
-    page.onscroll = function () {
+    var refresh = function () {
         var image = page.children[0];
         if (!image) return;
         if (!image.style.backgroundImage) image = page.children[1] || image;
@@ -18,7 +22,15 @@ function bindScroll(titlebar, page) {
         if (height > offsetHeight) {
             height = offsetHeight;
         }
+        if (height < 50) {
+            height = 50;
+        }
         css(titlebar, `background: center ${image.style.backgroundImage};background-size:cover;height:${height}px;`);
-        opacity(labelarea, 1 - titlebar.offsetHeight / offsetHeight);
-    }
+        opacity(labelarea, (1 - (titlebar.offsetHeight + offsetHeight) / offsetHeight / 2));
+        opacity(label, 1 - (titlebar.offsetHeight - 50) / (offsetHeight - 50));
+        return {};
+    };
+    page.setAttribute("ng-class", "refresh()");
+    render(page, { refresh });
+    page.onscroll = refresh;
 }
