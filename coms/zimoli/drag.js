094334f4a82d74a9f352cfee1c66c0c94759752a
不枝雀 <yunxu1019@qq.com>
2018-07-14 00:06:38 +0800

    支持拖动非绝对定位的元素

--- coms/zimoli/drag.js
+++ coms/zimoli/drag.js
@@ -9,12 +9,26 @@ function getMarginLeft(offsetLeft, offsetWidth, innerWidth) {
 }
 function drag(target, event, overflow = false) {
     var saved_delta = { x: target.offsetLeft - event.clientX, y: target.offsetTop - event.clientY };
+    var clone;
+    var saved_opacity = target.style.opacity;
     var cancelmousemove = onmousemove(window, function (event) {
         if (event.defaultPrevented) return;
         if (!saved_delta.ing) {
             var abs = Math.abs;
+            if (event.buttons !== 1) return clear();
             if (abs(target.offsetLeft - event.clientX - saved_delta.x < 3) && abs(target.offsetTop - event.clientY - saved_delta.y) < 3) return;
             saved_delta.ing = true;
+            if (!/absolute|fixed/.test(getComputedStyle(target).position)) {
+                clone = cloneVisible(target);
+                var position = getScreenPosition(target);
+                css(target, "opacity:0");
+                css(clone, `position:absolute;left:${position.left}px;top:${position.top}px;`);
+                appendChild(document.body, clone);
+                saved_delta.x += clone.offsetLeft - target.offsetLeft;
+                saved_delta.y += clone.offsetTop - target.offsetTop;
+            } else {
+                clone = target;
+            }
         }
         event.preventDefault();
         var offsetLeft = saved_delta.x + event.clientX;
@@ -41,11 +55,13 @@ function drag(target, event, overflow = false) {
         var marginTop = getMarginLeft(offsetTop, offsetHeight, innerHeight);
         var left = offsetLeft - marginLeft;
         var top = offsetTop - marginTop;
-        css(target, `left:${left * 100 / innerWidth}%;top:${top * 100 / innerHeight}%;margin-left:${marginLeft}px;margin-top:${marginTop}px`);
+        css(clone, `left:${left * 100 / innerWidth}%;top:${top * 100 / innerHeight}%;margin-left:${marginLeft}px;margin-top:${marginTop}px`);
     });
-    var cancelmouseup = onmouseup(window, function (event) {
+    var clear = function () {
         saved_delta = null;
+        if (clone !== target) remove(clone), css(target, { opacity: saved_opacity });
         cancelmousemove();
         cancelmouseup();
-    });
+    };
+    var cancelmouseup = onmouseup(window, clear);
 }
