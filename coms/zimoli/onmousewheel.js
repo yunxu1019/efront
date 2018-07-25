2ac83438f003b8806287d4a94e27714a90e02bfa
不枝雀 <yunxu1019@qq.com>
2018-07-26 00:13:13 +0800

    增加onmousewheel兼容性

--- coms/zimoli/onmousewheel.js
+++ coms/zimoli/onmousewheel.js
@@ -1,1 +1,30 @@
-var onmousewheel = "onmousewheel" in window ? on("mousewheel") : on("DOMMouseScroll");
+var _onmousewheel = "onmousewheel" in window || "onmousewheel" in document ? on("mousewheel") : on("DOMMouseScroll");
+var addDeltaY = function (event) {
+    if (isNumber(event.deltaY)) return;
+    if (isNumber(event.wheelDeltaY)) {
+        event.deltaY = event.wheelDeltaY;
+        event.deltaX = event.wheelDeltaX;
+    } else if (isNumber(event.axis) && event.detail) {
+        var delta = event.detail;
+        if (delta > 0) {
+            delta = 120 - 120 / (1 + delta / 12);
+        } else {
+            delta = 120 / (1 - delta / 12) - 120;
+        }
+        if (event.axis === 1) {
+            event.deltaX = delta;
+            event.deltaY = 0;
+        } else {
+            event.deltaX = 0;
+            event.deltaY = delta;
+        }
+    } else if (isNumber(event.wheelDelta)) {
+        event.deltaY = event.wheelDelta;
+    }
+}
+var onmousewheel = function (target, handler) {
+    return _onmousewheel(target, function (event) {
+        addDeltaY(event);
+        return handler.call(this, event);
+    });
+};
