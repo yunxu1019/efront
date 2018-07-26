5f54fc326a6769777b5c1d587e57de48eea67f36
不枝雀 <yunxu1019@qq.com>
2018-07-27 00:27:05 +0800

    点击时获取点击的坐标显示在地图上

--- coms/zimoli/maps_test.js
+++ coms/zimoli/maps_test.js
@@ -1,7 +1,23 @@
 function maps_test() {
     var map = new maps({
-        center: [114.834364, 34.82138],
-        zoom: 10
+        center: [/*兰考*/114.834364, 34.82138],
+        center: [/*蒙古*/104.751074, 44.163022],
+        center: [/*西亚*/48.068813, 29.321316],
+        zoom: 12
+    });
+    onclick(map, function (event) {
+        var map = this.map;
+        var layerx = event.offsetX || event.layerX || 0;
+        var layery = event.offsetY || event.layerY || 0;
+        var [centerlng, centerlat] = map.Center();
+        var zoom = map.Zoom();
+        var centerx = map.lng2x(centerlng, zoom);
+        var centery = map.lat2y(centerlat, zoom);
+        var { offsetWidth, offsetHeight } = this;
+        var x = centerx + (layerx - offsetWidth / 2) / 256;
+        var y = centery + (layery - offsetHeight / 2) / 256;
+        var lng = +map.x2lng(x, zoom).toFixed(6), lat = +map.y2lat(y, zoom).toFixed(6);
+        alert(`${lng},${lat}`);
     });
     var active;
     var buttons = ["百度", "谷歌", "OSM", "Default"].map((a, cx) => {
