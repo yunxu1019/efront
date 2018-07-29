e7a71eec7ed91cdaf1f8e741e177efa4cf20b168
不枝雀 <yunxu1019@qq.com>
2018-07-29 10:35:53 +0800

    去除尾部空格

--- coms/kugou/getKugouJsonpData.js
+++ coms/kugou/getKugouJsonpData.js
@@ -1,3 +1,3 @@
 var getKugouJsonpData = function (xhr) {
-    return JSON.parse(String(xhr.responseText || xhr.response || "").replace(/^.*?\(([\s\S]*?)\)$/, "$1")).data;
+    return JSON.parse(String(xhr.responseText || xhr.response || "").replace(/^.*?\(([\s\S]*?)\)[\s;]*$/, "$1")).data;
 };
