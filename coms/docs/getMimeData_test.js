2f31531c72c309ca4ee0d652c72bc2d69446ead4
不枝雀 <yunxu1019@qq.com>
2018-08-09 21:43:17 +0800

    读取tomcat中的mime类型的方法

---
+++ coms/docs/getMimeData_test.js
@@ -0,0 +1,20 @@
+function getMimeData_test() {
+    fetch("data/web.xml").then(function (result) {
+        return result.text();
+    }).then(function (result) {
+        var doc = div();
+        doc.innerHTML = result;
+        var mimeMappings = doc.querySelectorAll("mime-mapping");
+        mimeMappings = [].slice.call(mimeMappings, 0);
+        var mimeMap = {};
+        mimeMappings.forEach(function (map) {
+            [extension, mimeType] = [].map.call(map.children, a => a.innerText);
+            if (!mimeMap[mimeType]) {
+                mimeMap[mimeType] = [];
+            }
+            mimeMap[mimeType].push(extension);
+        });
+        var result = Object.keys(mimeMap).map(k => `${k}:${mimeMap[k].join("|")}`);
+        console.log(JSON.stringify(result, null, 4));
+    });
+}
