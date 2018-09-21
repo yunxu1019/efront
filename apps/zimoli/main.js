bf430fc967103e052e6bc2fe40654dc819391fca
不枝雀 <yunxu1019@qq.com>
2018-09-21 12:56:25 +0800

    背景色修改为黑色

--- apps/zimoli/main.js
+++ apps/zimoli/main.js
@@ -5,6 +5,7 @@ appendChild(view, kugou$menu, kugou$page);
 view.initialStyle = "transform:scale(.92)";
 css(view, "width:100%;height:100%;position:absolute;left:0;top:0;bottom:0;right:0;overflow:hidden");
 css(view, "background-image: linear-gradient(0deg, #0b3b5d, 5%, #031341, 90%, #235b71)");
+css("body", "background:#000");
 function main() {
     return view;
 }
