8b147f74e39a4d3cae9f560bf0b6048bb2322f72
不枝雀 <yunxu1019@qq.com>
2018-09-22 00:16:22 +0800

    增加button的点击效果

--- apps/zimoli/kugou/listen.js
+++ apps/zimoli/kugou/listen.js
@@ -7,22 +7,25 @@ page.innerHTML = `<div class=user-info>
     </div>
 </div>
 <div class=music-info>
-    <div ng-click=go('/zimoli/files/main') ><i>&#xe610;</i>本地音乐</div>
-    <div><i>&#xe616;</i>喜欢·歌单</div>
-    <div><i>&#xe606;</i>下载</div>
-    <div><i>&#xe64d;</i>最近</div>
+    <btn ng-click=go('/zimoli/files/main') ><i>&#xe610;</i>本地音乐</btn>
+    <btn><i>&#xe616;</i>喜欢·歌单</btn>
+    <btn><i>&#xe606;</i>下载</btn>
+    <btn><i>&#xe64d;</i>最近</btn>
 </div>
 <div class=button-group >
-    <div><i style=background-color:#ffbd0d;>&#xe62c;</i>乐库</div>
-    <div><i style=background:#2bcda2;>&#xe68c;</i>歌单</div>
-    <div><i style=background:#3ab5f7;>&#xe6df;</i>电台·酷群</div>
-    <div><i style=background:#e76ce8;>&#xe634;</i>猜你喜欢</div>
-    <div><i style=background:#ff6565;>&#xeb85;</i>每日推荐</div>
-    <div><i style=background:#ff852c;>&#xe603;</i>音乐圈</div>
+    <btn><i style=background-color:#ffbd0d;>&#xe62c;</i>乐库</btn>
+    <btn><i style=background:#2bcda2;>&#xe68c;</i>歌单</btn>
+    <btn><i style=background:#3ab5f7;>&#xe6df;</i>电台·酷群</btn>
+    <btn><i style=background:#e76ce8;>&#xe634;</i>猜你喜欢</btn>
+    <btn><i style=background:#ff6565;>&#xeb85;</i>每日推荐</btn>
+    <btn><i style=background:#ff852c;>&#xe603;</i>音乐圈</btn>
 </div>`.replace(/>\s+</g, "><");
 
-render(page,{
-    go
+render(page, {
+    go,
+    btn(a) {
+        return button(a, "white");
+    }
 });
 
 function main() {
