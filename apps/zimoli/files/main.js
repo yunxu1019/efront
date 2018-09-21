e947ff8dca7ab0434b770d9a97cf2c1b6bcb688d
不枝雀 <yunxu1019@qq.com>
2018-09-21 12:57:25 +0800

    支持跳转到文件管理页

---
+++ apps/zimoli/files/main.js
@@ -0,0 +1,6 @@
+titlebar("文件管理器");
+var page = div();
+page.initialStyle = "margin-left:100%;z-index:2";
+function main() {
+    return page;
+}
--- apps/zimoli/kugou/listen.js
+++ apps/zimoli/kugou/listen.js
@@ -7,7 +7,7 @@ page.innerHTML = `<div class=user-info>
     </div>
 </div>
 <div class=music-info>
-    <div><i>&#xe610;</i>本地音乐</div>
+    <div ng-click=go('/zimoli/files/main') ><i>&#xe610;</i>本地音乐</div>
     <div><i>&#xe616;</i>喜欢·歌单</div>
     <div><i>&#xe606;</i>下载</div>
     <div><i>&#xe64d;</i>最近</div>
@@ -20,6 +20,11 @@ page.innerHTML = `<div class=user-info>
     <div><i style=background:#ff6565;>&#xeb85;</i>每日推荐</div>
     <div><i style=background:#ff852c;>&#xe603;</i>音乐圈</div>
 </div>`.replace(/>\s+</g, "><");
+
+render(page,{
+    go
+});
+
 function main() {
     return page;
 }
