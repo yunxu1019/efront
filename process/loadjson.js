7bfd59f6b1da6278984d3e304e1661fb615ecb36
不枝雀 <yunxu1019@qq.com>
2018-09-23 23:28:12 +0800

    增加异步读取的方法

--- process/loadjson.js
+++ process/loadjson.js
@@ -1,6 +1,22 @@
 "use strict";
-var fs=require("fs");
-module.exports=function loadjson(url) {
+var fs = require("fs");
+var loadjson = module.exports = function loadjson(url) {
     var data = fs.readFileSync(url);
     return new Function("return " + String(data))();
-}
+};
+loadjson.async = function (url) {
+    return new Promise(function (ok, oh) {
+        fs.exists(url, function (exists) {
+            if (!exists) return oh(new Error("文件不存在!"));
+            fs.stat(url, function (error, stats) {
+                if (error) return oh(error);
+                if (!stats.isFile()) return oh(new Erorr("路径所指向的不是文件"));
+                if (stats.size > 2 * 1024 * 1024) return oh(new Error("文件过大!"));
+                fs.readFile(url, function (error, buffer) {
+                    if (error) return oh(error);
+                    ok(new Function("return " + String(buffer))());
+                });
+            });
+        });
+    });
+};
