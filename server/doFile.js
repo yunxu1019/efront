ebb46dae89e88ef1cb69ca88d164c9c8cd51be09
不枝雀 <yunxu1019@qq.com>
2018-09-28 01:50:18 +0800

    支持320M以内的大文件

--- server/doFile.js
+++ server/doFile.js
@@ -3,7 +3,53 @@ var path = require("path");
 var proxy = require("./proxy");
 var checkAccess = require("./checkAccess");
 var root = "./apps/zimoli";
-var cacheRangeSize = 2 * 1024 * 1024;
+var cacheRangeSize = 320 * 1024 * 1024;
+var cachePieceSize = 256 * 1024;
+var cacheCountLimit = 200;
+function pipe(h, start, end, res) {
+    var inc = start;
+    var timeout = false;
+    var safeend = function () {
+        timeout = true;
+        res.end();
+        fs.closeSync(h);
+        cacheCountLimit++;
+    };
+    cacheCountLimit--;
+    if (cacheCountLimit < 0) {
+        safeend();
+        return;
+    }
+    var halfend = setTimeout(safeend, 2 * 1000);
+    var saveend = function () {
+        clearTimeout(halfend);
+        safeend();
+    };
+    var run = function () {
+        var ind = inc + cachePieceSize;
+        if (ind > end) ind = end;
+        var chunk = Buffer.alloc(ind - inc);
+        if (inc >= end) return saveend();
+        fs.read(h, chunk, 0, chunk.length, inc, function (error, size, chunk) {
+            if (timeout) return;
+            if (error) {
+                res.writeHead(403, {});
+                res.write(String(error));
+                saveend();
+                return;
+            }
+            if (size < chunk.length) {
+                chunk = chunk.slice(0, size);
+            }
+            res.write(chunk);
+            inc = ind;
+            run();
+        });
+    };
+    run();
+}
+
+
 function doFile(req, res) {
     if (/^\/@/.test(req.url)) {
         var filepath = decodeURIComponent(req.url.slice(2));
@@ -34,42 +80,38 @@ function doFile(req, res) {
                 res.end("folder");
                 return;
             }
+            start = +start | 0;
+            if (!start || start < 0) {
+                start = 0;
+            }
+            if (start > stats.size) {
+                res.writeHead(400, {});
+                res.end("out of range");
+                return;
+            }
+            end = +end | 0;
+            if (!end || end < 0) {
+                end = start + cacheRangeSize;
+            }
+            if (end - start > cacheRangeSize) {
+                end = start + cacheRangeSize;
+            }
+            if (end > stats.size) {
+                end = stats.size;
+            }
             fs.open(filepath, "r", function (error, h) {
                 if (error) {
                     res.writeHead(403, {});
                     res.end(String(error));
                     return fs.closeSync(h);
                 }
-                start = +start | 0;
-                if (!start || start < 0) {
-                    start = 0;
-                }
-                end = +end | 0;
-                if (!end || end < 0) {
-                    end = start + cacheRangeSize;
-                }
-                if (end - start > cacheRangeSize) {
-                    end = start + cacheRangeSize;
-                }
-                var chunk = Buffer.alloc(end - start);
-                fs.read(h, chunk, 0, chunk.length, start, function (error, size, chunk) {
-                    if (error) {
-                        res.writeHead(403, {});
-                        res.end(String(error));
-                        return fs.closeSync(h);
-                    }
-                    if (size < chunk.length) {
-                        chunk = chunk.slice(0, size);
-                    }
-                    res.writeHead(206, {
-                        "Accept-Ranges": "bytes",
-                        "Content-Length": chunk.length,
-                        "Content-Range": `bytes ${start}-${start + chunk.length - 1}/${stats.size}`,
-                        "Last-Modified": stats.mtime.toUTCString(),
-                    });
-                    res.end(chunk);
-                    fs.closeSync(h);
+                res.writeHead(end === stats.size && start === 0 ? 200 : 206, {
+                    "Accept-Ranges": "bytes",
+                    "Content-Length": end - start,
+                    "Content-Range": `bytes ${start}-${end - 1}/${stats.size}`,
+                    "Last-Modified": stats.mtime.toUTCString(),
                 });
+                pipe(h, start, end, res);
             });
         })
     });
