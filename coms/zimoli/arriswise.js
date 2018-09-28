0a568778d2d2c45eecdc0cff6b676ed273a58a18
不枝雀 <yunxu1019@qq.com>
2018-09-29 01:43:18 +0800

    从横向方法直接构造纵向方法

---
+++ coms/zimoli/arriswise.js
@@ -0,0 +1,26 @@
+var reflect = {
+    Width: "Height",
+    Height: "Width",
+    width: "height",
+    height: "width",
+    X: "Y",
+    Y: "X",
+    Top: "Left",
+    Left: "Top",
+    top: "left",
+    left: "top",
+    Right: "bottom",
+    Bottom: "Right",
+    right: "bottom",
+    bottom: "right"
+};
+var regkeys = keys(reflect);
+var regexps = new RegExp(regkeys.join("|"), "g");
+function arriswise(func, argumentsList = [], thisObj) {
+    var newf = String(func).replace(regexps, function (matched) {
+        var searched = reflect[matched];
+        if (searched) return searched;
+        return matched;
+    });
+    return Function.apply(null, argumentsList.slice(0, argumentsList.length >> 1).concat("return " + newf)).apply(thisObj, argumentsList.slice(argumentsList.length >> 1))
+}
