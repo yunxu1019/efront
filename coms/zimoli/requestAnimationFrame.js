34ae60b0aa6867e5b3198ef73e526fa39dc618bb
不枝雀 <yunxu1019@qq.com>
2018-09-29 00:53:16 +0800

    以特定的时间点运行

--- coms/zimoli/requestAnimationFrame.js
+++ coms/zimoli/requestAnimationFrame.js
@@ -1,4 +1,8 @@
+var lastAnimateTime = +new Date;
 var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
     //一般集中在25毫秒，左右相差不超过1毫秒
-    return setTimeout(callback, 25);
+    var currentTime = +new Date;
+    var deltaTime = currentTime - lastAnimateTime > 16 ? 0 : currentTime % 16;
+    lastAnimateTime = deltaTime + currentTime;
+    return setTimeout(callback, deltaTime);
 };
