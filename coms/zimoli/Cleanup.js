55c9bbbdf9e9e62d3f6da90befaa4c196da86f43
不枝雀 <yunxu1019@qq.com>
2018-10-01 03:39:36 +0800

    修复无法清理第一个元素的问题

--- coms/zimoli/Cleanup.js
+++ coms/zimoli/Cleanup.js
@@ -1,7 +1,7 @@
 function Cleanup(list) {
     function cleanup() {
         var target = this;
-        for (var cx = list.length - 1; cx > 0; cx--) {
+        for (var cx = list.length - 1; cx >= 0; cx--) {
             var element = list[cx];
             if (element === target) {
                 list.splice(cx, 1);
