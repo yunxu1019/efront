619e0d948e918bfbcea6a433d21c9cfc6c5aea33
不枝雀 <yunxu1019@qq.com>
2018-09-30 17:33:37 +0800

    减少内存占用

--- coms/zimoli/button.js
+++ coms/zimoli/button.js
@@ -5,6 +5,51 @@ _label.className = "label";
 
 var btn = div();
 btn.tabIndex = 0;
+var isClickable = function () {
+    return +this.tabIndex === 0;
+};
+
+var hover = function () {
+    if (isClickable.call(this))
+        addClass(this, "hover");
+};
+var active = function () {
+    if (isClickable.call(this))
+        addClass(this, "active hover");
+};
+var resetactive = function () {
+    if (isClickable.call(this))
+        removeClass(this, "active");
+};
+var resetall = function () {
+    if (isClickable.call(this))
+        removeClass(this, "active hover");
+};
+var mousedown = function () {
+    var cancelmouseup = onmouseup(window, function () {
+        cancelmouseup();
+        resetactive();
+    });
+    active.call(this);
+};
+var mouseleave = function () {
+    removeClass(this, "hover");
+};
+var mousemove = function (event) {
+    if (onclick.preventClick && event.which) resetall.call(this);
+};
+
+var touchstart = function () {
+    var that = this;
+    var cancel = function () {
+        canceltouchcancel();
+        canceltouchend();
+        resetall.call(that);
+    };
+    var canceltouchcancel = ontouchcancel(window, cancel);
+    var canceltouchend = ontouchend(window, cancel);
+    active.call(this);
+};
 function button(texter, type) {
     var tracker = createElement(track);
     var _texter;
@@ -16,44 +61,13 @@ function button(texter, type) {
             html(_texter, texter);
     }
     var button = createElement(btn, tracker, _texter);
-    var hover = function () {
-        addClass(button, "hover");
-    };
-    var active = function () {
-        addClass(button, "active hover");
-    };
-    var resetactive = function () {
-        removeClass(button, "active");
-    };
-    var resetall = function () {
-        removeClass(button, "active hover");
-    }
     onremove(button, resetall);
     onmouseover(button, hover);
-    onmouseleave(button, function () {
-        removeClass(button, "hover");
-    });
-    onmousemove(button, function (event) {
-        if (onclick.preventClick && event.which) resetall();
-    });
-    onmousedown(button, function () {
-        var cancelmouseup = onmouseup(window, function () {
-            cancelmouseup();
-            resetactive();
-        });
-        active();
-    });
+    onmouseleave(button, mouseleave);
+    onmousemove(button, mousemove);
+    onmousedown(button, mousedown);
     ontouchmove(button, resetall);
-    ontouchstart(button, function () {
-        var cancel = function () {
-            canceltouchcancel();
-            canceltouchend();
-            resetall();
-        };
-        var canceltouchcancel = ontouchcancel(window, cancel);
-        var canceltouchend = ontouchend(window, cancel);
-        active();
-    });
+    ontouchstart(button, touchstart);
     button.setText = function (_text) {
         if (_text && _text.length === 2) {
             addClass(button, "space");
