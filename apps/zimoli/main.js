5b8b5829f8b71cd482d60896fcfac35d54d678df
不枝雀 <yunxu1019@qq.com>
2018-10-04 14:38:23 +0800

    pc上自适应的样式

--- apps/zimoli/main.js
+++ apps/zimoli/main.js
@@ -1,11 +1,12 @@
 // 中文编码 utf-8
 "use strict";
-var view = div();
-appendChild(view, kugou$menu, kugou$page);
-view.initialStyle = "transform:scale(.92)";
-css(view, "width:100%;height:100%;position:absolute;left:0;top:0;bottom:0;right:0;overflow:hidden");
-css(view, "background-image: linear-gradient(0deg, #0b3b5d, 5%, #031341, 90%, #235b71)");
+var menu = div();
+appendChild(menu, kugou$menu, kugou$page);
+var view = layer$leftCenter(menu);
+menu.initialStyle = "transform:scale(.92)";
+css(menu, "background-image: linear-gradient(0deg, #0b3b5d, 5%, #031341, 90%, #235b71);height:100%;width:100%");
 css("body", "background:#000");
+css(":after,:before,*", "box-sizing:border-box;");
 function main() {
     return view;
 }
