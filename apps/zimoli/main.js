95f4851bd131f727d86f092ceea4de6632915580
不枝雀 <yunxu1019@qq.com>
2018-10-05 08:07:18 +0800

    添加背景图像

--- apps/zimoli/main.js
+++ apps/zimoli/main.js
@@ -5,7 +5,7 @@ appendChild(menu, kugou$menu, kugou$page);
 var view = layer$leftCenter(menu);
 menu.initialStyle = "transform:scale(.92)";
 css(menu, "background-image: linear-gradient(0deg, #0b3b5d, 5%, #031341, 90%, #235b71);height:100%;width:100%");
-css("body", "background:#000");
+css("body", "background:url('images/background.jpg') no-repeat;background-size:cover;");
 css(":after,:before,*", "box-sizing:border-box;");
 function main() {
     return view;
