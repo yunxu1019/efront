f6adcb724c911e001d227df352642eabeb235d65
不枝雀 <yunxu1019@qq.com>
2018-10-06 21:43:14 +0800

    extend功能扩展

--- coms/zimoli/extend.js
+++ coms/zimoli/extend.js
@@ -4,9 +4,14 @@
  */
 
 
-function extend(o1, o2) {
-    if (o1 instanceof Object && o2 instanceof Object) for (var k in o2) {
-        o1[k] = o2[k];
+var extend = Object.assign || function (o1) {
+    if (o1 instanceof Object) {
+        for (var cx = 1, dx = arguments.length; cx < dx; cx++) {
+            var o2 = arguments[cx];
+            if (o2 instanceof Object) for (var k in o2) {
+                if (hasOwnProperty.call(o2, k)) o1[k] = o2[k];
+            }
+        }
     }
     return o1;
-}
+};
