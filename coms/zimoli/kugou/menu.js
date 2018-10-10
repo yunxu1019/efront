8c6dd66a7b4698a92c2ba09f49825107609edbb1
不枝雀 <yunxu1019@qq.com>
2018-10-10 13:40:49 +0800

    添加左侧图标

--- coms/zimoli/kugou/menu.js
+++ coms/zimoli/kugou/menu.js
@@ -4,20 +4,20 @@ menu.innerHTML = `
     <div><img class=avatar src=images/avatar.png /></div><div class=info><div class=name>不枝雀</div><div><span class=level>LV.5</span></div></div>
 </div>
 <div class=options-list>
-<btn>消息中心</btn>
-<btn>皮肤中心</btn>
-<btn>会员中心</btn>
-<btn>流量包月</btn>
-<btn>定时关闭</btn>
-<btn>蝰索音效</btn>
-<btn>音乐工具</btn>
-<btn>个性彩铃</btn>
-<btn>私人云盘</btn>
-<switch>仅Wi-Fi联网</switch>
-<switch>通知栏歌词</switch>
+<btn><i>&#xe602;</i>消息中心</btn>
+<btn><i>&#xe647;</i>皮肤中心</btn>
+<btn><i>&#xe604;</i>会员中心</btn>
+<btn><i>&#xe651;</i>流量包月</btn>
+<btn><i>&#xe62a;</i>私人云盘</btn>
+<btn><i>&#xe7bd;</i>定时关闭</btn>
+<btn><i>&#xe60b;</i>蝰索音效</btn>
+<btn><i>&#xe614;</i>音乐工具</btn>
+<btn><i>&#xe68b;</i>个性彩铃</btn>
+<switch><i>&#xe6ab;</i>仅wifi联网</switch>
+<switch><i>&#xe62f;</i>通知栏歌词</switch>
 </div>
 <div class=setting-button>
-<btn ng-click="go('/personal/setting')">设置</btn>
+<btn ng-click="go('/personal/setting')"><i>&#xe630;</i>设置</btn>
 </div>
 `.replace(/>\s+</g, "><");
 render(menu, {
