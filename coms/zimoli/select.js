c3062302366e34e9c3c7c6caa69ef716b5364047
不枝雀 <yunxu1019@qq.com>
2018-10-10 19:31:46 +0800

    解决ios无法弹出菜单的问题

--- coms/zimoli/select.js
+++ coms/zimoli/select.js
@@ -2,26 +2,37 @@ var saved_list;
 var _remove = function () {
     saved_list && remove(saved_list);
 };
+var preventDefault = function (event) {
+    event.preventDefault();
+};
+var lastTimeClick = 0;
 function select(target, list) {
     if (!target) {
         target = createElement(div);
     }
     target.tabIndex = 0;
     onblur(target, _remove);
-    onmousedown(list, function (event) {
-        event.preventDefault();
-    });
+    onmousedown(list, preventDefault);
     css(list, {
         zIndex: zIndex()
     });
     onremove(list, () => saved_list = null);
-    onmousedown(target, function () {
+    var mousedownfired = false;
+    var mousedown = function () {
+        if (mousedownfired) return;
+        mousedownfired = true;
         if (saved_list !== list) {
             _remove();
+            if (document.activeElement !== target) target.focus();
             popup(list, target);
             saved_list = list;
         }
         else _remove();
+    };
+    onmousedown(target, mousedown);
+    onclick(target, function(){
+        mousedown();
+        mousedownfired = false;
     });
     return target;
 }
