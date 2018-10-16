602d70032773ece9ea683d1ffc59434e1b77be9f
不枝雀 <yunxu1019@qq.com>
2018-10-16 22:47:30 +0800

    增加watch命令，修改public命令为build命令

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 // 中文编码 utf-8
 var isTestMode = process.argv.indexOf("test") >= 0;
-var isPublicMode = process.argv.indexOf("public") >= 0;
+var isPublicMode = process.argv.indexOf("public") >= 0|| process.argv.indexOf("build") >= 0;
+var isWatchMode = process.argv.indexOf("watch") >= 0;
 var isServerMode = process.argv.indexOf("server") >= 0;
 var isInitCommand = process.argv.indexOf("init") >= 0;
 if (isTestMode) {
@@ -9,9 +10,11 @@ if (isTestMode) {
 } else if (isServerMode) {
     require("./server/index");
 } else if (isPublicMode) {
-    require("./tools/public");
+    require("./tools/build");
 } else if (isInitCommand) {
     require("./tools/create");
+}else if(isWatchMode){
+    require("./tools/watch");
 } else {
     require("./process/setupenv");
     require("./process/console");
