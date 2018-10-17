439189aa4c40cd19b8440d86dc781dc0169c95f9
不枝雀 <yunxu1019@qq.com>
2018-10-18 00:16:54 +0800

    创建项目时的路径问题修复

--- tools/create.js
+++ tools/create.js
@@ -1,9 +1,8 @@
-require("../process/setupenv");
 var fs = require("fs");
 var path = require("path");
 var appname = process.env.APP;
 if (!appname) throw `请先设置app名称！如：${/win\d/i.test(process.platform) ? 'set' : 'export'} app=abc`;
-var { COMS_PATH, PAGE_PATH, PUBLIC_PATH } = process.env;
+var { COMS_PATH = "./coms", PAGE_PATH = "./apps", PUBLIC_PATH = "./public" } = process.env;
 function mkdirIfNotExists(dirname) {
     if (!fs.existsSync(dirname))
         fs.mkdirSync(dirname);
@@ -17,15 +16,22 @@ mkdirIfNotExists("_envs");
 mkdirIfNotExists(PAGE_PATH);
 mkdirIfNotExists(COMS_PATH);
 mkdirIfNotExists(PUBLIC_PATH);
-var setupData = fs.readFileSync(path.join(__dirname, "../_envs/setup.bat"));
-setupData = Buffer.concat([new Buffer(`set APP=${appname}\r\n`), setupData]);
+mkdirIfNotExists(path.join(PAGE_PATH, `${appname}`));
+var setupData = [
+    `set app=${appname}`,
+    `set PAGE_PATH=${PAGE_PATH}`,
+    `set COMS_PATH=${COMS_PATH}`,
+    `set public_path=${PUBLIC_PATH}`
+].join("\r\n");
+setupData = Buffer.from(setupData);
 fs.writeFileSync(path.join("_envs", "setup.bat"), setupData);
-try {
-    fs.linkSync(path.join(__dirname, "../coms/zimoli"), path.join(COMS_PATH, "zimoli"));
-} catch (e) {
-    copy(path.join(__dirname, "../apps/zimoli"), path.join(COMS_PATH, `zimoli`));
-}
-copy(path.join(__dirname, "../apps/zimoli"), path.join(PAGE_PATH, `${appname}`));
+copy(path.join(__dirname, "../coms/zimoli"), path.join(COMS_PATH, `zimoli`));
+var hasindex = false;
+fs.readdirSync(path.join(__dirname, "../apps/zimoli")).map(function (name) {
+    hasindex = hasindex || /^index.html$/i.test(name);
+    copy(path.join(__dirname, "../apps/zimoli/", name), path.join(PAGE_PATH, `${appname}`, name));
+});
+if (!hasindex) copy(path.join(__dirname, "../apps/index.html"), path.join(PAGE_PATH, `${appname}/index.html`));
 
 function copy(path1, path2) {
     fs.exists(path2, function (exists) {
