206df4df05df4780c513abfe4aea243bf3d74efb
不枝雀 <yunxu1019@qq.com>
2018-11-21 23:23:45 +0800

    解决click事件有时不触发的问题

--- coms/zimoli/onclick.js
+++ coms/zimoli/onclick.js
@@ -21,25 +21,25 @@ ontouchmove(window, function (event) {
     extendTouchEvent(event);
     clickcancel.call(this, event);
 });
-var onclick = function (target, handler) {
-    var offtouchend = ontouchend(target, function () {
-        dispatch(this, "click");
-    });
-    var offclick = _onclick(target, function (event) {
+if (window.addEventListener) {
+    window.addEventListener("touchend", function (event) {
+        var target = event.target;
+        var touch = event.changedTouches[0];
+        var clickEvent = document.createEvent("MouseEvents");
+        clickEvent.initMouseEvent("click", true, true, window, 1, touch.screenX, touch.screenY, touch.clientX, touch.clientY, false, false, false, false, 0, null);
+        dispatch(target, clickEvent);
+    }, true);
+    window.addEventListener("click", function (event) {
         if (!needFireClick) return;
         needFireClick = false;
         var saved_time = lasttime_click;
-        lasttime_click = +new Date;
-        if (lasttime_click - saved_time < 60) {
+        lasttime_click = event.timeStamp;
+        if (lasttime_click - saved_time < 60 || preventClick) {
             // 阻止非人为点击，防止误操作
+            event.preventDefault();
+            event.stopPropagation();
             return;
         }
-        if (preventClick) return;
-        return handler.call(this, event);
-    });
-
-    return function () {
-        offtouchend();
-        offclick();
-    };
+    }, true);
 }
+var onclick = _onclick;
