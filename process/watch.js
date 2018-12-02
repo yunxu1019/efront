0dcec6c3f3540468fc2dec447df67dbec82f38d9
不枝雀 <yunxu1019@qq.com>
2018-12-02 13:23:12 +0800

    不再支持多路径匹配

--- process/watch.js
+++ process/watch.js
@@ -11,25 +11,17 @@ function watch(file, then) {
         return;
     };
     var timmer = 0;
-    var index = file.lastIndexOf("\\") + 1 || file.lastIndexOf("\/") + 1;
-    var folder = file.slice(0, index);
-    var files = require("child_process").spawnSync(`dir /b ${file.replace(/\//g, "\\")}*`, {
-        shell: true
-    }).output[1];
-    var watchers = String(files).trim().split(/[\r\n]+/).map(function (f) {
-        var file = folder + f;
-        return fs.watch(file, function (a) {
-            var args = arguments;
-            clearTimeout(timmer);
-            timmer = setTimeout(function () {
-                try {
-                    then.apply(null, args);
-                } catch (e) {
-                    console.error("执行失败！", e.message);
-                }
-            }, 80);
-        });
-    });
+    var watchers = [fs.watch(file, function (a) {
+        var args = arguments;
+        clearTimeout(timmer);
+        timmer = setTimeout(function () {
+            try {
+                then.apply(null, args);
+            } catch (e) {
+                console.error("执行失败！", e.message);
+            }
+        }, 80);
+    })];
     watch_tree[file] = watchers;
 }
 module.exports = watch;
