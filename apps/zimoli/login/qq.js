d825f208e2c32b245240fca51f8ed2c055ab29d3
不枝雀 <yunxu1019@qq.com>
2018-12-11 21:38:37 +0800

    预加载时对QQ登录环境进行初始化

--- apps/zimoli/login/qq.js
+++ apps/zimoli/login/qq.js
@@ -1,20 +1,20 @@
 var qc_loader = script("http://qzonestyle.gtimg.cn/qzone/openapi/qc_loader.js");
+appendChild(document.body, qc_loader);
 qc_loader.async = true;
 qc_loader.setAttribute("data-appid", "1107914671");
-qc_loader.setAttribute("data-redirecturi", location.href.replace(/[^\/]+$/,"zimoli/login/qq_callback.html"));
+qc_loader.setAttribute("data-redirecturi", location.href.replace(/[^\/]+$/, "zimoli/login/qq_callback.html"));
 qc_loader.onload = function () {
-    console.log("loaded", window.QC)
-}
-onappend(qc_loader, function () {
-    console.log("append", qc_loader)
-});
+    remove(qc_loader);
+};
 
 function main() {
-    var interval=setInterval(function(){
-        if(!window.QC)return;
-        clearInterval(interval);
-        window.QC.Login.showPopup();
-        history.back();
-    },20);
-    return qc_loader;
+    window.QC && window.QC.Login.showPopup();
+    var interval = setInterval(function () {
+        if (/#.*?qq$/.test(location.href)) {
+            history.back();
+        } else {
+            clearInterval(interval);
+        }
+    }, 20);
+    return div();
 }
