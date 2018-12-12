ecc2a3a3fbe9574ef3fb2a5cff0e8f52999f8488
不枝雀 <yunxu1019@qq.com>
2018-12-12 13:04:27 +0800

    解决在英文浏览器上不能翻译英文的问题

--- coms/zimoli/i18n.js
+++ coms/zimoli/i18n.js
@@ -20,6 +20,7 @@ var source = {
         "!": "！",
         ":": "：",
         "": {
+            "a b": [/(\W|^)\s+(\W|$)/g, "$1$2"],
             "是……的": [/是(.+?)的/, "$1"],
             "……立即": [/^(.*?)立即/, "立即$1"]
         }
@@ -81,8 +82,19 @@ var source = {
     "zh-TW": {},
     "zh-HK": {},
 };
-var navagatorLanguage = navigator.language ||/** ie~-10 */navigator.userLanguage || navigator.systemLanguage;
+var getAvailableLanguageName = function (k) {
+    if (languageMap[k]) return languageMap[k];
+    if (source[k]) return k;
+    if (/^\w{2}\-\w{2}$/.test(k)) {
+        var [a, b] = k.split("-");
+        k = [a.toLowerCase(), b.toUpperCase()].join("-");
+        if (k in source) return k;
+    }
+    if (k.toLowerCase() in source) return k;
+    throw new Error(`不支持的语言类型${k}`);
+};
 
+var navagatorLanguage = getAvailableLanguageName(navigator.language ||/** ie~-10 */navigator.userLanguage || navigator.systemLanguage || navigator.browserLanguage);
 var isAdjective = function () {
 
 }
@@ -97,7 +109,8 @@ function i18n(message, _source = source) {
         }).join("");
     }
     var translated = message.replace(/([\s\w]*)([^\s\w]|$)/g, function (match, message, quote) {
-        return (message && message.toLowerCase().split(/\s+/).map(a => _search[a] || (console.warn(`未翻译，语言：${navagatorLanguage}，信息：${a}`), a)).join("")) + (quote && _search[quote] || quote || "");
+        var result = (message && message.toLowerCase().split(/\s+/).map(a => _search[a] || (console.warn(`未翻译，语言：${navagatorLanguage}，信息：${a}`), a)).join(" ")) + (quote && _search[quote] || quote || "");
+        return result.charAt(0).toUpperCase() + result.slice(1);
     });
     for (var k in checkSpell) {
         var [reg, rep] = checkSpell[k];
@@ -118,17 +131,13 @@ i18n.new = i18n.local = function (_source) {
         });
     } else {
         for (let k in _source) {
-            let language = languageMap[k] || k;
+            let language = getAvailableLanguageName(k);
             let Base = function () {
             };
-            if (language in source) {
-                Base.prototype = source[language];
-                var _base = new Base;
-                extend(_base, _source[k]);
-                localSource[language] = _base;
-            } else {
-                throw new Error(`不支持的语言类型${k}`);
-            }
+            Base.prototype = source[language];
+            var _base = new Base;
+            extend(_base, _source[k]);
+            localSource[language] = _base;
         }
     };
     return function (a, source = localSource) {
@@ -142,22 +151,13 @@ i18n.loadSource = function (_source) {
         });
     } else {
         for (let k in _source) {
-            let language = languageMap[k] || k;
-            if (language in source) {
-                extend(source[language], _source[k]);
-            } else {
-                throw new Error(`不支持的语言类型${k}`);
-            }
+            let language = getAvailableLanguageName(k);
+            extend(source[language], _source[k]);
         }
     }
     return i18n;
 };
 i18n.setLanguage = function (language) {
-    language = languageMap[language] || language;
-    if (language in source) {
-        navagatorLanguage = language;
-    } else {
-        throw new Error(`不支持的语言类型${k}`);
-    }
+    navagatorLanguage = getAvailableLanguageName(language);
     return i18n;
 };
