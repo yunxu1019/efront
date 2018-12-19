406ac4a1514454a4cc4e0d12f62ab55aaa5d0a81
不枝雀 <yunxu1019@qq.com>
2018-12-19 14:29:25 +0800

    支持ie系列

--- coms/zimoli/swap.js
+++ coms/zimoli/swap.js
@@ -1,5 +1,10 @@
+var isIE = /edge|msie/i.test(navigator.userAgent)/*edge|ie1-10*/ || typeof document.uniqueID === "string"/* ie11 */;
 function main() {
     var block = input();
     block.type = "checkbox";
+    if (isIE) {
+        block.setAttribute("ie", isIE);
+        block.with = createElement("label");
+    }
     return block;
 }
