7677316d1a21efcf035c0c9c6813414ad3b80a7f
不枝雀 <yunxu1019@qq.com>
2018-12-31 10:31:03 +0800

    非对象类型优先比较

--- coms/zimoli/deepEqual.js
+++ coms/zimoli/deepEqual.js
@@ -1,18 +1,29 @@
+var singleEqual = function (o1, o2) {
+    return ol === o2 || isNumber(o1) && isNumber(o2) && isNaN(o1) && isNaN(o2);
+};
+var objectEqual = function (o1, o2) {
+    if (o1.constructor !== o2.constructor) return false;
+    var keys1 = Object.keys(o1), keys2 = Object.keys(o2);
+    if (keys1.length !== keys2.length) return false;
+    keys1.sort(), keys2.sort();
+    var restkeys = [];
+    for (var cx = 0, dx = keys1.length; cx < dx; cx++) {
+        var k = keys1[cx];
+        if (keys2[cx] !== k) return false;
+        if (o1[k] !== o2[k]) restkeys.push(k);
+    }
+    for (var cx = 0, dx = restkeys.length; cx < dx; cx++) {
+        if (!deepEqual(o1[k], o2[k])) return false;
+    }
+    if (o1.valueOf instanceof Function && o2.valueOf instanceof Function && o1.valueOf() !== o2.valueOf()) return false;
+    if (o1.toString instanceof Function && o2.toString instanceof Function && o1.toString() !== o2.toString()) return false;
+    return true;
+
+}
 function deepEqual(o1, o2) {
     if (o1 instanceof Object && o2 instanceof Object) {
-        if (o1.constructor !== o2.constructor) return false;
-        var keys1 = Object.keys(o1), keys2 = Object.keys(o2);
-        if (keys1.length !== keys2.length) return false;
-        keys1.sort(), keys2.sort();
-        for (var cx = 0, dx = keys1.length; cx < dx; cx++) {
-            var k = keys1[cx];
-            if (keys2[cx] !== k) return false;
-            if (!deepEqual(o1[k], o2[k])) return false;
-        }
-        if (o1.valueOf instanceof Function && o2.valueOf instanceof Function && o1.valueOf() !== o2.valueOf()) return false;
-        if (o1.toString instanceof Function && o2.toString instanceof Function && o1.toString() !== o2.toString()) return false;
-        return true;
+        return objectEqual(o1, o2);
     } else {
-        return ol === o2;
+        return singleEqual(o1, o2);
     }
 }
