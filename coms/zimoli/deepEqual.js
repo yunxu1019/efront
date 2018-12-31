1dbd4d7711dd203171e3722641de060d48592f93
不枝雀 <yunxu1019@qq.com>
2019-01-01 00:20:01 +0800

    比较深层对像的算法

--- coms/zimoli/deepEqual.js
+++ coms/zimoli/deepEqual.js
@@ -1,7 +1,30 @@
 var singleEqual = function (o1, o2) {
-    return ol === o2 || isNumber(o1) && isNumber(o2) && isNaN(o1) && isNaN(o2);
+    return o1 === o2 || isNumber(o1) && isNumber(o2) && isNaN(o1) && isNaN(o2);
 };
-var objectEqual = function (o1, o2) {
+var deepEqualMarkLabel = "__zimo1i_deep_equal_mark_label";
+var restcompare = [];
+var ALLOW_CACHE_LENGTH = SAFE_CIRCLE_DEPTH << 6;
+var ALLOW_REST_DEPTH = 512;
+var count = 0, inc = 0;
+var checkMemery = function (msg) {
+    var used = count + restcompare.length;
+    if (used > ALLOW_CACHE_LENGTH) {
+        inc += used / ALLOW_CACHE_LENGTH | 0;
+        count = used % ALLOW_CACHE_LENGTH;
+        if (inc >= ALLOW_REST_DEPTH) {
+            console.warn("对象过大，deepEqual未能完成比对");
+            return false;
+        }
+        console.warn(msg);
+    }
+    return true;
+}
+var objectEqual = function (o1, o2, deep) {
+    if (deep > SAFE_CIRCLE_DEPTH) {
+        if (!checkMemery("深层对象过多，deepEqual将消耗更多的内存")) return false;
+        restcompare.push(o1, o2);
+        return true;
+    }
     if (o1.constructor !== o2.constructor) return false;
     var keys1 = Object.keys(o1), keys2 = Object.keys(o2);
     if (keys1.length !== keys2.length) return false;
@@ -10,19 +33,59 @@ var objectEqual = function (o1, o2) {
     for (var cx = 0, dx = keys1.length; cx < dx; cx++) {
         var k = keys1[cx];
         if (keys2[cx] !== k) return false;
-        if (o1[k] !== o2[k]) restkeys.push(k);
+        var v1 = o1[k], v2 = o2[k];
+        if (v1 instanceof Object && v2 instanceof Object) {
+            if (v1 !== v2) restkeys.push(k);
+            continue;
+        }
+        if (!singleEqual(v1, v2)) return false;
     }
-    for (var cx = 0, dx = restkeys.length; cx < dx; cx++) {
-        if (!deepEqual(o1[k], o2[k])) return false;
-    }
-    if (o1.valueOf instanceof Function && o2.valueOf instanceof Function && o1.valueOf() !== o2.valueOf()) return false;
-    if (o1.toString instanceof Function && o2.toString instanceof Function && o1.toString() !== o2.toString()) return false;
-    return true;
+    if (restkeys.length && !(o1[deepEqualMarkLabel] && o2[deepEqualMarkLabel])) {
+        count += restkeys.length;
+        if (!checkMemery('对象过大，deepEqual将消耗更多的时间')) return false;
+        o1[deepEqualMarkLabel] = true;
+        o2[deepEqualMarkLabel] = true;
+        var unequal = false;
+        for (var cx = 0, dx = restkeys.length; cx < dx; cx++) {
+            var k = restkeys[cx];
+            var v1 = o1[k], v2 = o2[k];
 
-}
+            if (!objectEqual(o1[k], o2[k], deep + 1)) {
+                unequal = true;
+                break;
+            }
+        }
+        delete o1[deepEqualMarkLabel];
+        delete o2[deepEqualMarkLabel];
+        if (unequal) return false;
+    }
+    while (deep === 0 && restcompare.length) {
+        count += restcompare.length;
+        var rest = restcompare;
+        restcompare = [];
+        for (var cx = 0, dx = rest.length; cx < dx; cx += 2) {
+            var v1 = rest[cx], v2 = rest[cx + 1];
+            if (!objectEqual(v1, v2, deep + 1)) return false;
+        }
+    }
+
+    if (o1.valueOf instanceof Function && o2.valueOf instanceof Function) {
+        var v1 = o1.valueOf(), v2 = o2.valueOf();
+        if (!(v1 instanceof Object && v2 instanceof Object)) return singleEqual(v1, v2);
+    }
+    try {
+        if (deep === 0 && o1.toString instanceof Function && o2.toString instanceof Function) return o1.toString() === o2.toString();
+    } catch (e) {
+        console.warn(e);
+    }
+    return true;
+};
 function deepEqual(o1, o2) {
     if (o1 instanceof Object && o2 instanceof Object) {
-        return objectEqual(o1, o2);
+        var result = objectEqual(o1, o2, 0);
+        restcompare = [];
+        inc = 0, count = 0;
+        return result;
     } else {
         return singleEqual(o1, o2);
     }
---
+++ coms/zimoli/deepEqual_test.js
@@ -0,0 +1,96 @@
+var createDeepObject = function (deep, width, leaf = {}) {
+    if (deep <= 0) return leaf;
+    var origin = {};
+    try {
+        for (var cx = 0, dx = width; cx < dx; cx++) {
+            origin[(width * 26 + cx).toString(36)] = createDeepObject(deep - 1, width);
+        }
+    } catch (e) {
+        console.log(deep)
+    }
+    return origin;
+}
+var checkUnEqual = function () {
+    return !deepEqual({}, { a: 1 })
+        && !deepEqual(0, 1)
+        && !deepEqual(0, [])
+        && !deepEqual("", [])
+        && !deepEqual("", false)
+        && !deepEqual(0, "")
+        && !deepEqual(0, false)
+        && !deepEqual(null, 0)
+        && !deepEqual(undefined, 0)
+        && !deepEqual({}, 1)
+        && !deepEqual({}, { a: 0 })
+        && !deepEqual({ a: 1 }, { a: 0 })
+        && !deepEqual({ b: 0 }, { a: 0 })
+        && !deepEqual({ b: 0 }, { a: 0 })
+        && !deepEqual(new Date(1), new Date(2))
+        && !deepEqual(document.createElement("a"), document.createElement("b"))
+        && !deepEqual(window, document)
+        && !deepEqual(function () { }, function (a) { return a })
+        && !deepEqual({ a: {} }, { a: new Date() })
+        && !deepEqual({ a: { c: {} } }, { a: { b: {} } })
+        && !deepEqual({ a: { b: { c: { d: { 1: 2 } } } } }, { a: { b: { c: { d: {} } } } })
+        && !deepEqual([1], [0])
+        && !deepEqual([1], { 0: 1 })
+        && !deepEqual([1], new String("1"))
+        ;
+};
+var checkCircle = function () {
+    var a = {};
+    var b = {};
+    a.a = a;
+    b.a = b;
+    return deepEqual(a, b);
+};
+var checkDoubleCircle = function () {
+    a = { a: {} };
+    b = { a };
+    c = { a: {} };
+    c.a.a = c;
+    a.a.a = a;
+    // b与a出现环形的节点不一样
+    return !deepEqual(a, b) && deepEqual(a, a.a) && deepEqual(c, a);
+};
+var checkDeep = function () {
+    var a = createDeepObject(SAFE_CIRCLE_DEPTH * 8, 1);
+    var b = createDeepObject(SAFE_CIRCLE_DEPTH * 8, 1);
+    return deepEqual(a, b);
+};
+var checkDeepDepth = function () {
+    var a = [1];
+    var b = [1];
+    console.log("checkDeepDepth data start", +new Date)
+    for (var cx = 0, dx = 2 * SAFE_CIRCLE_DEPTH + 10; cx < dx; cx++) {
+        if (cx === 3) {
+            a = new Array(SAFE_CIRCLE_DEPTH << 4).fill(a);
+            b = new Array(SAFE_CIRCLE_DEPTH << 4).fill(b);
+        } else if (cx > 2 * SAFE_CIRCLE_DEPTH) {
+            a = [a, a];
+            b = [b, b];
+        } else {
+            a = [a];
+            b = [b];
+        }
+    }
+    console.log("checkDeepDepth data end", +new Date)
+    return deepEqual(a, b);
+}
+var checkDeepCircle = function () {
+    var a = createDeepObject(10, 3);
+    var b = createDeepObject(10, 3);
+    a = createDeepObject(1, 3, a);
+    b = createDeepObject(1, 3, b);
+    return deepEqual(a, b);
+};
+function deepEqual_test() {
+    console.log(
+        checkUnEqual(),
+        checkCircle(),
+        checkDoubleCircle(),
+        checkDeep(),
+        checkDeepCircle(),
+        checkDeepDepth(),
+    );
+}
