fc1d98e8327af012b53b8e9b8a8136d7a6c4e530
不枝雀 <yunxu1019@qq.com>
2019-01-03 21:56:55 +0800

    getScreeenPosition如果传入空参，则返回可视区的大小

--- coms/zimoli/getScreenPosition.js
+++ coms/zimoli/getScreenPosition.js
@@ -1,32 +1,39 @@
 function getScreenPosition(target) {
-    if (!target) return;
-    if (target.getBoundingClientRect) {
-        var rect = target.getBoundingClientRect();
-        var { left, top, width, height, right, bottom } = rect;
-        rect = { left, top, width, height, right, bottom };
-        if (rect.width === undefined) {
-            rect.width = rect.right - rect.left;
-            rect.height = rect.bottom - rect.top;
-        }
-        return rect;
-    }
-    var left = target.offsetLeft,
-        top = target.offsetTop,
-        width = target.offsetWidth,
-        height = target.offsetHeight;
-    while (target.offsetParent) {
-        target = target.offsetParent;
-        left += target.offsetLeft - target.scrollLeft + target.clientLeft;
-        top += target.offsetTop - target.scrollTop + target.clientTop;
-    }
-    var right = left + width,
-        bottom = top + height;
-    return {
-        left,
-        right,
-        bottom,
-        top,
-        width,
-        height
+    if (!target) {
+        target = {
+            offsetTop: 0,
+            offsetLeft: 0,
+            offsetWidth: +innerWidth,
+            offsetHeight: +innerHeight
+        };
     };
+    if (!target.getBoundingClientRect) {
+        var left = target.offsetLeft,
+            top = target.offsetTop,
+            width = target.offsetWidth,
+            height = target.offsetHeight;
+        while (target.offsetParent) {
+            target = target.offsetParent;
+            left += target.offsetLeft - target.scrollLeft + target.clientLeft;
+            top += target.offsetTop - target.scrollTop + target.clientTop;
+        }
+        var right = left + width,
+            bottom = top + height;
+        return {
+            left,
+            right,
+            bottom,
+            top,
+            width,
+            height
+        };
+    }
+    var rect = target.getBoundingClientRect();
+    var { left, top, width, height, right, bottom } = rect;
+    rect = { left, top, width, height, right, bottom };
+    if (rect.width === undefined) {
+        rect.width = rect.right - rect.left;
+        rect.height = rect.bottom - rect.top;
+    }
+    return rect;
 }
