078c4409d9e7384d90db16b1825011602ecd71fd
不枝雀 <yunxu1019@qq.com>
2019-01-19 01:32:30 +0800

    支持读取和设置位置信息

--- coms/zimoli/move.js
+++ coms/zimoli/move.js
@@ -1,3 +1,8 @@
+
+
+/**
+ * 让子窗口位置适应父窗口大小变化，即让两窗口锁定点的位置比例相等
+ */
 function getMarginLeft(offsetLeft, offsetWidth, innerWidth) {
     // -marginLeft/offsetWidth=(-marginLeft+offsetLeft)/innerWidth
     // marginLeft/innerWidth-marginLeft/offsetWidth=offsetLeft/innerWidth
@@ -7,17 +12,35 @@ function getMarginLeft(offsetLeft, offsetWidth, innerWidth) {
     if (offsetWidth === +innerWidth) return -offsetWidth / 2;
     return offsetLeft * offsetWidth / (offsetWidth - innerWidth);
 }
+
+function getOffsetLeft(x, offsetWidth, innerWidth) {
+    // left=x*innerWidth;
+    // marginLeft=offsetLeft-left
+    // -marginLeft/offsetWidth=(-marginLeft+offsetLeft)/innerWidth
+    // (left-offsetLeft)/offsetWidth=(left-offsetLeft+offsetLeft)/innerWidth
+    // (left-offsetLeft)/offsetWidth=left/innerWidth
+    // (left-offsetLeft)/offsetWidth=x
+    // (left-offsetLeft)=x*offsetWidth
+    // offsetLeft=left-x*offsetWidth
+    // offsetLeft=x*innerWidth-x*offsetWidth
+    // offsetLeft=x*(innerWidth-offsetWidth)
+    return x * (innerWidth - offsetWidth);
+}
 function move(offsetLeft, offsetTop, overflow = false) {
     var {
         offsetHeight,
         offsetWidth
     } = this;
+    var {
+        clientWidth = innerWidth,
+        clientHeight = innerHeight
+    } = this.offsetParent || {};
     if (!overflow) {
-        if (offsetLeft + offsetWidth > innerWidth) {
-            offsetLeft = innerWidth - offsetWidth;
+        if (offsetLeft + offsetWidth > clientWidth) {
+            offsetLeft = clientWidth - offsetWidth;
         }
-        if (offsetTop + offsetHeight > innerHeight) {
-            offsetTop = innerHeight - offsetHeight;
+        if (offsetTop + offsetHeight > clientHeight) {
+            offsetTop = clientHeight - offsetHeight;
         }
         if (offsetLeft < 0) {
             offsetLeft = 0;
@@ -26,9 +49,42 @@ function move(offsetLeft, offsetTop, overflow = false) {
             offsetTop = 0;
         }
     }
-    var marginLeft = getMarginLeft(offsetLeft, offsetWidth, innerWidth);
-    var marginTop = getMarginLeft(offsetTop, offsetHeight, innerHeight);
+    var marginLeft = getMarginLeft(offsetLeft, offsetWidth, clientWidth);
+    var marginTop = getMarginLeft(offsetTop, offsetHeight, clientHeight);
     var left = offsetLeft - marginLeft;
     var top = offsetTop - marginTop;
-    css(this, `left:${left * 100 / innerWidth}%;top:${top * 100 / innerHeight}%;margin-left:${marginLeft}px;margin-top:${marginTop}px`);
+    css(this, `left:${left * 100 / clientWidth}%;top:${top * 100 / clientHeight}%;margin-left:${marginLeft}px;margin-top:${marginTop}px`);
 }
+move.getPosition = function (target) {
+    var {
+        offsetLeft,
+        offsetTop,
+        offsetHeight,
+        offsetWidth
+    } = target;
+    var {
+        clientWidth = innerWidth,
+        clientHeight = innerHeight
+    } = this.offsetParent || {};
+    var marginLeft = getMarginLeft(offsetLeft, offsetWidth, clientWidth);
+    var marginTop = getMarginLeft(offsetTop, offsetHeight, clientHeight);
+    var left = offsetLeft - marginLeft;
+    var top = offsetTop - marginTop;
+    return [left / clientWidth, top / clientHeight];
+};
+
+move.setPosition = function (target, [x, y]) {
+    var {
+        offsetLeft,
+        offsetTop,
+        offsetHeight,
+        offsetWidth
+    } = target;
+    var {
+        clientWidth = innerWidth,
+        clientHeight = innerHeight
+    } = this.offsetParent || {};
+    var offsetLeft = getOffsetLeft(x, offsetWidth, clientWidth);
+    var offsetTop = getOffsetLeft(y, offsetHeight, clientHeight);
+    move.call(target, offsetLeft, offsetTop);
+};
