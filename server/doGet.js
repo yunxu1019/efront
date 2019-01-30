a46c8a2b07cfe75db25194dd74315a7257cc9ec7
不枝雀 <yunxu1019@qq.com>
2019-01-30 22:09:29 +0800

    解决文件名中有多个小数点时无法识别文件类型的问题

--- server/doGet.js
+++ server/doGet.js
@@ -23,7 +23,7 @@ var response = function (data, url, req, res) {
         res.writeHead(304, {});
     }
     else {
-        var extend = url.match(/\.(.*?)$/);
+        var extend = url.match(/\.([^\.]*?)$/);
         var headers = {
             "Content-Length": data.length
         };
