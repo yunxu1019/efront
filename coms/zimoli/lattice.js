21dcbe0b028db56c14a9943d16c3edcfabf1c48d
不枝雀 <yunxu1019@qq.com>
2019-02-02 14:32:20 +0800

    支持渲染元素

--- coms/zimoli/lattice.js
+++ coms/zimoli/lattice.js
@@ -6,17 +6,18 @@ onresize(window, function () {
 var mountedLattices = [];
 var complete_class = "complete";
 var inadequate_class = "lack";
-function lattice(layers, minWidth, maxWidth = minWidth << 1) {
+function lattice(element, minWidth, maxWidth = minWidth << 1, layers) {
     var boxCount;
     var resize = function () {
+        var _layers = layers || _box.src || [];
         var clientWidth = _box.clientWidth;
         boxCount = clientWidth / minWidth | 0;
-        if (boxCount >= layers.length) boxCount = layers.length, addClass(_box, complete_class);
+        if (boxCount >= _layers.length) addClass(_box, complete_class);
         else removeClass(_box, complete_class);
         if (boxCount < 1) boxCount = 1, addClass(_box, inadequate_class);
         else removeClass(_box, inadequate_class);
-    }
-    var _box = list(function (index) {
+    };
+    var _box = layers ? list(element, function (index) {
         var offset = boxCount * index;
         var objs = layers.slice(offset, offset + boxCount).map(function (a) {
             css(a, {
@@ -29,7 +30,21 @@ function lattice(layers, minWidth, maxWidth = minWidth << 1) {
         var _container = div();
         appendChild(_container, objs);
         return _container;
-    });
+    }) : list(element);
+    var insertBefore = _box.insertBefore;
+    var build = function (element) {
+        css(element, {
+            width: (1000000 / boxCount | 0) / 10000 + "%",
+            maxWidth: maxWidth * renderPixelRatio + "pt",
+        });
+        if (element.with) {
+            element.with.forEach(build);
+        }
+    };
+    _box.insertBefore = function (element) {
+        build(element);
+        return insertBefore.apply(_box, arguments);
+    };
     onappend(_box, function () {
         resize();
         mountedLattices.push(_box);
@@ -48,6 +63,31 @@ function lattice(layers, minWidth, maxWidth = minWidth << 1) {
         index = realIndex / boxCount;
         remove(_box.children);
         _box.go(index);
-    }
+    };
     return _box;
+}
+function main() {
+    var element, minWidth, maxWidth, layers;
+    [].forEach.call(arguments, function (arg) {
+        if (isNode(arg)) {
+            element = arg;
+        } else if (isArray(arg)) {
+            layers = arg;
+        } else if (isFinite(arg)) {
+            if (minWidth) {
+                if (arg >= minWidth) {
+                    maxWidth = arg;
+                } else {
+                    maxWidth = minWidth;
+                    minWidth = arg;
+                }
+            } else {
+                minWidth = arg;
+            }
+        }
+    });
+    if (isNode(element) && !minWidth) {
+        minWidth = +element.getAttribute("min-width");
+    }
+    return lattice(element, minWidth, maxWidth, layers);
 }
