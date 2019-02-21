fa57da4890ac5b57da3269655156dfafffbc3aca
不枝雀 <yunxu1019@qq.com>
2019-02-21 13:16:34 +0800

    过滤undefined数据

--- coms/zimoli/serialize.js
+++ coms/zimoli/serialize.js
@@ -2,7 +2,7 @@ function serialize(map, spliter = "&", equals = "=") {
     var result = [];
     var encode = spliter === "&" && equals === "=" ? encodeURIComponent : a => a;
     for (var k in map) {
-        result.push(encode(k) + equals + encode(map[k]));
+        if (map[k] !== undefined) result.push(encode(k) + equals + encode(map[k]));
     }
     return result.join(spliter);
 }
