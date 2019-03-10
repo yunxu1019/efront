4c6b7e50b3f1c3087caf78babef29df05a1316b3
不枝雀 <yunxu1019@qq.com>
2019-03-10 17:34:26 +0800

    从doPost方法中提出finalpacker

--- process/finalpacker.js
+++ process/finalpacker.js
@@ -1,137 +1,102 @@
-"use strict";
-var commbuilder = require("./commbuilder"),
-    pagebuilder = commbuilder;
-var cconbuilder = require("./iconbuilder");
-var indexbuilder = require("./getfile");
-var fs = require("fs");
-var path = require("path");
-var dest_folder_root = path.join(__dirname, "../public");
-var packer = module.exports = function (appname) {
-    page_packer()
-    var
-        page,
-        coms,
-        cons,
-        index,
-        colors,
-        dest;
-    page && page_packer(page);
-    coms && comm_packer(coms);
-    cons && ccon_packer(cons, colors);
-    index && index_packer(index);
-};
-//页面文件可能会有多级文件夹
-var page_packer = function (src, dst) {
-    if (!fs.existsSync(src)) return;
-    if (fs.statSync(src).isFile()) {
-        if(!/\.[jt]sx?$/i.test(src))return;
-        try {
-            var buff = pagebuilder(fs.readFileSync(src), path.basename(src), src, []);
-            dst && fs.writeFileSync(dst, buff);
-        } catch (e) {
-            console.error(`${src} =>|| ${dst} !`);
-        }
-        return;
-    }
-    if (!fs.existsSync(dst)) mkdirSync(dst);
-    fs.readdirSync(src).forEach(function (name) {
-        page_packer(path.join(src, name), dst && path.join(dst, name.replace(/\.js$/, "")));
-    });
-};
-//组件文件只有一级文件夹
-var comm_packer = function (src, dst) {
-    if (!fs.existsSync(src)) return;
-    if (fs.statSync(src).isFile()) {
-        if(!/\.[jt]sx?$/i.test(src))return;
-        try {
-            var buff = commbuilder(fs.readFileSync(src), path.basename(src), src, []);
-            if (src.match(/main\.js$/i)) {
-                buff = buff.toString().replace(/(["'`])post\1/i, '$1get$1');
+var setupenv = require("../process/setupenv");
+var env = process.env;
+var getpagefile = require("../process/cache");
+var commbuilder = require("../process/commbuilder");
+var iconbuilder = require("../process/iconbuilder");
+var aapibuilder = require("../process/aapibuilder");
+var getcommfile = require("../process/cache")(env.COMS_PATH, commbuilder).async;
+var getpagefile = require("../process/cache")(env.PAGE_PATH, commbuilder).async;
+var getaapifunc = require("../process/cache")(env.APIS_PATH, aapibuilder).async;
+var geticonfile = require("../process/cache")(env.ICON_PATH, iconbuilder).async;
+var {
+    PAGE,
+    COMM,
+    AAPI,
+    ICON
+} = env;
+var ccons_root = "./" + ICON;
+var comms_root = "./" + COMM;
+var pages_root = "./" + PAGE;
+var aapis_root = "./" + AAPI;
+var getfrompath = function (name, __root, getter, extt) {
+    __root = __root.split(/,/);
+    return new Promise(function (ok) {
+        var inc = 0;
+        var run = function () {
+            if (inc >= __root.length) return ok("");
+            var body = getter(__root[inc] + "/" + name, extt);
+            inc++;
+            if (body instanceof Promise) {
+                var error;
+                body.catch(function (e) {
+                    error = e;
+                }).then(function (body) {
+                    if (error) return ok(error);
+                    if (body instanceof Buffer || body instanceof Function) ok(body);
+                    else run();
+                });
+            } else {
+                if (body instanceof Buffer || body instanceof Function) ok(body);
+                else run();
             }
-            dst && fs.writeFileSync(path.join(dst, path.basename(src).replace(/\.js$/, "")), buff);
-        } catch (e) {
-            console.error(`${src} =>|| ${dst} !`);
-        }
-        return;
-    }
-    if (!fs.existsSync(dst)) mkdirSync(dst);
-    fs.readdirSync(src).forEach(function (name) {
-        comm_packer(path.join(src, name), dst);
-    });
+        };
+        run();
+    })
 };
-//图标//colors是数组
-var ccon_packer = function (src, dst, colors) {
-    if (!fs.existsSync(src)) return;
-    if (fs.statSync(src).isFile()) {
-        try {
-            var base = cconbuilder(fs.readFileSync(src));
-            dst && fs.writeFileSync(dst, base);
-        } catch (e) {
-            console.error(`${src} =>|| ${dst} !`);
-        }
-        colors instanceof Array && colors.forEach(function (color) {
-            try {
-                var buff = cconbuilder.color(base, color);
-                dst && fs.writeFileSync(dst + "." + color.toString(16) + ".png", buff);
-            } catch (e) {
-                console.error(`${src} =>|| ${dst} ${color.toString(16)}!`);
-            }
-        });
-        return;
-    }
-    if (!fs.existsSync(dst)) mkdirSync(dst);
-    fs.readdirSync(src).forEach(function (name) {
-        ccon_packer(path.join(src, name), dst && path.join(dst, name.replace(/\.png$/, "")), colors);
-    });
+var geticon = function (name, env) {
+    var _ccons_root = env.ICON || ccons_root;
+    return getfrompath(name, _ccons_root, geticonfile, ".png");
 };
 
-var index_packer = function (src, dst) {
-    if (!fs.existsSync(src)) return;
-    if (fs.statSync(src).isFile()) {
-        try {
-            var buff = indexbuilder(src);
-            buff = buff.toString().replace(/(["'`])post\1/i, '$1get$1');
-            dst && fs.writeFileSync(dst, buff);
-        } catch (e) {
-            console.error(`${src} =>|| ${dst}!`, e);
-        }
-        return;
-    }
-    if (!fs.existsSync(dst)) mkdirSync(dst);
-    "index.html,index.htm".split(",").forEach(function (name) {
-        index_packer(path.join(src, name), path.join(dst, name));
-    });
+var getcomm = function (name, env) {
+    var _comms_root = env.COMM || comms_root;
+    name = name.replace(/(\w)\$/g, "$1/");
+    return getfrompath(name, _comms_root, getcommfile, [".js", ".html"]);
 };
-var clear = function (dest, clear_anyway = false) {
-    if (!fs.existsSync(dest)) return console.error(`dest does't exist:${dest}`);
-    if (!clear_anyway) {
-        if (fs.statSync(dest).isFile()) return console.error(`clear failed:${dest}`);
-        if (path.relative(dest_folder_root, dest).startsWith("..")) return console.error(`can only clear driectories in ${dest_folder_root}!`);
-        return clear(dest, true);
-    }
-    if (fs.statSync(dest).isFile()) return fs.unlinkSync(dest);
-    fs.readdirSync(dest).forEach(function (name) {
-        clear(path.join(dest, name), true);
-    });
-    fs.rmdirSync(dest);
+
+var getpage = function (name, env) {
+    var _pages_root = env.PAGE || pages_root;
+    return getfrompath(name, _pages_root, getpagefile, [".js", ".html"]);
 };
-function mkdirSync(dir){
-    if(fs.existsSync(dir))return;
-    var dirs=dir.replace(/[\\\/]+$/).split(/[\\\/]+/);
-    var mked=dirs.shift();
-    while(fs.existsSync(mked)){
-        mked=path.join(mked,dirs.shift());
-    }
-    while(dirs.length){
-        fs.mkdirSync(mked);
-        mked=path.join(mked,dirs.shift());
-    }
-    fs.mkdirSync(mked);
+
+var getapi = function (name, env) {
+    var _aapis_root = env.AAPI || aapis_root;
+    return getfrompath(name, _aapis_root, getaapifunc, ".js");
+};
+
+var managers = {
+    comm: getcomm,
+    api: getapi,
+    aapi: getapi,
+    page: getpage,
+    ccon: geticon,
+    color: iconbuilder.color
 }
-Object.assign(packer, {
-    page: page_packer,
-    comm: comm_packer,
-    ccon: ccon_packer,
-    index: index_packer,
-    clear
-});
+
+var exports = module.exports = function (url, callback) {
+    var match = url.match(
+        ///// 1 //        2           //// 3 //     4      ////
+        /^\/(.*?)(comm|page|ccon|aa?pi)\/(.*?)(?:\.js|\.png)?$/
+    );
+    if (match) {
+        var appc = match[1],
+            type = match[2],
+            name = match[3],
+            extt = match[4];
+        var env = appc ? setupenv(appc) : {};
+        var manager = managers[type];
+        if (!manager) return res.end();
+        var result = manager(name, env);
+        var response = function (result) {
+            callback(result, type);
+        };
+        if (result instanceof Promise) {
+            result.then(response)
+        } else {
+            response(result);
+        }
+    } else {
+        callback();
+    }
+};
+Object.assign(exports, managers);
--- server/doPost.js
+++ server/doPost.js
@@ -1,72 +1,7 @@
 "use strict";
-var setupenv = require("../process/setupenv");
-var env = process.env;
-var getpagefile = require("../process/cache");
-var commbuilder = require("../process/commbuilder");
-var iconbuilder = require("../process/iconbuilder");
-var aapibuilder = require("../process/aapibuilder");
-var getcommfile = require("../process/cache")(env.COMS_PATH, commbuilder).async;
-var getpagefile = require("../process/cache")(env.PAGE_PATH, commbuilder).async;
-var getaapifunc = require("../process/cache")(env.APIS_PATH, aapibuilder).async;
-var geticonfile = require("../process/cache")(env.ICON_PATH, iconbuilder).async;
+var finalpacker = require("../process/finalpacker");
 var message = require("../process/message");
 var proxy = require("./proxy");
-var {
-    PAGE,
-    COMM,
-    AAPI,
-    ICON
-} = env;
-var ccons_root = "./" + ICON;
-var comms_root = "./" + COMM;
-var pages_root = "./" + PAGE;
-var aapis_root = "./" + AAPI;
-var getfrompath = function (name, __root, getter, extt) {
-    __root = __root.split(/,/);
-    return new Promise(function (ok) {
-        var inc = 0;
-        var run = function () {
-            if (inc >= __root.length) return ok("");
-            var body = getter(__root[inc] + "/" + name, extt);
-            inc++;
-            if (body instanceof Promise) {
-                var error;
-                body.catch(function (e) {
-                    error = e;
-                }).then(function (body) {
-                    if (error) return ok(error);
-                    if (body instanceof Buffer || body instanceof Function) ok(body);
-                    else run();
-                });
-            } else {
-                if (body instanceof Buffer || body instanceof Function) ok(body);
-                else run();
-            }
-        };
-        run();
-    })
-};
-var geticon = function (name, env) {
-    var _ccons_root = env.ICON || ccons_root;
-    return getfrompath(name, _ccons_root, geticonfile, ".png");
-};
-
-var getcomm = function (name, env) {
-    var _comms_root = env.COMM || comms_root;
-    name = name.replace(/(\w)\$/g, "$1/");
-    return getfrompath(name, _comms_root, getcommfile, [".js", ".html"]);
-};
-
-var getpage = function (name, env) {
-    var _pages_root = env.PAGE || pages_root;
-    return getfrompath(name, _pages_root, getpagefile, [".js", ".html"]);
-};
-
-var getapi = function (name, env) {
-    var _aapis_root = env.AAPI || aapis_root;
-    return getfrompath(name, _aapis_root, getaapifunc, ".js");
-};
-
 var readdata = function (req, res, then, max_length) {
     var buff = [],
         length = 0;
@@ -97,13 +32,6 @@ var handle = {
         }, 200000);
     }
 };
-var managers = {
-    comm: getcomm,
-    api: getapi,
-    aapi: getapi,
-    page: getpage,
-    ccon: geticon,
-}
 
 if (process.env.IN_TEST_MODE) {
     let connections = [];
@@ -126,52 +54,31 @@ var doPost = module.exports = function (req, res) {
         return handle[url](req, res);
     }
     url = proxy(req);
-    var match = url.match(
-        ///// 1 //            2            //// 3 //     4      ////
-        /^\/(.*?)(comm|page|ccon|aa?pi)\/(.*?)(?:\.js|\.png)?$/
-    );
-    if (match) {
-        var appc = match[1],
-            type = match[2],
-            name = match[3],
-            extt = match[4];
-        var env = appc ? setupenv(appc) : {};
-        var manager = managers[type];
-        if (!manager) return res.end();
-        var result = manager(name, env);
-        var response = function (result) {
-            switch (type) {
-                case "api":
-                    if (result instanceof Function) result(req, res);
-                    else res.writeHead(404, {}) | res.end();
-                    break;
-                case "aapi":
-                    if (result instanceof Function) res.end(result());
-                    else res.writeHead(404, {}) | res.end();
-                    break;
-                case "comm":
-                    res.end(String(result));
-                    break;
-                case "page":
-                    res.end(String(result));
-                    break;
-                case "ccon":
-                    res.end(String(result));
-                    break;
-                default:
-                    res.end();
-            }
-        };
-        if (result instanceof Promise) {
-            result.then(response)
-        } else {
-            response(result);
+    finalpacker(url, function (result, type) {
+        switch (type) {
+            case "api":
+                if (result instanceof Function) result(req, res);
+                else res.writeHead(404, {}) | res.end();
+                break;
+            case "aapi":
+                if (result instanceof Function) res.end(result());
+                else res.writeHead(404, {}) | res.end();
+                break;
+            case "comm":
+                res.end(String(result));
+                break;
+            case "page":
+                res.end(String(result));
+                break;
+            case "ccon":
+                res.end(String(result));
+                break;
+            default:
+                res.end();
         }
-    } else {
-        res.end();
-    }
+    });
 };
 doPost.ccon = function (name, color) {
-    var data = geticon(name);
-    return iconbuilder.color(String(data), color);
+    var data = finalpacker.ccon(name);
+    return finalpacker.color(String(data), color);
 }
