5049627662d9ba686c5014377b3a0f846616f7d3
不枝雀 <yunxu1019@qq.com>
2019-03-12 23:57:40 +0800

    支持类似于efront /reptile的命令

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 // 中文编码 utf-8
 var isTestMode = process.argv.indexOf("test") >= 0;
+var isHelpMode = process.argv.indexOf("help") >= 0;
 var isPublicMode = process.argv.indexOf("public") >= 0 || process.argv.indexOf("build") >= 0;
 var isWatchMode = process.argv.indexOf("watch") >= 0;
 var isServerMode = process.argv.indexOf("server") >= 0;
 var isInitCommand = process.argv.indexOf("init") >= 0;
-if (isTestMode) {
+var loadModule = process.argv.slice(2).filter(e => /^https?\:\/\/|\/|\.[tj]sx?$/i.test(e));
+if (isHelpMode) {
+    console.log("these commands can be used: test server public init watch");
+} else if (isTestMode) {
     require("./tester/main");
 } else if (isServerMode) {
     require("./server/index");
@@ -15,6 +19,8 @@ if (isTestMode) {
     require("./tools/create");
 } else if (isWatchMode) {
     require("./tools/watch");
+} else if (loadModule.length > 0) {
+    require("./process/efront")(loadModule[0]);
 } else {
     require("./process/setupenv");
     require("./process/console");
