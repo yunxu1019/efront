dda808f3769040f6f30b052ea48c642e9354b218
不枝雀 <yunxu1019@qq.com>
2019-03-24 00:45:28 +0800

    响应按下鼠标的状态

--- coms/zimoli/swap.js
+++ coms/zimoli/swap.js
@@ -1,6 +1,6 @@
 var isIE = /edge|msie/i.test(navigator.userAgent)/*edge|ie1-10*/ || typeof document.uniqueID === "string"/* ie11 */;
 function main() {
-    var block = input();
+    var block = document.createElement('input');
     block.type = "checkbox";
     if (isIE) {
         block.setAttribute("ie", isIE);
