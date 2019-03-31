7c89873bef14e0852fed17f8890e3e2cc2f0ef5a
不枝雀 <yunxu1019@qq.com>
2019-03-31 10:55:04 +0800

    不再使用b元素作为名称元素

--- coms/zimoli/tree.js
+++ coms/zimoli/tree.js
@@ -96,7 +96,7 @@ function tree(constructor) {
             span = constructor(com);
         } else {
             span = div();
-            html(span, `${"<t></t>".repeat(com.tab)}<b>${com.name}</b>${com.test ? "<i>_test</i>" : ""}${com.closed && com.length ? " <a>(" + com.count + ")</a>" : ""}`);
+            html(span, `${"<t></t>".repeat(com.tab)}<c>${com.name}</c>${com.test ? "<i>_test</i>" : ""}${com.closed && com.length ? " <a>(" + com.count + ")</a>" : ""}`);
         }
         var _div = button(span);
         addClass(_div, "tab" + com.tab);
