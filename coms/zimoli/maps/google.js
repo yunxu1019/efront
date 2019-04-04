91a80b10bef63acbd787991c487ed6076c04d41d
不枝雀 <yunxu1019@qq.com>
2019-04-04 22:48:10 +0800

    卫星地图接口

--- coms/zimoli/maps/google.js
+++ coms/zimoli/maps/google.js
@@ -1,3 +1,4 @@
 var google = {
-    url: `http://mt{1-3}.google.cn/vt/lyrs=h&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}`
+    url: `http://mt{1-3}.google.cn/vt/lyrs=h&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}`,
+    url: `http://mt{1-3}.google.cn/vt/lyrs=s&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}`
 };
