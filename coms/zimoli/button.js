cda85bb36f094b2870e3fc31897612cecdb7086a
不枝雀 <yunxu1019@qq.com>
2019-04-06 16:23:47 +0800

    button默认使用button元素

--- coms/zimoli/button.js
+++ coms/zimoli/button.js
@@ -3,7 +3,7 @@ var track = createElement(div);
 track.className = "track";
 _label.className = "label";
 
-var btn = div();
+var btn = document.createElement('button');
 btn.tabIndex = 0;
 var hover = function () {
     addClass(this, "hover");
