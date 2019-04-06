fa686cef0789d49356155b25359057e5735cd660
不枝雀 <yunxu1019@qq.com>
2019-04-07 01:10:50 +0800

    支持创建session级别的对象

--- coms/zimoli/data.js
+++ coms/zimoli/data.js
@@ -46,39 +46,66 @@ function encodeStructure(array) {
         body
     };
 }
+const pagePathName = location.pathname;
+const instanceDataMap = {};
 
-var cache = {};
-var encodeParams = function (params) {
-    if (!(params instanceof Object)) return params;
-};
-function main(url, params, formulator) {
-    var data = [];
-    init(url, function (response) {
-        extend(data, response);
-    });
-    return data;
+class LoadingArray extends Array {
+    totalCount = 0;
+    data = [];
+    is_errored = null;
+    error_message = null;
+    is_loading = true;
+    is_readonly = null;
+    loading_promise = null;
 }
-
-var loadingArray={
-    isLoading(){
-    }
-};
 
 var data = {
     decodeStructure,
     encodeStructure,
     /**
-     * 返回一个被延长生命周期的对象
-     * @param {} dataId 
+     * 返回一个延长生命周期的内存对象
+     * @param instanceId 数据唯一标识
+     * @param onlyFromLocalStorage 是否只从localStorage加载
      */
-    getInstauce(dataId) {
+    getInstauce(instanceId, onlyFromLocalStorage = false) {
+        if (!instanceDataMap[instanceId]) {
+            const data = instanceDataMap[instanceId] = new LoadingArray;
+            const storageId = instanceId + pagePathName;
+            Object.assign(data, JSON.parse(localStorage.getItem(storageId)));
+            if (!onlyFromLocalStorage) {
+                Object.assign(data, JSON.parse(sessionStorage.getItem(storageId)));
+            }
+            data.is_loading = false;
+        }
+        return instanceDataMap[instanceId];
 
     },
     /**
-     * 设置一个被延长生命周期的对象
-     * @param {*} dataId 
-     * @param {*} isLocalStorageEnabled 
+     * 设置一个延长生命周期的数据对象
+     * @param {*} instanceId 数据唯一标识
+     * @param {*} data 数据本体
+     * @param {boolean|number} [rememberWithStorage=0] 是否存储到localStorage，默认为否，只存储到sessionStorage
      */
-    setInstance(dataId, isLocalStorageEnabled = false) {
+    setInstance(instanceId, data, rememberWithStorage = 0) {
+        const instance = this.getInstauce(instanceId);
+        this.rebuildInstance(instance, data);
+        const storageId = instanceId + pagePathName;
+        if (rememberWithStorage !== false) {
+            sessionStorage.setItem(storageId, JSON.stringify(data));
+        }
+        if (rememberWithStorage) {
+            localStorage.setItem(storageId, JSON.stringify(data));
+        }
+        return instanceDataMap[instanceId];
+    },
+    rebuildInstance(instance, data, old = instance) {
+        if (instance === data) { return; }
+        instance.splice(0, instance.length);
+        Object.keys(old).forEach(function (k) {
+            if (instance[k] === old[k]) {
+                delete instance[k];
+            }
+        });
+        Object.assign(instance, data);
     }
 };
