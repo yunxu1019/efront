b40385e3846fbe820194f7b324ab4b00fe05634a
不枝雀 <yunxu1019@qq.com>
2019-04-07 19:40:40 +0800

    加载共享路径出错出抛出错误

--- server/checkAccess.js
+++ server/checkAccess.js
@@ -5,16 +5,21 @@ var configfilepath = "./data/shared.json";
 loadjson.async(configfilepath).then(function (json) {
     if (json instanceof Array) {
         rootDirectorys.splice(0, rootDirectorys.length);
-        rootDirectorys.push.apply(rootDirectorys, json);
+        rootDirectorys.push.apply(rootDirectorys, json.apply(function (dir) {
+            return path.normalize(dir);
+        }));
     }
-}).catch(function () {
+}).catch(function (error) {
+    console.warn('加载共享路径出错', error);
     // 未配置网络路径
 });
 var rootDirectorys = [];
 
 function checkAccess(fullpath) {
+    fullpath = path.resolve(fullpath);
     for (var cx = 0, dx = rootDirectorys.length; cx < dx; cx++) {
         var relpath = path.relative(fullpath, rootDirectorys[cx]);;
+        console.log(fullpath, relative, rootDirectorys[cx]);
         if (/^\.\.?[\/\\]|^$|/.test(relpath)) return true;
     }
     return false;
