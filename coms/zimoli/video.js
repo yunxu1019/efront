5811772f89a1bc392bb17217b9cd2965ad84a011
不枝雀 <yunxu1019@qq.com>
2019-04-07 18:05:33 +0800

    video支持render

--- coms/zimoli/video.js
+++ coms/zimoli/video.js
@@ -1,9 +1,17 @@
 function video(src) {
-    var video = createElement("video");
-    video.type = "video/" + src.replace(/^.*?\.(\w*)$/g, "$1");
-    video.src = src;
+    if (isString(src)) {
+        var video = createElement("video");
+        video.type = "video/" + src.replace(/^.*?\.(\w*)$/g, "$1");
+        video.src = src;
+    } else if (isNode(src)) {
+        if (/video/i.test(src.tagName)) {
+            var video = src;
+        } else {
+            var video = document.createElement("video");
+        }
+    }
     // video.autoplay = true;
-    on("dblclick")(video,function(){
+    on("dblclick")(video, function () {
         requestFullScreen(video);
     })
     return video;
