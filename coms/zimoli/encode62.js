615c9983f30f7eefdcddd927d88d2cbfc60da766
不枝雀 <yunxu1019@qq.com>
2019-04-07 14:06:08 +0800

    增加timeupdate方法

--- coms/zimoli/encode62.js
+++ coms/zimoli/encode62.js
@@ -10,28 +10,26 @@ var encode62 = {
         var time_rest = string.slice(string.length - time_delta.toString(36).length, string.length);
         var time_start = parseInt((new Date() - parseInt(time_rest, 36)) / time_delta) * time_delta;
         var time_stamp = time_start + parseInt(time_rest, 36);
-        return this.decode62(string.slice(0, string.length - time_delta.toString(36).length), time_stamp.toString(36));
+        return this.encode(string.slice(0, string.length - time_delta.toString(36).length), time_stamp.toString(36));
     },
     timeencode(string) {
         var { time_delta } = this;
         var time_stamp = +new Date();
         var time_rest = time_stamp % time_delta;
-        return this.encode62(string, time_stamp.toString(36)) + time_rest.toString(36).padStart(time_delta.toString(36).length, '0');
+        return this.encode(string, time_stamp.toString(36)) + time_rest.toString(36).padStart(time_delta.toString(36).length, '0');
     },
-    encode62(data, sign) {
-        if (!sign) return data;
-        var result = String(data);
-        sign = String(sign);
-        var src = this.src;
-        var result = result.replace(/\w/g, function (w, cx) {
-            var code = map[w];
-            if (!code) return w;
-            var s = code ^ (sign.charCodeAt(cx % sign.length) % src.length);
-            return src[s];
-        });
-        return result;
+    timeupdate(string) {
+        var { time_delta } = this;
+        var time_rest = string.slice(string.length - time_delta.toString(36).length, string.length);
+        var time_start = parseInt((new Date() - parseInt(time_rest, 36)) / time_delta) * time_delta;
+        var time_stamp = time_start + parseInt(time_rest, 36);
+        if (time_stamp + (time_delta >> 1) > +new Date) {
+            return string;
+        } else {
+            return this.timeencode(this.timedecode(string));
+        }
     },
-    decode62(data, sign) {
+    encode(data, sign) {
         if (!sign) return data;
         var result = String(data);
         sign = String(sign);
@@ -45,3 +43,4 @@ var encode62 = {
         return result;
     }
 };
+encode62.decode62 = encode62.encode62 = encode62.encode;
