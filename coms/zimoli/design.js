a2f8e4ed9cb0739f077edb08b73afa47de97eb09
不枝雀 <yunxu1019@qq.com>
2019-04-17 20:44:51 +0800

    字段设计工具

---
+++ coms/zimoli/design.js
@@ -0,0 +1,28 @@
+function main(fields, types) {
+    var page = form();
+    page.innerHTML = design;
+    var scope = render(page, {
+        select,
+        fields,
+        btn: button,
+        add() {
+            this.fields.push({});
+        },
+        remove(field) {
+            var fields = this.fields;
+            for (var cx = fields.length - 1; cx >= 0; cx--) {
+                if (fields[cx] === field) {
+                    fields.splice(cx, 1);
+                }
+            }
+        },
+        types
+    }).$scope;
+    autodragchildren(page, page, function (src, dst) {
+        var fields = scope.fields;
+        var field = fields.splice(src, 1)[0];
+        fields.splice(dst, 0, field);
+        console.log(JSON.stringify(scope.fields));
+    });
+    return page;
+}
