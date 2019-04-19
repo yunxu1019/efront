8744af93258535e97a7494a8424f5b19a6ae293a
不枝雀 <yunxu1019@qq.com>
2019-04-19 23:40:29 +0800

    删除无用代码

--- apps/jsc/spliter_test.js
+++
@@ -1,9 +0,0 @@
-var spliter = require("./spliter");
-var tester = require("./tester/main");
-describe("将字符串与代码分开", function () {
-    var test_code_1 = `var a="";`
-    it("查找所有双引号字符串的位置", function () {
-        var texts = spliter(test_code_1).texts;
-        filter((a, cx) => a === codes[cx].source);
-    })
-});
