8c75cc2f0326f2ec11d96f6495e21184f32e9454
不枝雀 <yunxu1019@qq.com>
2019-04-27 17:33:02 +0800

    解决有时可能无法结束的问题

--- coms/reptile/queue.js
+++ coms/reptile/queue.js
@@ -2,19 +2,31 @@ function queue(list, count = 1, context = null) {
     var f = this;
     return new Promise(function (ok) {
         var cx = 0;
+        var result = [];
+        var loaded_count = 0;
+        var error_count = 0;
         var run = function () {
-            if (cx >= list.length) return ok();
+            if (cx >= list.length && loaded_count >= list.length) return ok(result);
+            var saved_cx = cx;
             var args = list[cx++];
-            Promise.resolve(f.call(context, args)).then(run).catch(run);
+            Promise.resolve(f.call(context, args)).then(function (data) {
+                result[saved_cx] = data;
+            }).catch(function () {
+                error_count++;
+            }).then(function () {
+                loaded_count++;
+            }).then(run);
         };
+        if (count > list.length >> 1) {
+            count = list.length >> 1;
+        }
         if (!(count >= 1)) {
             count = 1;
-        }
-        if (count > list.length >> 1) {
-            count = list.length;
         }
         while (count-- > 0) {
             run();
         }
+    }).then(function (res) {
+        return res;
     });
 }
