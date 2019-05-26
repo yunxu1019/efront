45613977fc6383824b59554723dc7e7a446d53ae
不枝雀 <yunxu1019@qq.com>
2019-05-26 15:30:09 +0800

    左侧菜单调整为公共组件

--- apps/xiaohua/frame/left.js
+++ apps/xiaohua/frame/left.js
@@ -2,6 +2,11 @@ var page = div();
 page.innerHTML = left;
 render(page, {
     ylist: menu,
+    btn: button,
+    go,
+    open(event) {
+        go(event.item.href);
+    },
     menus: data.asyncInstance('menus'),
 });
 function main() {
