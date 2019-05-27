9013b3ebc1bc5fe9269f869dcf5324fc8a466dfb
不枝雀 <yunxu1019@qq.com>
2019-05-28 05:01:41 +0800

    列表启用用lattice组件

--- apps/xiaohua/photo/list.js
+++ apps/xiaohua/photo/list.js
@@ -1,15 +1,19 @@
 var page = createVboxWithState(state);
 page.innerHTML = list;
 var passport = encode62.timeencode(encode62.decode62(user._passport, user.session));
+var stylesheet = page.querySelector("style");
 var scope = render(page, {
     videos: [
     ],
+    lattice,
+    getStyles() {
+        return this.videos ? this.videos.map((e, i) => `.photo-${i}{background-image:url('${this.encode(e.href)}')}`).join("\r\n") : '';
+    },
     encode(src) {
         return "/@/data/xiaohua/photos" + src.replace(/\.?[^\.]+$/, function (m) {
             passport = encode62.timeupdate(passport);
             return "!" + passport + m;
         });
-
     },
     video(element) {
         onclick(element, e => element.play());
