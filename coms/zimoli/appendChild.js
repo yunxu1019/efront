ef518a3f17fb8e00924c57ebf6ebadd1f6684c9a
不枝雀 <yunxu1019@qq.com>
2019-06-03 00:48:40 +0800

    不再移除附加元素的动画

--- coms/zimoli/appendChild.js
+++ coms/zimoli/appendChild.js
@@ -43,7 +43,7 @@ function appendChild(parent, obj, transition) {
                 isFunction(appendChild.transition) && appendChild.transition(o, o.initialStyle);
             }
             _appendChild.call(parent, o);
-            o.with && appendChild(parent, o.with, false);
+            o.with && appendChild(parent, o.with, transition);
             if (parent.isMounted)
                 _onappend(o);
         }
@@ -62,7 +62,7 @@ function insertBefore(alreadyMounted, obj, transition) {
         var o = release(children[cx]);
         if (o.removeTimer) clearTimeout(o.removeTimer);
         _insertBefore.call(parent, o, alreadyMounted);
-        o.with && insertBefore(alreadyMounted, o.with, false);
+        o.with && insertBefore(alreadyMounted, o.with, transition);
         if (parent.isMounted)
             _onappend(o);
         if (o.initialStyle && transition !== false) {
@@ -83,7 +83,7 @@ function insertAfter(alreadyMounted, obj, transition) {
         var o = release(children[cx]);
         if (o.removeTimer) clearTimeout(o.removeTimer);
         _insertBefore.call(parent, o, alreadyMounted.nextSibling);
-        o.with && insertBefore(alreadyMounted.nextSibling, o.with, false);
+        o.with && insertBefore(alreadyMounted.nextSibling, o.with, transition);
         if (parent.isMounted)
             _onappend(o);
         if (o.initialStyle && transition !== false) {
