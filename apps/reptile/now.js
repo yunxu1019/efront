b9064edc9f6e9c9d30836d41e69e23efe1473662
不枝雀 <yunxu1019@qq.com>
2019-07-07 00:01:19 +0800

    v1.7.3

--- apps/reptile/now.js
+++ apps/reptile/now.js
@@ -1,27 +1,27 @@
-var folder = `https://1251953721.vod2.myqcloud.com/0ec02e46vodcq1251953721/7c4525705285890791033559507/`;
-var m3u8 = folder + `playlist.m3u8`;
-function index(a, total) {
-    a = String(a);
-    return String(total).replace(/\d/g, function (m, index, input) {
-        if (index + a.length < input.length) return '0';
-        return a[index + a.length - input.length];
+function main(url) {
+    var electron = require("electron");
+    var { BrowserWindow } = electron.BrowserWindow ? electron : electron.remote;
+    var window = new BrowserWindow({
+        width: 800, height: 800, show: false,
+        webPreferences: {
+            // 支持node
+            nodeIntegration: true,
+            //缩放级别
+            zoomFactor: 1.0,
+            //使用原生的window.open
+            nativeWindowOpen: true,
+            //允许跨域资源
+            webwebSecurity: true,
+        }
     });
+    window.showInactive();
+    window.on('closed', () => {
+        window = null
+    });
+    window.setMenu(null);
+    // 加载远程URL
+    // window.webContents.openDevTools();
+    window.setAlwaysOnTop(true);
+    window.loadURL("http://efront.cc/zimoli/");
+    // window.setPosition(innerWidth, 0);
 }
-fetch(m3u8).then(function (a) {
-    var reg = /^(.*?)\?start=(\d+)&end=.*?$/;
-    var downloaded = {};
-    var list = a.toString().split(/[\r\n]+/).filter(e => e && !/^#/.test(e)).map(function (name) {
-        var match = reg.exec(name);
-        if (!match) return;
-        var file = match[1];
-        if (downloaded[file]) return;
-        downloaded[file] = true;
-        return file;
-    }).filter(a => !!a);
-    return queue.call(list, function (file, cx) {
-        console.info(file);
-        return fetch(folder + file).then(function (buff) {
-            return fs('/data/' + index(cx, list.length) + '.' + file).writeSync(buff);
-        });
-    });
-});
