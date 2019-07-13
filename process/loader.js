2befada710ea92fd9e47b405bec284498ad682a1
不枝雀 <yunxu1019@qq.com>
2019-07-13 18:26:13 +0800

    解决iPhoneXS上1px线不显示的问题

--- process/loader.js
+++ process/loader.js
@@ -74,7 +74,7 @@ var FILE_NAME_REG = /^[^\/].*?[\/\?\-\.\+]|(?:[^\/]\.|[\-\+\?])[^\/]*?$/;
 var devicePixelRatio = window.devicePixelRatio || 1;
 // if (isBadDevice) devicePixelRatio = 1;
 var renderPixelRatio = !/win/i.test(navigator.platform) && devicePixelRatio > 1 && window.innerWidth > 360 && window.innerHeight > 360 ? .86 : .75;
-if (document.querySelector && devicePixelRatio > 1 && /Linux/.test(navigator.platform) && navigator.maxTouchPoints > 0) {
+if (document.querySelector && devicePixelRatio > 1 && /iPhone|Linux/.test(navigator.platform) && navigator.maxTouchPoints > 0) {
     let ratio = +(1000000 / devicePixelRatio + .5 | 0) / 1000000;
     document.querySelector("meta[name=viewport]").setAttribute("content", `width=device-width,target-densitydpi=device-dpi,user-scalable=no,initial-scale=${ratio},maximum-scale=${ratio}`);
     renderPixelRatio *= devicePixelRatio;
