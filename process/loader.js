310b3328ee44bee05fa60bed16955fd4df2fbbc4
不枝雀 <yunxu1019@qq.com>
2019-07-15 01:09:05 +0800

    引导代码禁用6秒以上的缓存

--- process/loader.js
+++ process/loader.js
@@ -52,6 +52,29 @@ if (PREVENT_FRAMEWORK_MODE !== false) {
         throw message;
     }
 }
+var start_time = +new Date / 1000 | 0;
+-function () {
+    var page_time = location.search.replace(/^.*?[\?&]\=([^&]+).*?$/i, "$1");
+    page_time = parseInt(page_time, 36) || 0;
+    var mode_time = 315360000;
+    var mark_time = start_time % mode_time;
+    var delta_time = 6;
+
+    if (mark_time - page_time > delta_time
+        || mark_time + mode_time > page_time
+        && mark_time < delta_time
+        && page_time + delta_time > mode_time) {
+        var search = location.search.split(/([\?&])\=[^&]+&?/i).join("&").replace(/^[\?&]+|[\?&]+$/g, "");
+        if (search) {
+            search += "&=" + mark_time.toString(36);
+        } else {
+            search = "=" + mark_time.toString(36);
+        }
+        location.replace("?" + search);
+        return;
+    }
+}();
+
 // 检查性能
 var isBadDevice;
 {
@@ -480,7 +503,7 @@ var initIfNotDefined = function (defined, path, onload) {
     else hook(--requires_count);
 }
 var modules = {
-    start_time: +new Date,
+    start_time,
     IS_BAD_DEVICE: isBadDevice,
     MOVELOCK_DELTA: 3 * renderPixelRatio,
     SAFE_CIRCLE_DEPTH,
