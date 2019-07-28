a02014d535e0cbdeb76b771b14cce1759d6e0b54
不枝雀 <yunxu1019@qq.com>
2019-07-28 10:57:11 +0800

    script自动加载

--- coms/zimoli/script.js
+++ coms/zimoli/script.js
@@ -1,5 +1,40 @@
-function main(src) {
+function script(url, judger) {
     var script = document.createElement("script");
-    script.src = src;
+    var ok = oh = function () { };
+    var needcallback = !!judger;
+    var clear = function () {
+        remove(script);
+    };
+    var onload = function () {
+        clear();
+        if (!needcallback || needcallback > 1000) return;
+        var res = seek(window, judger);
+        if (res !== undefined) return ok(res);
+        requestAnimationFrame(onload);
+        needcallback++;
+    };
+    var onerror = function (e) {
+        clear();
+        if (!needcallback || needcallback > 1000) return;
+        oh(e);
+    };
+    on('load')(script, onload);
+    on('error')(script, onerror);
+    script.onabort = function () {
+        clear();
+        needcallback = false;
+    };
+    script.done = script.then = function (f) {
+        if (f instanceof Function) ok = f;
+    };
+    script.error = script.catch = function (f) {
+        if (f instanceof Function) oh = f;
+    };
+    script.src = url;
+    setTimeout(function () {
+        if (!script.parentNode) {
+            appendChild(document.body, script);
+        }
+    });
     return script;
 }
