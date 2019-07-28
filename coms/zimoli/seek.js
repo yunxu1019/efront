a427bbbf46413a91e77b08cb44354f4d8b1f4e31
不枝雀 <yunxu1019@qq.com>
2019-07-28 10:59:12 +0800

    对象重建工具

---
+++ coms/zimoli/seek.js
@@ -0,0 +1,31 @@
+var seek = function (object, seeker) {
+    if (seeker === null || seeker === undefined || !object) return;
+    if (seeker instanceof Function) {
+        return seeker(object);
+    }
+    if (typeof seeker === "string") {
+        seeker = seeker.split(".");
+    }
+    if (seeker instanceof Array) {
+        if (!seeker.length) return;
+        for (var cx = 0, dx = seeker.length; cx < dx; cx++) {
+            var key = seeker[cx];
+            if (typeof key === "string") {
+                object = object[key];
+            } else {
+                object = seek(object, key);
+            }
+            if (object === undefined || object === null) return;
+        }
+        return object;
+    }
+    if (seeker instanceof Object) {
+        var dist = {};
+        for (var k in seeker) {
+            var o = seek(object, seeker[o]);
+            if (o === undefined) return;
+            dist[k] = o;
+        }
+        return dist;
+    }
+};
