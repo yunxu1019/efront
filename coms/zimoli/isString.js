b6d65842bbd5b424b88901d43b33b2442855e08c
不枝雀 <yunxu1019@qq.com>
2019-08-22 21:31:53 +0800

    修改isString的判断方法

--- coms/zimoli/isString.js
+++ coms/zimoli/isString.js
@@ -3,9 +3,6 @@
  * 2017-3-18 13:50:45
  */
 
-if (!"".trim) String.prototype.trim = function () {
-    return this.replace(/^\s+|\s+$/, "");
-};
 var isString = function isString(o) {
-    return typeof o === "string";
+    return typeof o === "string" || o instanceof String;
 }
