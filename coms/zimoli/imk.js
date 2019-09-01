a49d752d78ac03ef028854d4b521b0bca0b76270
不枝雀 <yunxu1019@qq.com>
2019-09-01 21:18:41 +0800

    小红点

---
+++ coms/zimoli/imk.js
@@ -0,0 +1,29 @@
+var page = div();
+var follow = function () {
+};
+on("mouseenter")(page, function () {
+    var r = (page.offsetWidth + page.offsetHeight) / 4;
+    page.style.transition = "none";
+
+    var offmousemove = on("mousemove")(window, function (event) {
+        var { layerX: x, layerY: y } = event;
+        var dx = x - r;
+        var dy = y - r;
+        page.style.borderRadius = [-dx * r - dy * r, dx * r + dy * r, dx * r + dy * r, -dx * r + dy * r]
+            .map(a => Math.abs(r - a / r).toFixed(2)).map(a => Math.max(a, 2) + "px").join(" ")
+    });
+    var offmouseleave = on('mouseleave')(page, function (event) {
+        page.style.transition = "";
+        page.style.borderRadius = "";
+        offmousemove();
+        offmouseleave();
+    });
+});
+
+function main() {
+    page.innerHTML = imk;
+    document.body.appendChild(page);
+    drag.on(page);
+
+    return page;
+}
