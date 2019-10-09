8bfad432f623ec7a84852d4040fd1f3c0576d4a3
不枝雀 <yunxu1019@qq.com>
2019-10-10 01:19:42 +0800

    整合fullscreen功能

---
+++ coms/zimoli/fullscreen.js
@@ -0,0 +1,24 @@
+var fullscreen = {
+    is() {
+        if (/chrome/i.test(navigator.userAgent)) {//webkit
+            return window.innerHeight === screen.height && window.innerWidth === screen.width;
+        } else {//IE 9+  fireFox
+            return window.outerHeight === screen.height && window.outerWidth === screen.width;
+        }
+    },
+    hasTarget() {
+        return !!(document.fullscreenElement || document.msFullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement);
+    },
+    exec(element) {
+        requestFullScreen(element);
+    },
+    exit(alert) {
+        if (this.hasTarget()) cancelFullScreen();
+        else
+            var error = "按 F11 退出全屏";
+        if (isFunction(alert) && error) alert(error);
+        return error;
+    }
+};
+fullscreen.request = fullscreen.requestFullscreen = fullscreen.exec;
+fullscreen.cancel = fullscreen.exitFullscreen = fullscreen.exit;
