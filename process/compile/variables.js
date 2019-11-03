c7d3813dbdc724555ff26c11f01d42d3f512d54e
不枝雀 <yunxu1019@qq.com>
2019-11-03 23:01:05 +0800

    添加两个数字常量

--- process/compile/variables.js
+++ process/compile/variables.js
@@ -2,7 +2,7 @@
 /**
  * 读取全局变量
  */
-var keyword = `break do in typeof case else instanceof var catch export new void class extends return while const finally super with continue for switch yield debugger function this default if throw delete import try enum await null true false arguments`;
+var keyword = `NaN Infinity break do in typeof case else instanceof var catch export new void class extends return while const finally super with continue for switch yield debugger function this default if throw delete import try enum await null true false arguments`;
 var keywords = {};
 keyword.split(/\s+/g).forEach(function (key) {
     keywords[key] = true;
