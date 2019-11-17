9357d24c7d01cc1e3fa9bcfb12026f6be358b0ec
不枝雀 <yunxu1019@qq.com>
2019-11-17 12:19:36 +0800

    支持只读框和编辑框

--- coms/zimoli/model.js
+++ coms/zimoli/model.js
@@ -24,6 +24,15 @@ var constructors = {
         var elem = input();
         return elem;
     },
+    text(field, data) {
+        var ipt = input();
+        ipt.setAttribute("ng-model", "data[field.key]");
+        render(ipt, {
+            field,
+            data
+        });
+        return ipt;
+    },
     'checkbox'() {
         var elem = input();
         elem.setAttribute("type", "checkbox");
@@ -40,12 +49,66 @@ var constructors = {
     "range"() {
         var elem = input();
         return elem;
+    },
+    "none"() {
+
     }
 };
-function main(data, field) {
-    var constructor = constructors[field.type || field.editor] || constructors.input;
-    var element = constructor(field);
-    element.setAttribute("model", field.key);
-    render(element, data);
-    return element;
+var readonly_types = {
+    "date"() {
+
+    },
+    "size"(field, data) {
+        var f = data[field.key];
+        var log = Math.log2(f) / 10 | 0;
+        f /= Math.pow(2, log * 10);
+        f = +f.toFixed(2);
+        return f + "KMGT".charAt(log - 1) + "B"
+    }
+}
+function main(elem) {
+    var build = function () {
+        var { data, readonly, field } = elem;
+        if (!field || !data) return;
+        var run = function () {
+            if (data !== elem.data || field !== elem.field || readonly !== elem.readonly) return;
+            if (readonly) {
+                elem.innerHTML = '<span ng-bind=get()></span>';
+                var type = elem.getAttribute('type');
+                if (type !== field.type) {
+                    elem.setAttribute("type", field.type);
+                }
+                render(elem, {
+                    get() {
+                        var type = field.type || field.editor;
+                        var editor = readonly_types[type];
+                        if (editor) {
+                            return editor(field, data);
+                        }
+                        return data[field.key];
+                    }
+                });
+            } else {
+                var editor = constructors[field.type];
+                var input = editor ? editor(field, data) : input();
+                remove(elem.children);
+                if (input) appendChild(elem, input);
+            }
+        };
+        if (data.loading_promise || field.loading_promise) {
+            Promise.all([
+                data.loading_promise,
+                field.loading_promise
+            ]).then(run);
+        } else {
+            run();
+        }
+
+    }
+    on("changes")(elem, function ({ changes }) {
+        if (changes.data || changes.field || changes.readonly) {
+            build();
+        }
+    });
+    return elem;
 }
