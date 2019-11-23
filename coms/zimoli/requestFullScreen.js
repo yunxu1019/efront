10d4aba3d23ffc22970a276dfd519646b3ce9150
不枝雀 <yunxu1019@qq.com>
2019-11-23 10:54:29 +0800

    支持低版本ie

--- coms/zimoli/requestFullScreen.js
+++ coms/zimoli/requestFullScreen.js
@@ -2,5 +2,5 @@ var requestFullScreen = function (doc) {
     if (!doc) {
         doc = document.documentElement;
     }
-    (doc.requestFullscreen || doc.mozRequestFullScreen || doc.webkitRequestFullScreen || noop).call(doc);
+    (doc.requestFullscreen || doc.msRequestFullscreen || doc.mozRequestFullScreen || doc.webkitRequestFullScreen || noop).call(doc);
 }
