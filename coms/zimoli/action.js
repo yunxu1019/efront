10ab65f3faf827dc042efbda858d484912447cc3
不枝雀 <yunxu1019@qq.com>
2019-12-10 23:41:22 +0800

    防止config中的item被置空

--- coms/zimoli/action.js
+++ coms/zimoli/action.js
@@ -2,13 +2,17 @@ function main(config, item) {
     return new Promise(function (ok, oh) {
         if (!config) return ok();
         if (config instanceof Object) {
+            if (config.do instanceof Function) {
+                config.do();
+                return;
+            }
             if (config.modal) {
                 var path = isString(config.modal) ? config.modal : config.modal.path;
                 if (config.params) {
                     var params = getParams(item, config.params);
                 }
                 var args = extend({}, config.modal instanceof Object ? config.modal : { path: config.modal },
-                    config.params ? { params } : { item });
+                    config.params ? { params } : {}, item ? { item } : {});
                 init("zimoli", function (zimoli) {
                     zimoli.prepare(path, function () {
                         var page = popup(path, args);
