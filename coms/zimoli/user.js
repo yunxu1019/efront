aeb4be27e65e6e7c5239994c7d9138623af7c6fe
不枝雀 <yunxu1019@qq.com>
2020-02-10 00:43:37 +0800

    修改一些异步方法为同步方法

--- coms/zimoli/user.js
+++ coms/zimoli/user.js
@@ -8,65 +8,78 @@ var emptyProto = {
     isLogin: false,
     roles: null
 };
+var passport;
 var loadUserData;
 var proto = {
     targetUrl: null,
     loginPath: null,
     session_time: 300000,
     session: "",
-    setStateFunction(stateFunc) {
-        if (!isFunction(stateFunc)) throw new Error("只能传入函数类型的参数");
-    },
-    loadSession() {
-        var object = userInstance;
-        if (object.session && !(new Date() > object.session_time)) {
-            proto.session_time = object.session_time;
-            proto.session = object.session;
-            this.Login(object);
-        }
-        return object.session || "";
-    },
-    setSessionTime(session_time) {
-        proto.session_time = session_time;
-    },
-    saveSession(session = this.session) {
-        this.session = session;
-        data.setInstance(USERINFO, {
-            roles: proto.roles,
-            name: proto.name,
-            _id: proto._id,
-            session: session,
-            _passport: this._passport,
-            isLogin: !!session,
-            session_time: +new Date() + proto.session_time
-        });
-    },
+
     Login(userinfo) {
-        proto.roles = userinfo.roles || [];
-        proto.isLogin = true;
-        proto.name = userinfo.name;
-        proto._id = "org.couchdb.user:" + userinfo.name;
-        user.clean && user.clean(proto.loginPath);
-        this.refresh();
-    },
-    setUserDataLoader(method) {
-        loadUserData = method;
-    },
-    refresh() {
-        loadUserData instanceof Function && loadUserData();
+        this.login(userinfo);
     },
     Logout() {
-        delete user.isLogin;
-        delete user.roles;
-        delete user._passport;
-        proto._passport = "";
-        proto.roles = null;
-        proto.isLogin = false;
-        this.saveSession("");
-    },
-    setLoginPath(pathname) {
-        proto.loginPath = pathname;
+        this.logout();
     }
 };
 extend(proto, emptyProto, userInstance);
 var user = enrich(proto);
+user.setLoginPath = function (pathname) {
+    proto.loginPath = pathname;
+};
+user.setUserDataLoader = function (method) {
+    loadUserData = method;
+};
+user.setSessionTime = function (session_time) {
+    proto.session_time = session_time;
+};
+user.refresh = function () {
+    loadUserData instanceof Function && loadUserData();
+};
+
+user.getPassport = function () {
+    if (!passport) {
+        passport = encode62.timeencode(encode62.decode62(user._passport, user.session));
+    }
+    return passport = encode62.timeupdate(passport);
+};
+user.login = function (userinfo) {
+    proto.roles = userinfo.roles || [];
+    proto.isLogin = true;
+    proto.name = userinfo.name;
+    proto._id = "org.couchdb.user:" + userinfo.name;
+    user.clean && user.clean(proto.loginPath);
+    this.refresh();
+};
+user.loadSession = function () {
+    var object = userInstance;
+    if (object.session && !(new Date() > object.session_time)) {
+        proto.session_time = object.session_time;
+        proto.session = object.session;
+        this.Login(object);
+    }
+    return object.session || "";
+};
+user.saveSession = function (session = this.session) {
+    this.session = session;
+    data.setInstance(USERINFO, {
+        roles: proto.roles,
+        name: proto.name,
+        _id: proto._id,
+        session: session,
+        _passport: this._passport,
+        isLogin: !!session,
+        session_time: +new Date() + proto.session_time
+    });
+};
+user.logout = function () {
+    delete user.isLogin;
+    delete user.roles;
+    delete user._passport;
+    proto._passport = "";
+    proto.roles = null;
+    proto.isLogin = false;
+    passport = '';
+    this.saveSession("");
+};
