d110619431dc4e001854a2c48ad7a6004d6a1ef7
不枝雀 <yunxu1019@qq.com>
2020-02-12 02:43:28 +0800

    还原loadSession和saveSession

--- coms/zimoli/user.js
+++ coms/zimoli/user.js
@@ -15,7 +15,27 @@ var proto = {
     loginPath: null,
     session_time: 300000,
     session: "",
-
+    loadSession: function () {
+        var object = userInstance;
+        if (object.session && !(new Date() > object.session_time)) {
+            proto.session_time = object.session_time;
+            proto.session = object.session;
+            this.Login(object);
+        }
+        return object.session || "";
+    },
+    saveSession: function (session = this.session) {
+        this.session = session;
+        data.setInstance(USERINFO, {
+            roles: proto.roles,
+            name: proto.name,
+            _id: proto._id,
+            session: session,
+            _passport: this._passport,
+            isLogin: !!session,
+            session_time: +new Date() + proto.session_time
+        });
+    },
     Login(userinfo) {
         this.login(userinfo);
     },
@@ -51,27 +71,6 @@ user.login = function (userinfo) {
     proto._id = "org.couchdb.user:" + userinfo.name;
     user.clean && user.clean(proto.loginPath);
     this.refresh();
-};
-user.loadSession = function () {
-    var object = userInstance;
-    if (object.session && !(new Date() > object.session_time)) {
-        proto.session_time = object.session_time;
-        proto.session = object.session;
-        this.Login(object);
-    }
-    return object.session || "";
-};
-user.saveSession = function (session = this.session) {
-    this.session = session;
-    data.setInstance(USERINFO, {
-        roles: proto.roles,
-        name: proto.name,
-        _id: proto._id,
-        session: session,
-        _passport: this._passport,
-        isLogin: !!session,
-        session_time: +new Date() + proto.session_time
-    });
 };
 user.logout = function () {
     delete user.isLogin;
