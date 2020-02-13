a83d8d59cb0af509175717e0168eaa115a0fa7f5
不枝雀 <yunxu1019@qq.com>
2020-02-13 23:57:57 +0800

    转移parseKV.js

---
+++ process/parseKV.js
@@ -0,0 +1,16 @@
+function parseKV(string, spliter = "&", equals = "=") {
+    var object = {};
+    var decode = spliter === "&" && equals === "=" ? decodeURIComponent : a => a;
+    if (typeof string === "string") {
+        var kvs = string.split(spliter);
+        for (var cx = 0, dx = kvs.length; cx < dx; cx++) {
+            var kv = kvs[cx];
+            if (!kv) continue;
+            var index = kv.indexOf(equals);
+            if (index < 0) index = kv.length;
+            object[decode(kv.slice(0, index))] = index === kv.length ? undefined : decode(kv.slice(index + 1));
+        }
+    }
+    return object;
+}
+module.exports = parseKV;
