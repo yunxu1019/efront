ce2191ff612187e4a495236b5ad11d6c289ee996
不枝雀 <yunxu1019@qq.com>
2020-03-04 01:14:26 +0800

    添加一种异步调用的方式

---
+++ coms/zimoli/care.js
@@ -0,0 +1,61 @@
+/**
+ * 接收对象参数
+ * ie7 及以下不受支持
+ * @param {Object|Function} target 
+ * @param {Function|string} type 
+ * @param {Function} listener 
+ */
+function care(target, type, listener) {
+    var [target, type, listener] = parse.apply(this, arguments);
+    if (!target[type]) {
+        target[type] = [];
+    }
+    if (!target[type]) {
+        target[type] = [];
+    }
+    var listeners = target[type];
+    if (listener instanceof Function && !~listeners.indexOf(listener)) {
+        if (listeners.length > 600) throw new Error("请不要在同一个对象上使用过多的同类型的care!");
+        listeners.push(listener);
+    }
+}
+function parse(target, type, listener) {
+    switch (arguments.length) {
+        case 2:
+            listener = type;
+            type = "default";
+            break;
+        case 3:
+            if (type instanceof Function) {
+                var temp = type;
+                type = listener;
+                listener = temp;
+            }
+            break;
+        case 1:
+            target = this;
+            if (target instanceof Function) {
+                listener = target;
+                type = "default";
+            }
+            break;
+        default:
+            throw new Error("参数数量不正确");
+    }
+    if (!(target instanceof Object)) throw new Error("care只能使用在对象上！");
+    type = `care(${type})`;
+
+    return [target, type, listener];
+}
+function clean(target, type, listener) {
+    var [target, type, listener] = parse.apply(this, arguments);
+    var listeners = target[type];
+    if (listener instanceof Function) {
+        for (var cx = listeners.length - 1; cx >= 0; cx--) {
+            if (listeners[cx] === listener) listeners.splice(cx, 1);
+        }
+    } else if (listeners) {
+        listeners.splice(0, listeners.length);
+    }
+}
+care.clean = clean;
---
+++ coms/zimoli/cast.js
@@ -0,0 +1,37 @@
+function cast(target, type, data) {
+    switch (arguments.length) {
+        case 2:
+            data = type;
+            type = "default";
+            break;
+        case 3:
+            if (!isString(type) && isString(data)) {
+                var temp = type;
+                type = data;
+                data = temp;
+            }
+            break;
+        case 1:
+            data = target;
+            target = this;
+            type = "default";
+            break;
+        default:
+            throw new Error("参数数量不正确");
+    }
+    if (!(target instanceof Object)) throw new Error("cast只能使用在对象上！");
+
+    type = `care(${type})`;
+    var datakey = `cast(${type})`;
+    if (target[type] instanceof Array) {
+        target[type].forEach(function (listener) {
+            if (listener instanceof Function) {
+                listener.call(target, data, target[datakey]);
+            }
+        });
+        target[datakey] = data;
+        if (listener instanceof Function) {
+            target[type].push(listener);
+        }
+    }
+}
