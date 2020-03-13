d4e3b3a289f594b47bf2daabca2d8ef889d4a383
不枝雀 <yunxu1019@qq.com>
2020-03-14 06:41:12 +0800

    支持删除播放列表

--- coms/kugou/playList.js
+++ coms/kugou/playList.js
@@ -11,10 +11,16 @@ function main() {
         },
         song: kugou$song,
         padding,
+        remove(i) {
+            var list = kugou$musicList.slice(0);
+            list.splice(i, 1);
+            data.setInstance("musicList", list, true);
+        },
         play(i) {
             modules.kugou$player.play(i);
         },
         musicList: kugou$musicList,
     });
+    touchList(page.querySelector("list"));
     return page;
 }
