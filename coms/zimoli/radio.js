cf2c543f416f75727644d40c9c9f7994d9de95ea
不枝雀 <yunxu1019@qq.com>
2020-03-18 08:25:38 +0800

    radio

---
+++ coms/zimoli/radio.js
@@ -0,0 +1,28 @@
+function main(elem = document.createElement("radio-group")) {
+    care(elem, function (field) {
+        console.log(field, 'field');
+        elem.innerHTML = radio;
+        var { options } = field;
+        if (options instanceof Array) {
+            options = options.map((a, i) => {
+                if (a instanceof Object) return a;
+                return { key: i, name: a };
+            });
+        } else if (options instanceof Object) {
+            options = Object.keys(field.options).map(k => ({ name: options[k], key: k }));
+        } else {
+            options = null;
+        }
+        render(elem, {
+            a: button,
+            field,
+            options,
+            select(a) {
+                this.options.active = a;
+                elem.value = a.key;
+                dispatch(elem, 'change');
+            }
+        });
+    });
+    return elem;
+}
