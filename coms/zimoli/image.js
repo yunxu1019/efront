76c02995a835665329452f9845f301de2b45b0f5
不枝雀 <yunxu1019@qq.com>
2020-03-18 10:15:04 +0800

    图片选择器

---
+++ coms/zimoli/image.js
@@ -0,0 +1,20 @@
+var { URL } = window;
+function main(elem = div()) {
+    elem.innerHTML = image;
+    render(elem, {
+        hasInstance: false,
+        choose() {
+            chooseFile('image/*').then((file) => {
+                var f = file[0];
+                if (URL) {
+                    var url = URL.createObjectURL(f);
+                    css(elem, {
+                        backgroundImage: `url('${url}')`
+                    });
+                }
+                this.hasInstance = true;
+            });
+        }
+    });
+    return elem;
+}
