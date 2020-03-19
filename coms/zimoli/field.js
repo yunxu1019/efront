356a8da224359558ee5268d3069c2fdf7f10eef3
不枝雀 <yunxu1019@qq.com>
2020-03-19 22:48:11 +0800

    支持不使用内部元素

--- coms/zimoli/field.js
+++ coms/zimoli/field.js
@@ -1,5 +1,16 @@
-function field() {
+function main() {
     var result = option.apply(null, arguments);
+    care(result, function (p) {
+        var [f, data] = p;
+        result.innerHTML = field;
+        console.log(f);
+        render(result, {
+            model,
+            data,
+            field: f
+        });
+    });
+    console.log(result);
     result.removeAttribute("tabindex");
     return result;
 }
