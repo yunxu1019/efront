dae14f1c23cc1b5c43b83a3256a530de86ee0fef
不枝雀 <yunxu1019@qq.com>
2020-03-20 05:39:39 +0800

    删除log

--- coms/zimoli/field.js
+++ coms/zimoli/field.js
@@ -3,14 +3,12 @@ function main() {
     care(result, function (p) {
         var [f, data] = p;
         result.innerHTML = field;
-        console.log(f);
         render(result, {
             model,
             data,
             field: f
         });
     });
-    console.log(result);
     result.removeAttribute("tabindex");
     return result;
 }
