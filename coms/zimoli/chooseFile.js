542616d2aec80e1754200e3182426adadc816bd1
不枝雀 <yunxu1019@qq.com>
2020-03-22 19:34:40 +0800

    增加一些兼容代码

--- coms/zimoli/chooseFile.js
+++ coms/zimoli/chooseFile.js
@@ -2,11 +2,29 @@ function chooseFile(accept, multiple) {
     var form = document.createElement("form");
     form.innerHTML = `<input type='file'${accept ? ` accept="${accept}"` : ''}${multiple ? ' multiple' : ''} />`;
     var [input] = form.children;
-    var result = new Promise(function (ok, oh) {
+    var result = new Promise(function (ok) {
+        if (/msie\s+[2-9]/i.test(navigator.userAgent)) return alert("无法在当前浏览器操作！");
         on("change")(input, function () {
-            setTimeout(() => {
-                ok(this.files);
-            }, 100);
+            if (!this.files) {
+                var value = this.value;
+                if (!value) return;
+                var ActiveXObject = window.ActiveXObject;
+                if (!ActiveXObject) {
+                    alert("您的浏览器环境无法选择文件！");
+                    return;
+                }
+                try {
+                    var fso = new ActiveXObject('Scripting.FileSystemObject');
+                    var file = fso.GetFile(value);
+                    ok([file]);
+                } catch (e) {
+                    alert(e + "打开文件失败！");
+                }
+            } else {
+                setTimeout(() => {
+                    ok(this.files);
+                }, 100);
+            }
         });
     });
     input.click();
