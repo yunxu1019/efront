778037e963780a48fb8ea2ea6cbfc3e55632eba9
不枝雀 <yunxu1019@qq.com>
2020-03-22 14:20:18 +0800

    样式修改

--- process/colored_console.js
+++ process/colored_console.js
@@ -16,7 +16,6 @@ var colors = {
     FgMagenta: "\x1b[35m",
     FgCyan: "\x1b[36m",
     FgWhite: "\x1b[37m",
-    FgGray: "\x1b[90m",
     BgBlack: "\x1b[40m",
     BgRed: "\x1b[41m",
     BgGreen: "\x1b[42m",
@@ -25,23 +24,43 @@ var colors = {
     BgMagenta: "\x1b[45m",
     BgCyan: "\x1b[46m",
     BgWhite: "\x1b[47m",
+    FgGray: "\x1b[90m",
+    FgRed2: "\x1b[91m",
+    FgGreen2: "\x1b[92m",
+    FgYellow2: "\x1b[93m",
+    FgBlue2: "\x1b[94m",
+    FgPurple: "\x1b[95m",
+    FgCyan2: "\x1b[96m",
+    FgWhite2: "\x1b[97m",
+    // test: "\x1b[97m",
+    BgGray: "\x1b[100m",
+    BgRed2: "\x1b[102m",
+    BgGreen2: "\x1b[102m",
+    BgYellow2: "\x1b[103m",
+    BgBlue2: "\x1b[104m",
+    BgMagenta2: "\x1b[105m",
+    BgCyan2: "\x1b[106m",
+    BgWhite2: "\x1b[107m",
 };
-var lastLogLength = 0;
+var lastLogLength = 0, lastLogTime = 0;
 [
-    "info:提示:FgBlue:",
+    "info:提示:FgCyan:",
     "warn:警告:FgYellow:",
-    "error:错误:FgWhite:BgRed"
+    "error:错误:FgRed2:"
 ].forEach(function (config) {
     var [log, info = log.toUpperCase(), fg, bg] = config.split(":");
     var fgColor = colors[fg] || "",
         bgColor = colors[bg] || "",
         reset = colors.Reset;
-    var toLength = function (n) {
+    var toLength = function (n, a = -1) {
         n = String(n);
         if (n.length < 2) {
             n = '0' + n;
         }
-        return n;
+        if (a === -1 && n.length < 3) {
+            n = '0' + n;
+        }
+        return colors.BgGray + colors.FgWhite2 + n + colors.BgGray + colors.FgWhite;
     };
     var formatDate = function () {
         var year = this.getFullYear();
@@ -52,23 +71,24 @@ var lastLogLength = 0;
         var seconds = this.getSeconds();
         var milli = this.getMilliseconds();
         milli = toLength(milli);
-        if (milli.length < 3) {
-            milli = '0' + milli;
-        }
         var offset = -this.getTimezoneOffset() / 60;
         if (offset > 0) {
-            offset = '+' + toLength(offset);
+            offset = '+' + toLength(offset, 0);
         } else if (offset < 0) {
-            offset = '-' + toLength(-offset);
+            offset = '-' + toLength(-offset, 0);
         } else {
             offset = '';
         }
-        return `${[year, month, date].map(toLength).join('-')}T${[hours, minutes, seconds].map(toLength).join(':')}.${milli}Z${offset}`;
+        return `${[year, month, date].map(toLength).join('-') + colors.FgGray}T${[hours, minutes, seconds].map(toLength).join(':')}.${milli}Z${offset}`;
     };
     var logger = function (...args) {
-        var label = colors.Bright + fgColor + bgColor + info + reset;
-        var time_stamp = colors.FgGray + formatDate.call(new Date) + reset;
-        var str = [label, time_stamp, ...args].join(" ");
+        var label = fgColor + bgColor + info + reset;
+        var time_stamp = '';
+        if (new Date - lastLogTime > 60 * 1000) {
+            lastLogTime = new Date;
+            time_stamp = formatDate.call(lastLogTime) + reset + "\r\n";
+        }
+        var str = [time_stamp, label, ...args].join(" ");
         str = str.replace(/\<([a-z][\w]*)[^\>]*\>([\s\S]*?)\<\/\1\>/ig, function (_, c, s) {
             switch (c) {
                 case "red":
@@ -94,7 +114,7 @@ var lastLogLength = 0;
         var width = process.stderr.columns;
         var hasNewLine = /^(warn|error)$/.test(log);
         var cleaner = ("\r" + " ".repeat(width - 1) + "\b".repeat(width - 1)).repeat(parseInt(lastLogLength / width) + 1);
-        hasNewLine ? process.stderr.write((lastLogLength ? cleaner : "") + str.trim() + "\r\n") : process.stderr.write(cleaner + "\r" + str);
+        hasNewLine ? process.stderr.write((lastLogLength ? cleaner : "") + str + "\r\n") : process.stderr.write(cleaner + "\r" + str);
         lastLogLength = hasNewLine ? 0 : str.length + str.replace(/[\x20-\xff]/g, "").length;
     };
     colored[log] = logger;
