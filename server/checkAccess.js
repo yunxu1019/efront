071bf7b20488b7ec7cefa19f8927170fbd2eb502
不枝雀 <yunxu1019@qq.com>
2020-03-22 19:33:01 +0800

    增加读取用户文件夹中的shared.json

--- server/checkAccess.js
+++ server/checkAccess.js
@@ -1,18 +1,23 @@
 "use strict";
 var path = require("path");
+var os = require("os");
 var loadjson = require("../process/loadjson");
-var configfilepath = "./data/shared.json";
-loadjson.async(configfilepath, true).then(function (json) {
-    if (json instanceof Array) {
-        rootDirectorys.splice(0, rootDirectorys.length);
-        rootDirectorys.push.apply(rootDirectorys, json.map(function (dir) {
-            return path.normalize(dir);
-        }));
-    }
-}).catch(function (error) {
-    console.warn('加载共享路径出错', error);
-    // 未配置网络路径
-});
+var getFile = function (configfilepath) {
+    loadjson.async(configfilepath, true).then(function (json) {
+        if (json instanceof Array) {
+            rootDirectorys.splice(0, rootDirectorys.length);
+            rootDirectorys.push.apply(rootDirectorys, json.map(function (dir) {
+                return path.normalize(dir);
+            }));
+        }
+    }).catch(function (error) {
+        console.warn('加载共享路径出错', error);
+        // 未配置网络路径
+    });
+};
+getFile('data/shared.json');
+getFile(path.join(os.userInfo().homedir, '.efront/shared.json'));
+
 var rootDirectorys = [];
 
 function checkAccess(fullpath) {
