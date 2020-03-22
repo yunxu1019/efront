4e2e2638fae6529bf60a08a0f6cffb0210fd668c
不枝雀 <yunxu1019@qq.com>
2020-03-22 15:32:52 +0800

    配置项修改

--- _envs/setup.bat
+++ _envs/setup.bat
@@ -2,20 +2,12 @@ REM 中文编码gb2312
 rem 默认应用
 if not defined app set APP=zimoli
 rem database
-set DB_HOST=localhost
-set DB_PORT=3306
-set DB_USERNAME=root
-set DB_PASSWORD=root
-set DB_DATABASE=%APP%
-SET HOST.QINIU=ouxjfkv92.bkt.clouddn.com
+REM set HTTPS_PORT=444
+
 rem https证书路径
-set PATH.SSL_PFX=%~DP0cert\efront.cc.pfx
 rem 把以下值写入.\cert\private.bat 以阻止提交
+REM set PATH.SSL_PFX=%~DP0cert\efront.cc.pfx
 REM set PASSWORD.SSL_PFX=%ssl证书密码%
-REM set KEY.AMAP=%高德地图key%
-REM set KEY.QINIU_ACCESS=%七牛云access%
-REM set KEY.QINIU_SECRET=%七牛云secret%
-REM set KEY.REQUEST_SECRET=%efront通信密钥%
 @if exist "%~dp0cert\private.bat" @call "%~dp0cert\private"
 
 rem 前文已配置的所有参数可用 app=%app%.bat 重写
