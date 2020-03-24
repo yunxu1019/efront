eb4056cb05800adc025d03650da01687465f0b0b
不枝雀 <yunxu1019@qq.com>
2020-03-25 00:15:26 +0800

    支持导出Promise

--- coms/zimoli/script.js
+++ coms/zimoli/script.js
@@ -1,7 +1,13 @@
 function script(url, judger) {
     var script = document.createElement("script");
     var oh, ok = oh = function () { };
+    var reject, resolve;
+    var promise = new Promise((ok, oh) => {
+        reject = oh;
+        resolve = ok;
+    });
     var needcallback = !!judger;
+
     var clear = function () {
         remove(script);
     };
@@ -9,7 +15,7 @@ function script(url, judger) {
         clear();
         if (!needcallback || needcallback > 1000) return;
         var res = seek(window, judger);
-        if (res !== undefined) return ok(res);
+        if (res !== undefined) return ok(res), resolve(res);
         requestAnimationFrame(onload);
         needcallback++;
     };
@@ -17,6 +23,7 @@ function script(url, judger) {
         clear();
         if (!needcallback || needcallback > 1000) return;
         oh(e);
+        reject(e);
     };
     on('load')(script, onload);
     on('error')(script, onerror);
@@ -24,11 +31,19 @@ function script(url, judger) {
         clear();
         needcallback = false;
     };
-    script.done = script.then = function (f) {
+    script.done = function (f) {
         if (f instanceof Function) ok = f;
+        return script;
     };
-    script.error = script.catch = function (f) {
+    script.error = function (f) {
         if (f instanceof Function) oh = f;
+        return script;
+    };
+    script.then = function (f) {
+        return promise.then(f);
+    };
+    script.catch = function (f) {
+        return promise.catch(f);
     };
     script.src = url;
     setTimeout(function () {
@@ -36,5 +51,6 @@ function script(url, judger) {
             appendChild(document.head, script);
         }
     });
+    script.loading_promise = promise;
     return script;
 }
