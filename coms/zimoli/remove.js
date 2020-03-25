dfe0947fa66ea8751381a35802933a28b46194ac
不枝雀 <yunxu1019@qq.com>
2020-03-25 09:18:28 +0800

    解决有些元素无remove事件的问题

--- coms/zimoli/remove.js
+++ coms/zimoli/remove.js
@@ -35,22 +35,24 @@ function remove(node, transition) {
 }
 
 function _onremove(node, event) {
-    if (!node.isMounted) return;
-    var onremove = node.onremove;
-    node.isMounted = false;
-    if (!event) {
-        event = createEvent("remove");
-    }
-    dispatch(node, event);
-    if (isArray(onremove)) {
-        onremove.map(function (remove_hindler) {
-            remove_hindler.call(this, event);
-        }, node);
-    }
-    if (isFunction(onremove)) {
-        onremove.call(node, event);
-    }
+    if (node.isMounted === false) return;
     var children = node.childNodes;
+    if (node.isMounted) {
+        var onremove = node.onremove;
+        if (!event) {
+            event = createEvent("remove");
+        }
+        dispatch(node, event);
+        if (isArray(onremove)) {
+            onremove.map(function (remove_hindler) {
+                remove_hindler.call(this, event);
+            }, node);
+        }
+        if (isFunction(onremove)) {
+            onremove.call(node, event);
+        }
+        node.isMounted = false;
+    }
     if (children) for (var cx = 0, dx = children.length; cx < dx; cx++) {
         _onremove(children[cx], event);
     }
