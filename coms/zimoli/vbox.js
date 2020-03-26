100e040175c8306623359d9e83a0a25388ceb750
不枝雀 <yunxu1019@qq.com>
2020-03-26 11:35:10 +0800

    scroll方法提取

--- coms/zimoli/vbox.js
+++ coms/zimoli/vbox.js
@@ -1,4 +1,5 @@
 function ybox(generator) {
+    var scrollY = vscroll.Y;
     var abs = Math.abs;
     var sqrt = Math.sqrt;
     var sign = Math.sign || function (a) {
@@ -161,54 +162,7 @@ function ybox(generator) {
             smooth(false);
         }
     });
-    var scrollY = function (deltay, useIncrease) {
-        deltay = scrollOutside.call(this, deltay);
-        if (isFunction(this.scrollY)) return this.scrollY(deltay, useIncrease);
-        else this.scrollTop += deltay;
-        return this.scrollTop;
-    };
-    var Top = function () {
-        if (isFunction(this.Top)) return this.Top();
-        return this.scrollTop;
-    };
-    var height = function () {
-        if (isFunction(this.height)) return this.height();
-        return this.clientHeight;
-    };
-    var Height = function () {
-        if (isFunction(this.Height)) return this.Height();
-        return this.scrollHeight;
-    };
-    var scrollOutside = function (deltay) {
-        var _box = this;
-        if (_box.YScrollBoxId === 1) return deltay;
-        var offsetParent = _box.offsetParent;
-        if (!offsetParent) return deltay;
-        var _boxPosition = getScreenPosition(_box);
-        var _parentPosition = getScreenPosition(offsetParent);
-        if (_boxPosition.bottom - 1 >= _parentPosition.bottom && deltay > 0) {
-            var deltaScroll = _boxPosition.bottom - _parentPosition.bottom;
-            deltaScroll = min(deltay, deltaScroll);
-            scrollY.call(offsetParent, deltaScroll, false);
-            deltay = deltay - deltaScroll;
-        } else if (_boxPosition.top < _parentPosition.top && deltay < 0) {
-            var deltaScroll = _boxPosition.top - _parentPosition.top;
-            deltaScroll = max(deltay, deltaScroll);
-            scrollY.call(offsetParent, deltaScroll, false);
-            deltay = deltay - deltaScroll;
-        } else if (Top.call(_box) <= 0 && deltay < 0) {
-            var top = Top.call(offsetParent);
-            scrollY.call(offsetParent, deltay, false);
-            deltaScroll = Top.call(offsetParent) - top;
-            deltay = deltay - deltaScroll;
-        } else if (Top.call(_box) + height.call(_box) >= Height.call(_box) && deltay > 0) {
-            var top = Top.call(offsetParent);
-            scrollY.call(offsetParent, deltay, false);
-            deltaScroll = Top.call(offsetParent) - top;
-            deltay = deltay - deltaScroll;
-        }
-        return scrollOutside.call(offsetParent, deltay);
-    };
+
     var smooth_timer;
     var mousemove = function (event) {
         var { clientX, clientY } = event;
