190f5afe830f883dddbd5201f1112ddf2bf62d28
不枝雀 <yunxu1019@qq.com>
2020-04-08 22:15:23 +0800

    修改样式

--- coms/kugou/song.js
+++ coms/kugou/song.js
@@ -1,13 +1,20 @@
 function main(elem = div()) {
+    elem = document.createElement("song");
+    if (!elem.innerHTML) elem.innerHTML = song;
+    var $scope = render(elem, {
+        filterTime,
+        png: img,
+        playState: playState,
+        song: {},
+        musicList,
+    }).$scope;
     care(elem, function (item) {
-        elem.innerHTML = song;
-        render(this, {
-            filterTime,
-            song: {
-                songMarked:mark(item.name || item.songName || item.songname_original, elem.mark),
-                singerMarked:mark(item.singer || item.singerName || item.singername, elem.mark)
-            }
-        });
+        $scope.song = {
+            hash: item.hash,
+            imgurl: item.imgurl,
+            songMarked: mark(item.name || item.songName || item.songname_original, elem.mark),
+            singerMarked: mark(item.singer || item.singerName || item.singername, elem.mark)
+        };
     });
     return block(elem);
 }
