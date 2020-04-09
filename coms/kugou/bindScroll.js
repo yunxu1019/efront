f388dd369435b2dfb2f3e6b2e0f02715c5992276
不枝雀 <yunxu1019@qq.com>
2020-04-10 00:10:23 +0800

    使用fromOffset代替px

--- coms/kugou/bindScroll.js
+++ coms/kugou/bindScroll.js
@@ -3,20 +3,20 @@ var topHeight = 44;
 function bindScroll(titlebar, page) {
     var labelarea = div();
     var [label, back] = titlebar.children;
-    css(back, `height:${topHeight}px;`);
+    css(back, `height:${fromPixel(topHeight)}`);
     appendChild.after(label, labelarea);
     css(labelarea, "height:100%;width:100%;background:#000;position:absolute;left:0;top:0;z-index:-1");
     onappend(page, function () {
-        css(titlebar, `min-height:${topHeight}px;`);
+        css(titlebar, `min-height:${fromPixel(topHeight)}`);
         setOpacity(labelarea, 0);
         setOpacity(label, 0);
-        refresh();
+        setTimeout(refresh)
     });
     var refresh = function () {
         var image = page.children[0];
         if (!image) return;
-        if (!image.style.backgroundImage) image = page.children[1] || image;
-        if (!image.style.backgroundImage) return;
+        if (!image.style.backgroundImage && !/img/i.test(image.className)) image = page.children[1] || image;
+        if (!image.style.backgroundImage && !/img/i.test(image.className)) return;
         setOpacity(image, 0);
         image.style.opacity = 0;
         var scrollTop = page.scrollTop;
@@ -28,11 +28,15 @@ function bindScroll(titlebar, page) {
         if (height < topHeight) {
             height = topHeight;
         }
-        css(titlebar, `background: center ${image.style.backgroundImage};background-size:cover;height:${height}px;`);
+        if (height > page.offsetHeight >> 1) {
+            height = page.offsetHeight >> 1;
+            css(image, { paddingBottom: fromOffset(height) })
+        }
+        css(titlebar, `background: center ${image.style.backgroundImage};background-size:cover;height:${fromOffset(height)}`);
         setOpacity(labelarea, (1 - (titlebar.offsetHeight + offsetHeight) / offsetHeight / 2));
         setOpacity(label, 1 - (titlebar.offsetHeight - topHeight) / (offsetHeight - topHeight));
         return {};
     };
-    page.setAttribute("ng-class", "refresh()");
+    if (!page.renders) page.renders = [];
     page.onscroll = refresh;
 }
