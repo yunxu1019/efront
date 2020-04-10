ff637c11f88b6714adfc09326b24b1c2ce2cda09
不枝雀 <yunxu1019@qq.com>
2020-04-11 00:38:40 +0800

    解决list可能不初始化的问题

--- coms/zimoli/list.js
+++ coms/zimoli/list.js
@@ -87,6 +87,7 @@ function ylist(container, generator, $Y) {
     //设置当前下标
     var scrollTo = function (itemIndex) {
         if (!list.offsetHeight || !list.offsetWidth) {
+            saved_itemIndex = itemIndex;
             return;
         }
         var index = itemIndex | 0;
