bd27ec330daa4910c513ff5d5abb3ebd612e6e30
不枝雀 <yunxu1019@qq.com>
2020-04-10 22:20:11 +0800

    修复左侧菜单无法折叠的问题

--- docs/main.js
+++ docs/main.js
@@ -28,7 +28,10 @@ api("/getAllComponents").success(function (result) {
     alert(err);
 });
 onactive(leftArea, function (event) {
-    if (event.value.tab === 1) return;
+    if (event.value.tab === 1) {
+        event.value.closed = !event.value.closed;
+        return;
+    }
     if (!event.value.test) return alert("仅可以测试点击以_test结尾的哦");
     commNameInput.value = event.value.name + "_test";
     state({
