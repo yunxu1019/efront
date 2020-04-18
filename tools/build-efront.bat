534c14ce8d9f43e43c32d3dc0404690176ac4923
不枝雀 <yunxu1019@qq.com>
2020-04-19 03:32:38 +0800

    更新编译efront2的脚本

--- tools/build-efront.bat
+++ tools/build-efront.bat
@@ -2,7 +2,8 @@
 @echo off
 cls
 setlocal
-
+pushd
+cd %~dp0..
 set coms_path=./coms,
 set page_path=./coms
 set public_path=./public
@@ -14,7 +15,8 @@ set coms=./
 set libs_path=./
 set libs=typescript,esprima,escodegen,esmangle,pngjs,less-node
 set destpath=efront.js
-call efront publish
+rd /s /q %public_path%
+call node . publish
+popd
 goto :end
-
 :end
