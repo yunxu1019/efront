57ec9e3d0475f73b89b545d782e44206da74fcfc
不枝雀 <yunxu1019@qq.com>
2020-04-21 19:46:41 +0800

    只有设置了LIBS_PATH或LIBS才检查

--- coms/efront/isLib.js
+++ coms/efront/isLib.js
@@ -10,10 +10,10 @@ var {
 var joinpath = ([a, b, c]) => path.join(a === ":" ? path.join(__dirname, "../../") : a, b, c || '');
 var comms_root = mixin(COMS_PATH, COMM).map(joinpath).filter(fs.existsSync);
 
-var libs_root = [].concat(
+var libs_root = (LIBS_PATH || LIBS) ? [].concat(
     mixin(LIBS_PATH, LIBS).map(joinpath),
     mixin(comms_root, LIBS_PATH, LIBS).map(joinpath),
-).filter(fs.existsSync);
+).filter(fs.existsSync) : [];
 function isLib(fullpath) {
     for (var cx = 0, dx = libs_root.length; cx < dx; cx++) {
         var rel = path.relative(libs_root[cx], fullpath);
