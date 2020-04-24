582bde02759f1d556c359d47f41d287906ccc53d
不枝雀 <yunxu1019@qq.com>
2020-04-24 11:44:10 +0800

    Revert "更新less"
    This reverts commit ec39b07af34b8a6dd894a4e3ddaa42673f2667ce.

--- tools/update-less.bat
+++ tools/update-less.bat
@@ -3,26 +3,23 @@
 setlocal
 if not defined registry set registry=http://registry.npm.taobao.org
 
-if not exist node_modules/less call npm install less@latest --registry=%registry%
-
+call npm install less@latest --registry=%registry%
 if not exist node_modules/less goto :error1
 
-set coms_path=node_modules\less\lib,coms\typescript-helpers
-set coms=./
+set coms_path=node_modules\less\lib,node_modules,coms\typescript
 set public_path=coms\less-node
 set page=./
-set app=./less-node/index.js
+set app=/less-node/index.js
 set destpath=%public_path%\index.js
 git restore %destpath%
-call node coms/efront publish %*
-if not exist %public_path%\less-node goto :error2
-echo ;>>%public_path%\less-node
-echo module.exports=this.lessNode.default;>>%public_path%\less-node
+call efront publish
+if not exist %public_path%\lessNode goto :error2
+echo ;>>%public_path%\lessNode
+echo module.exports=this.lessNode.default;>>%public_path%\lessNode
 if exist %destpath%  del %destpath%
-move %public_path%\less-node %destpath%
+move %public_path%\lessNode %destpath%
 node %destpath%
 if errorlevel == 1 goto :recover
-
 goto :end
 
 :error1
