c58004f32e7e5d7c76b0e0cbd79dcde8fdd78c66
不枝雀 <yunxu1019@qq.com>
2020-05-07 00:13:06 +0800

    处理ios上blur事件不触发的问题

--- coms/zimoli/preventOverflowScrolling.js
+++ coms/zimoli/preventOverflowScrolling.js
@@ -2,6 +2,12 @@ if ("webkitOverflowScrolling" in document.documentElement.style) {
     var preventOverflowScrolling = function (target) {
         ontouchmove(target, event => event.preventDefault());
     };
+    on('touchend')(window, function (event) {
+        var { activeElement } = document;
+        var target = getTargetIn(activeElement, event.target);
+        if (target) return;
+        activeElement.blur();
+    });
 } else {
     var preventOverflowScrolling = function () {
     };
