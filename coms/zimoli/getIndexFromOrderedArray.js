7af4f27e56bac2804b7a28233a5840fa6999737b
不枝雀 <yunxu1019@qq.com>
2020-05-12 13:19:48 +0800

    修改读取value的方法

--- coms/zimoli/getIndexFromOrderedArray.js
+++ coms/zimoli/getIndexFromOrderedArray.js
@@ -1,9 +1,13 @@
+var getValue = function (o) {
+    return o instanceof Object && "value" in o ? o.value : o
+};
+var defaultIsLE = (sample, search) => getValue(sample) <= getValue(search);
 /**
  * 读取元素在有序数组中的位置
  * @param {Array} orderArray 
  * @param {string|number} searchItem 
  */
-var getIndexFromOrderedArray = function (orderArray, searchItem, isLE = (sample, search) => (typeof sample === "object" ? sample.value : sample) <= (typeof search === "object" ? search.value : search)) {
+var getIndexFromOrderedArray = function (orderArray, searchItem, isLE = defaultIsLE) {
     var count = 0;
     for (var cx = 0, dx = orderArray.length, ci = cx + dx >>> 1; cx < dx; ci = cx + dx >>> 1) {
         var orderItem = orderArray[ci];
