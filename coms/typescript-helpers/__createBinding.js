8ada79773c1a9b5fe69fc28fded6ed8e003abba0
不枝雀 <yunxu1019@qq.com>
2020-05-22 23:11:58 +0800

    更新typescript到3.9.3

---
+++ coms/typescript-helpers/__createBinding.js
@@ -0,0 +1,14 @@
+/**
+ * typescript:commonjscreatebinding
+ * 这个文件由工具生成，请不要手动修改
+ * 
+ * Efront Authors
+ * 2020-05-22T15:06:22.318Z
+ */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
