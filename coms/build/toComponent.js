1b0eb06d6d3c275d7c56a733d8cb71975d6cfffc
不枝雀 <yunxu1019@qq.com>
2020-06-27 11:19:21 +0800

    增加识别crypt环境变量

--- coms/build/toComponent.js
+++ coms/build/toComponent.js
@@ -51,7 +51,7 @@ function toComponent(responseTree) {
         return $key;
     };
     var strings = "slice,length,split,concat,apply,reverse,exec,indexOf,string,join,call,exports".split(",");
-    var encoded = !/(false|0|null)/i.test(process.env.ENCODE) && !/^(false|0|null)/i.test(process.env.ENCRYPT);
+    var encoded = !/(false|0|null)/i.test(process.env.ENCODE) && !/^(false|0|null)/i.test(process.env.ENCRYPT) && !/^(false|0|null)/i.test(process.env.CRYPT);
     var encode = function (source) {
         if (!encoded) return source;
         source = evalString(source);
