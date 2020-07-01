baba371fa84f1cdf9a0712962fac157ba9ff40e8
不枝雀 <yunxu1019@qq.com>
2020-07-02 02:41:34 +0800

    支持递归合并数据

--- coms/zimoli/merge.js
+++ coms/zimoli/merge.js
@@ -1,5 +1,5 @@
 function mergeToElement(node, src) {
-    if(!src)return;
+    if (!src) return;
     var dst_o = getNamedElements(node);
     for (var k in dst_o) {
         var elem = dst_o[k];
@@ -35,14 +35,30 @@ function merge(dst, src) {
     if (isFunction(src)) {
         src = src();
     }
+    if (!(src instanceof Object)) {
+        return src;
+    }
     if (isElement(dst)) {
-        mergeToElement(dst,src);
+        mergeToElement(dst, src);
     } else if (isDate(src)) {
         dst = new Date(src);
-    } else if (src instanceof Object) {
-        for (var k in src) {
-            var v = src[k];
-            dst[k] = v;
+    } else if (dst instanceof Array) {
+        if (src instanceof Array) {
+            dst.splice(0, dst.length);
+            dst.push.apply(dst, src);
+        } else {
+            dst.forEach(d => merge(d, src));
         }
+    } else if (dst instanceof Object) {
+        if (src instanceof Array) {
+            src.forEach(s => merge(dst, s));
+        } else {
+            for (var k in src) {
+                dst[k] = merge(dst[k], src[k]);
+            }
+        }
+    } else {
+        return merge(src instanceof Array ? [] : {}, src);
     }
+    return dst;
 }
---
+++ coms/zimoli/merge_test.js
@@ -0,0 +1,3 @@
+function main() {
+    console.log(merge({ 1: 3,b:{c:2} }, [2, { a:2 },{b:{d:2}}]))
+}
