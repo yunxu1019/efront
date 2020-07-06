635cc121484a7b2b1532ba73eef0037ff1aa69c3
不枝雀 <yunxu1019@qq.com>
2020-07-06 23:16:56 +0800

    添加grid兼容性测试

--- apps/moue/home/index2.vue
+++ apps/moue/home/index2.vue
@@ -2,17 +2,54 @@
 & {
   color: #fff;
   background: #ccc;
+  height: 100%;
+  width: 100%;
+  position: relative;
   appearance: none;
+}
+&,
+* {
+  box-sizing: border-box;
+}
+.nav {
+  height: 60px;
+  margin-top: -60px;
+}
+> .menu,
+> .main {
+  display: inline-block;
+  vertical-align: top;
+  height: 100%;
+}
+> .menu {
+  width: 200px;
+  margin-left: -200px;
+}
+> .main {
+  width: 100%;
+}
+& {
+  padding: 60px 0 0 200px;
+}
+> .body {
+  width: 100%;
 }
 </style>
 <template>
-  <btn>abc</btn>
+  <grid editable>
+    <div class="nav"></div>
+    <div class="menu"></div>
+    <div class="main">
+      <btn>abc</btn>
+    </div>
+  </grid>
 </template>
 <script>
 export default {
   components: {
     btn: button_test,
-    btn: moue(button)
+    btn: moue(button),
+    grid: moue(grid)
   }
 };
 </script>
