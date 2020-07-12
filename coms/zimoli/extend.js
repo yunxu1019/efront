b1df479a513da4e751a0bfcb542f2650b836b356
不枝雀 <yunxu1019@qq.com>
2020-07-12 17:39:56 +0800

    修复Object.create(null)得到的对象无法使用extend的问题

--- coms/zimoli/extend.js
+++ coms/zimoli/extend.js
@@ -6,12 +6,11 @@
 var hasOwnProperty = {}.hasOwnProperty, Object = this.Object;
 
 var extend = Object.assign || function (o1) {
-    if (o1 instanceof Object) {
-        for (var cx = 1, dx = arguments.length; cx < dx; cx++) {
-            var o2 = arguments[cx];
-            if (o2 instanceof Object) for (var k in o2) {
-                if (hasOwnProperty.call(o2, k)) o1[k] = o2[k];
-            }
+    o1 = Object(o1);
+    for (var cx = 1, dx = arguments.length; cx < dx; cx++) {
+        var o2 = arguments[cx];
+        for (var k in o2) {
+            if (hasOwnProperty.call(o2, k)) o1[k] = o2[k];
         }
     }
     return o1;
--- coms/zimoli/extendIfNeeded.js
+++ coms/zimoli/extendIfNeeded.js
@@ -1,11 +1,10 @@
 var hasOwnProperty = {}.hasOwnProperty;
 function extendIfNeeded(o1) {
-    if (o1 instanceof Object) {
-        for (var cx = 1, dx = arguments.length; cx < dx; cx++) {
-            var o2 = arguments[cx];
-            if (o2 instanceof Object) for (var k in o2) {
-                if (hasOwnProperty.call(o2, k) && !(k in o1)) o1[k] = o2[k];
-            }
+    o1 = Object(o1);
+    for (var cx = 1, dx = arguments.length; cx < dx; cx++) {
+        var o2 = arguments[cx];
+        for (var k in o2) {
+            if (hasOwnProperty.call(o2, k) && !(k in o1)) o1[k] = o2[k];
         }
     }
     return o1;
