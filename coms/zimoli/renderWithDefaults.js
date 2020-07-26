601f11d9d0febebcae998271246af844b4afd016
不枝雀 <yunxu1019@qq.com>
2020-07-27 00:43:25 +0800

    增加一些跳转方法

--- coms/zimoli/renderWithDefaults.js
+++ coms/zimoli/renderWithDefaults.js
@@ -1,22 +1,5 @@
-var getNeededElementReplacers = function (page) {
-    var dest = Object.create(null);
-    var foreach = [].forEach;
-    var get = function (elem) {
-        var { tagName } = elem;
-        tagName = tagName.toLowerCase().replace(/\-(\w)/g, (_, a) => a.toUpperCase());
-        dest[tagName] = false;
-        foreach.call(elem.children, get);
-    };
-    get(page);
-    var res = Object.create(null);
-    Object.keys(dest).filter(k => k in renderDefaults).forEach(function (key) {
-        res[key] = renderDefaults[key];
-    });
-    return res;
-};
 function main(page, $scope = {}) {
-    var replacers = getNeededElementReplacers(page);
-    extendIfNeeded($scope, replacers);
+    extendIfNeeded($scope, renderDefaults);
     render(page, $scope);
     return page;
 }
