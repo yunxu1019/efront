eb30a898f3a5e0ba8d53a10cfa59887613d1885f
不枝雀 <yunxu1019@qq.com>
2020-08-24 21:06:49 +0800

    某e组件预置数据时机提前到create之前

--- coms/moue/apply.js
+++ coms/moue/apply.js
@@ -1,6 +1,30 @@
 function apply(context, args) {
     var [params, from] = args;
-    var vue = new Vue(this);
-    extend(vue, { params, state: context, from });
+    if (!this.props) this.props = {};
+    if (this.props instanceof Array) {
+        var props = this.props;
+        'params,state,from'.split(',').forEach(k => {
+            if (~props.indexOf(k)) {
+                props.push(k)
+            }
+        })
+    } else {
+        extendIfNeeded(this.props, {
+            params: {},
+            state: {},
+            from: {},
+        })
+    }
+    var vue = new Vue({
+        render: (h) => {
+            return h(this, {
+                props: {
+                    params,
+                    state: context,
+                    from
+                }
+            });
+        }
+    });
     return vue.$mount().$el;
 }
