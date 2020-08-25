9d2da760640d4b08bed0d1bd56e7202469899e05
不枝雀 <yunxu1019@qq.com>
2020-08-25 13:17:05 +0800

    添加一个jsonp工具

---
+++ coms/zimoli/jsonp.js
@@ -0,0 +1,27 @@
+var jsonpid = 0;
+function jsonp(url, params) {
+    var extras = {};
+    jsonpid++;
+    var jsonpcb = 'jsonp' + jsonpid;
+    var callbacks = [];
+    for (var k in params) {
+        var f = params[k]
+        if (isFunction(f)) {
+            callbacks.push(f);
+            extras[k] = jsonpcb;
+        } else {
+            extras[k] = params[k];
+        }
+    }
+    window[jsonpcb] = function (a) {
+        delete window[jsonpcb];
+        callbacks.forEach(c => c(a));
+        remove(s);
+    };
+    var search = serialize(extras);
+    if (search) {
+        url += /\?/.test(url) ? "&" + search : "?" + search;
+    }
+    var s = script(url);
+    return s;
+}
