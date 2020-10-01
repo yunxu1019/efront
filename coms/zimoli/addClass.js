53bd12999999afa04ef5cd9908b9e7c560fc20ab
不枝雀 <yunxu1019@qq.com>
2020-10-01 21:49:05 +0800

    解决添加className后可能多出一个undefined的问题

--- coms/zimoli/addClass.js
+++ coms/zimoli/addClass.js
@@ -1,16 +1,16 @@
 function addClass(target, classNames) {
     var _className = target.className;
-    var arr = String(_className).trim().split(/\s+/);
+    var arr = String(_className || "").trim().split(/\s+/);
     var cls = {};
     for (var cx = 0, dx = arr.length; cx < dx; cx++) {
         var c = arr[cx];
-        cls[c] = true;
+        if (c) cls[c] = true;
     }
     if (isString(classNames)) {
         var clsNames = classNames.trim().split(/\s+/);
         for (var cx = 0, dx = clsNames.length; cx < dx; cx++) {
             var c = clsNames[cx];
-            cls[c] = true;
+            if (c) cls[c] = true;
         }
     } else {
         merge(cls, classNames);
