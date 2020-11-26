b65dc8670bd3b92331b148f84b1efb13a2c8064e
不枝雀 <yunxu1019@qq.com>
2020-11-26 18:13:43 +0800

    对组件列表进行排序

--- apis/docs/getAllComponents.js
+++ apis/docs/getAllComponents.js
@@ -11,7 +11,33 @@ function getAllcomponents() {
             }
             var result = [], map = {};
             var current_name_prefix;
-            names.filter(a => comm_file_reg.test(a)).forEach(function (name) {
+            names.filter(name => {
+                if (
+
+                    !comm_file_reg.test(name)
+                ) return false;
+                if (test_file_reg.test(name)) {
+                    name = name.replace(test_file_reg, "");
+                    map[name] = {
+                        test: true
+                    };
+                    return false;
+
+                }
+                return true;
+
+            }).map(name => {
+                name = name.replace(comm_file_reg, "");
+                return name;
+            }).sort((a, b) => {
+                a0 = a[0].toLowerCase();
+                b0 = b[0].toLowerCase();
+                if (a0 > b0) return 1;
+                if (a0 < b0) return -1;
+                var delta = (b in map) - (a in map);
+                if (delta) return delta;
+                return a > b ? 1 : a < b ? -1 : 0;
+            }).forEach(function (name) {
                 if (current_name_prefix !== name.charAt(0).toUpperCase()) {
                     current_name_prefix = name.charAt(0).toUpperCase();
                     result.push({
@@ -19,28 +45,17 @@ function getAllcomponents() {
                         tab: 1
                     });
                 }
-                if (test_file_reg.test(name)) {
-                    name = name.replace(test_file_reg, "");
-                    if (map[name]) {
-                        map[name].test = true;
-                    } else {
-                        map[name] = {
-                            test: true
-                        };
-                    }
+                var obj = {
+                    name,
+                    tab: 2
+                };
+                if (map[name]) {
+                    obj.test = map[name].test;
                 } else {
-                    name = name.replace(comm_file_reg, "");
-                    var obj = {
-                        name,
-                        tab: 2
-                    };
-                    if (map[name]) {
-                        obj.test = map[name].test;
-                    } else {
-                        map[name] = obj;
-                    }
-                    result.push(obj);
+                    map[name] = obj;
                 }
+                result.push(obj);
+
             });
             ok(result);
         });
