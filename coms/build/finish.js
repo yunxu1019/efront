66aeff07041c80e4bc7197fd08776af10a7f937d
不枝雀 <yunxu1019@qq.com>
2021-01-04 13:04:47 +0800

    显示打包时占用的内存

--- coms/build/finish.js
+++ coms/build/finish.js
@@ -1,6 +1,14 @@
 var reloadListener = require("./reloadListeners");
+var size = require("../basic/size");
+var showMemery = function () {
+    var m = process.resourceUsage().maxRSS * 1024;
+    var { system, user } = process.cpuUsage();
+    process.title = `${size(m)}, ${+(100 * user / (user + system)).toFixed(2)}%`;
+};
+var timer = setInterval(showMemery, 12);
 var finish = function (deltaTime) {
-    console.info(`完成，用时${deltaTime / 1000}秒。\r\n`);
+    clearInterval(timer);
+    console.info(`完成，用时${deltaTime / 1000}秒，占用内存${size(process.resourceUsage().maxRSS * 1024)}。\r\n`);
     reloadListener.fire();
 };
 module.exports = finish;
