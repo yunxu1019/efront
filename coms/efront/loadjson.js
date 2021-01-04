04ff4f4f1c9ab242c4b243ade6c5258c0e207146
不枝雀 <yunxu1019@qq.com>
2021-01-04 20:11:14 +0800

    修改一处过时的代码

--- coms/efront/loadjson.js
+++ coms/efront/loadjson.js
@@ -6,25 +6,18 @@ var loadjson = module.exports = function loadjson(url) {
 };
 loadjson.async = function (url, ignoreIfNotExist) {
     return new Promise(function (ok, oh) {
-        fs.exists(url, function (exists) {
-            if (!exists) {
-                if (!ignoreIfNotExist) {
-                    return oh(new Error(`文件不存在!${url}`));
-                }
-                return;
-            };
-            fs.stat(url, function (error, stats) {
+        if (!fs.existsSync(url)) return;
+        fs.stat(url, function (error, stats) {
+            if (error) return oh(error);
+            if (!stats.isFile()) return oh(new Error(`路径所指向的不是文件${url}`));
+            if (stats.size > 2 * 1024 * 1024) return oh(new Error(`文件过大!${url}`));
+            fs.readFile(url, function (error, buffer) {
                 if (error) return oh(error);
-                if (!stats.isFile()) return oh(new Error(`路径所指向的不是文件${url}`));
-                if (stats.size > 2 * 1024 * 1024) return oh(new Error(`文件过大!${url}`));
-                fs.readFile(url, function (error, buffer) {
-                    if (error) return oh(error);
-                    try {
-                        ok(new Function("return " + String(buffer))());
-                    } catch (e) {
-                        oh(e);
-                    }
-                });
+                try {
+                    ok(new Function("return " + String(buffer))());
+                } catch (e) {
+                    oh(e);
+                }
             });
         });
     });
