aceb6089cc2c6a4e7d90f87f939193c4723f1673
不枝雀 <yunxu1019@qq.com>
2021-02-09 03:38:37 +0800

    优化触摸板和鼠标滚轮的缩放操作

--- coms/zimoli/picture.js
+++ coms/zimoli/picture.js
@@ -158,7 +158,7 @@ var create = (url, key) => {
         if (!deltaY) return;
         if (!this.locked) setInitParams();
         this.locked = true;
-        var scale = Math.pow(0.999, deltaY);
+        var scale = Math.pow(0.99, 20 * Math.atan(deltaY / 20));
         if (scaled * scale >= 2.5 && scale > 1) {
             scale = 2.5 / scaled;
         }
