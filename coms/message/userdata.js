c0b44249a67f687c12cd3e1588671c4c6679367d
不枝雀 <yunxu1019@qq.com>
2021-02-16 07:37:48 +0800

    解决无初始值时写入文件失败的问题

--- coms/message/userdata.js
+++ coms/message/userdata.js
@@ -4,6 +4,8 @@ var userpath = path.join(require("os").homedir(), ".efront");
 var JSAM = require("../basic/JSAM");
 function save(pathname, data) {
     var fullpath = path.join(userpath, pathname);
+    var folderpath = path.dirname(fullpath)
+    if (!fs.existsSync(folderpath)) fs.mkdirSync(folderpath, { recursive: true });
     fs.writeFileSync(fullpath, JSAM.stringify(data));
 }
 function load(pathname) {
