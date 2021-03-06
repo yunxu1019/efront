0cc686fc6027caec18710452418b17e44991e076
不枝雀 <yunxu1019@qq.com>
2021-03-06 10:31:43 +0800

    支持处理数组类型的数据

--- coms/basic/getIndexFromOrderedArray.js
+++ coms/basic/getIndexFromOrderedArray.js
@@ -2,7 +2,18 @@
 var getValue = function (o) {
     return isObject(o) && "value" in o ? o.value : o
 };
-var defaultIsLE = (sample, search) => getValue(sample) <= getValue(search);
+var arrayIsLE = function (a, b) {
+    var a_length = a.length, b_length = b.length;
+    var total = a_length < b_length ? a_length : b_length;
+    for (var cx = 0, dx = total; cx < dx; cx++) {
+        var a0 = a[cx];
+        var b0 = b[cx];
+        if (a0 !== b0) return a0 <= b0;
+    }
+    return a_length <= b_length;
+};
+var isArray = Array.isArray || (a => a instanceof Array);
+var defaultIsLE = (sample, search) => isArray(sample) && isArray(search) ? arrayIsLE(sample, search) : getValue(sample) <= getValue(search);
 /**
  * 读取元素在有序数组中的位置
  * @param {Array} orderArray 
