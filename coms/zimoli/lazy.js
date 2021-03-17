08fefab6418ff23ac2c13ec4a9e9c7de7060bc91
不枝雀 <yunxu1019@qq.com>
2021-03-18 07:32:01 +0800

    自动忽略一些过时的延时操作

--- coms/zimoli/lazy.js
+++ coms/zimoli/lazy.js
@@ -3,23 +3,24 @@ function lazy(run, time = false) {
     var fire = function () {
         if (time > 17) {
             if (fireing === true) {
-                setTimeout(fire, +time);
+                fireing = setTimeout(fire, +time);
             } else {
                 run.apply(that, args);
+                fireing = false;
             }
         } else {
             if (fireing === true) {
                 run.apply(that, args);
             }
+            fireing = false;
         }
-        fireing = false;
     };
     return function () {
         args = arguments;
         that = this;
         if (fireing) return fireing = true;
         if (time > 0) {
-            setTimeout(fire, +time);
+            fireing = setTimeout(fire, +time);
         } else {
             fireing = requestAnimationFrame(fire);
             if (time === false || time === 0) fireing = true;
