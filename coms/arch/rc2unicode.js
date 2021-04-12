9f3b0d300e53c62517960b0d1fc61c7da3edbac7
不枝雀 <yunxu1019@qq.com>
2021-04-13 03:22:40 +0800

    还原部分代码

--- coms/arch/rc2unicode.js
+++ coms/arch/rc2unicode.js
@@ -3,7 +3,7 @@ var encodeUTF16 = require("../basic/encodeUTF16");
 function rc2unicode(buff) {
     var dist = [];
     decodeUTF8(buff).split("").forEach(function (a) {
-        var buff = encodeUTF16(a);
+        var buff = encodeUTF16(a).reverse();
         dist.push.apply(dist, buff);
     });
     return dist;
