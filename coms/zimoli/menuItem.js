15d4f99392a3c28e2422f5de8fe55fe37a53448f
不枝雀 <yunxu1019@qq.com>
2021-06-02 03:54:23 +0800

    修复菜单内容不显示的问题

--- coms/zimoli/menuItem.js
+++ coms/zimoli/menuItem.js
@@ -1,7 +1,7 @@
 function main(elem, scope, hasIcon) {
     var item = elem || document.createElement('menu-item');
     item.innerHTML = menuItem;
-    if (isObject(scope)) {
+    if (isObject(scope) && scope !== elem.$scope) {
         render(item, { menu: scope, useIcon: hasIcon })
     }
     return item;
