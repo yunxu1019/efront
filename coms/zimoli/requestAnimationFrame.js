c67072ace13e8c87ffff9ab187a26c5276e829f5
不枝雀 <yunxu1019@qq.com>
2021-06-11 08:03:02 +0800

    解决无法在Nodejs环境下运行的问题

--- coms/zimoli/requestAnimationFrame.js
+++ coms/zimoli/requestAnimationFrame.js
@@ -1,4 +1,5 @@
 var lastAnimateTime = +new Date;
+var window = this;
 var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
     //一般集中在25毫秒，左右相差不超过1毫秒
     var currentTime = +new Date;
