b46a6b90ce16133cf44cf70d6cf90578dd9d443a
不枝雀 <yunxu1019@qq.com>
2021-06-12 13:49:55 +0800

    处理空值

--- coms/basic/parseURL.js
+++ coms/basic/parseURL.js
@@ -1,6 +1,7 @@
 // -----///////// -1 ----------////////////2---3-----/////// --4------2//////5------6-----////////-7--5///8--9----//10--11---10///--12-----8//
 var reg = /^([^\/\:@\?#]+\:)?(?:\/\/)?(?:(([^\/@\:]+)?(?:\:([^\/@\:]+))?)@)?(([^\/\:\?\#]+)?(?:\:(\d+))?)(([^\?#]+)?(\?([^#]*))?(#[\s\S]*)?)$/;
 function parseURL(url) {
+    if (url === undefined || url === null) url = '';
     var [__, protocol, auth, username, password, host, hostname, port, path, pathname, search, query, hash] = reg.exec(url);
     return { protocol, auth, username, password, host, hostname, port, path, pathname, search, query, hash, href: __ };
 }
