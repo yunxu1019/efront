05c6623cc56520761909499af049808d20b7ff31
不枝雀 <yunxu1019@qq.com>
2021-06-12 14:50:12 +0800

    启动脚本修改

--- tools/run-efront.bat
+++ tools/run-efront.bat
@@ -1,16 +1,16 @@
 :<<a
 goto :win
 a
-
-export libs_path=./
-export coms_path=./coms
+dp=`dirname $0`
+export libs_path=$dp/../coms
+#export coms_path=./coms
 export libs=typescript,esprima,escodegen,esmangle,pngjs,less-node
-node coms/efront ./efront/index.js $*
+node $dp/../coms/efront ./efront/index.js $*
 exit
 
 
 :win
 set libs_path=%~dp0../coms
-set coms_path=%~dp0../coms
+@REM set coms_path=%~dp0../coms
 set libs=typescript,esprima,escodegen,esmangle,pngjs,less-node
 node %~dp0../coms/efront ./efront/index.js %*
