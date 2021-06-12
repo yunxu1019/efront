9f2d9c82ced0f9e173cbe615479c271a3ad82003
不枝雀 <yunxu1019@qq.com>
2021-06-12 23:27:21 +0800

    启动刷量任务

--- tools/install-all.js
+++ tools/install-all.js
@@ -6,28 +6,38 @@ var parsed = JSON.parse(String(versions).replace(/\'/g, '"'));
 var scripts = `
 npm install -g efront
 efront version
-efront ip
 efront help
 efront help version
 efront -v
 efront --version
-efront simple from kugou
-efront build
 `.trim().split(/[\r\n]+/);
-
+var run = function (script, timelimit) {
+    if (!timelimit) timelimit = 20000;
+    if (timelimit < 2000) timelimit = 2000;
+    var timeStart = +new Date;
+    var gram = child_process.exec(script, {
+        shell: true
+    });
+    gram.stdout.pipe(process.stderr);
+    gram.stdout.pipe(process.stdout);
+    var timer = 0;
+    return Promise.race([
+        ok => gram.once("exit", ok),
+        ok => timer = setTimeout(ok, timelimit)
+    ].map(
+        a => new Promise(a)
+    )).then(function () {
+        if (!gram.killed) gram.kill();
+        clearTimeout(timer);
+        console.info(`执行: <gray>${script}</gray>，用时: <gray>", ${((new Date - timeStart) / 1000).toFixed(2)}</gray>秒`);
+    });
+};
 queue.call(parsed, function (version) {
     var version = parsed.pop();
     var commands = scripts.slice(0);
     commands[0] += "@" + version;
-    return queue.call(commands, function (script) {
-        var timeStart = +new Date;
-        var gram = child_process.exec(script, {
-            shell: true
-        });
-        gram.stdout.pipe(process.stderr);
-        gram.stdout.pipe(process.stdout);
-        return new Promise(ok => gram.once("exit", ok)).then(function () {
-            console.info(`执行: <gray>${script}</gray>，用时: <gray>", ${((new Date - timeStart) / 1000).toFixed(2)}</gray>秒`);
-        });
+    v = versions.split('.').map(a => parseInt(a));
+    return queue.call(commands, run).then(function () {
+        if (v[0] > 1 || v[1] >= 19) return run('efront live 80 443', 20000);
     });
 });
