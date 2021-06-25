e2057ba1611c17419b4b9e4cbee0336c0cf94559
不枝雀 <yunxu1019@qq.com>
2021-06-25 09:07:36 +0800

    部分自动识别的配置优先使用环境变量提供的

--- coms/efront/index.js
+++ coms/efront/index.js
@@ -52,12 +52,13 @@ var setAppnameAndPorts = function (args) {
 var detectEnvironment = function () {
     let fs = require("fs");
     let currentpath = process.cwd(), config = {
-        page_path: currentpath,
+        page_path: memery.PAGE_PATH,
         comm: memery.COMM || '',
         coms_path: memery.COMS_PATH,
         app: memery.APP || '',
         page: memery.PAGE,
     };
+    if (config.page_path === undefined) config.page_path = currentpath;
     var env_path = [];
     return new Promise(function (ok) {
         fs.readdir(currentpath, function (error, names) {
@@ -75,9 +76,9 @@ var detectEnvironment = function () {
             }).forEach(function (name) {
                 if (/page|^app|界面|页面|应用|系统/i.test(name)) {
                     // 高屋|瓴|楼|台|宫|阁|殿|庙|堂|会|场|司|衙|门|党|帮|派|族|山|庄|寺|教|家|城|店|军|队|团|师|营|苟
-                    config.page_path = name;
+                    if (config.page_path === undefined) config.page_path = name;
                 } else if (/^src|source|^code|源|代码/i.test(name)) {
-                    config.page_path = name;
+                    if (config.page_path === undefined) config.page_path = name;
                     coms_path.push(name);
                 } else if (/node_modules|lib|com|fun|depe|组件|模块|依赖|库|函数/i.test(name)) {
                     // 卡木|设施|员|工|匠|子|弟|臣|下|客|器|械|备|库|房|土|基|石|砖
