e6c0f96b89b6d6b2f227a2ad9e07e19e35a1f3a5
不枝雀 <yunxu1019@qq.com>
2021-07-08 16:34:08 +0800

    不再依赖isFunction

--- coms/basic/getIndexFromOrderedArray.js
+++ coms/basic/getIndexFromOrderedArray.js
@@ -19,7 +19,7 @@ var defaultIsLE = (sample, search) => isArray(sample) && isArray(search) ? array
  * @param {Array} orderArray 
  * @param {string|number} searchItem 
  */
-module.exports = function (orderArray, searchItem, isLE = isFunction(searchItem) ? searchItem : defaultIsLE) {
+module.exports = function (orderArray, searchItem, isLE = typeof searchItem === "function" ? searchItem : defaultIsLE) {
     for (var cx = 0, dx = orderArray.length, ci = cx + dx >>> 1; cx < dx; ci = cx + dx >>> 1) {
         var orderItem = orderArray[ci];
         if (isLE(orderItem, searchItem)) {
