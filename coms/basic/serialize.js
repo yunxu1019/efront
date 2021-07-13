0a9a27e990da3cd95300167b5d9c9f7bec17e2ca
不枝雀 <yunxu1019@qq.com>
2021-07-13 10:18:34 +0800

    支持转换一些没有等号的参数

--- coms/basic/serialize.js
+++ coms/basic/serialize.js
@@ -3,6 +3,7 @@ function serialize(map, spliter = "&", equals = "=") {
     var encode = spliter === "&" && equals === "=" ? encodeURIComponent : a => a;
     for (var k in map) {
         if (map[k] !== undefined) result.push(encode(k) + equals + encode(map[k]));
+        else result.push(encode(k));
     }
     return result.join(spliter);
 }
