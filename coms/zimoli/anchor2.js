c3a07e017e195402109faf34f2733f69a0f12f3c
不枝雀 <yunxu1019@qq.com>
2021-09-25 10:34:21 +0800

    修复无法正常跳转的问题

--- coms/zimoli/anchor2.js
+++ coms/zimoli/anchor2.js
@@ -1,7 +1,6 @@
 function anchor2(label, href) {
     var a = document.createElement("a");
     a.href = href || "";
-    a.href = "";
     if (isString(label)) {
         a.innerHTML = label;
     } else {
