1b277a43b51c470f8e1d06e03539eafb5b49c3f1
不枝雀 <yunxu1019@qq.com>
2021-10-03 23:34:47 +0800

    删除一些无用代码

--- coms/database/table_test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-describe("table_test",function(){
-    var table
-    it("init",function(){
-        table=require("./table");
-        var user=new table("user");
-        console.log(user);
-    });
-});
