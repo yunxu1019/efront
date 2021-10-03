59241af1ae6a56397d996c72ef02b117ca6d9cda
不枝雀 <yunxu1019@qq.com>
2021-10-04 00:45:28 +0800

    修复一些样式

--- apps/feedback/pages/home.js
+++ apps/feedback/pages/home.js
@@ -1,20 +1,44 @@
 function main() {
+    function loadlist() {
+        data.from("load-list", { selector: { userid: user.id } }, function (data) {
+            $scope.items.unshift.apply($scope.items, data);
+        });
+    }
+    var user = data.getInstance("efrontuser");
+
     var page = view();
     page.innerHTML = home;
+
     var $scope = {
         data: {
             title: '反馈',
             content: '',
         },
+        user,
+        items: [],
         send() {
-            data.from('add', {
+            if (!this.data.content.trim()) return alert.error("无法发送空信息！");
+            var d = {
                 _id: `efront:${+new Date}`,
-                content: this.data.content
+                userid: this.user.id,
+                content: this.data.content,
+            };
+            this.items.push(d);
+            data.from('add', d).loading_promise.then(function (a) {
+                d.loaded = true;
             });
         },
     };
+    if (!user.id) {
+        data.from("ip", function (response) {
+            var id = response.data + "-" + (+new Date / 1000 | 0).toString(32);
+            data.patchInstance("efrontuser", { id }, true);
+            loadlist();
+        });
+    } else {
+        loadlist();
+    }
     renderWithDefaults(page, $scope);
-    page.dragTarget = page.children[0];
-    resize.on(page);
+    resize.on(page, page.children[0]);
     return page;
 }
