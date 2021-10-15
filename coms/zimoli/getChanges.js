525a4b72cf8c060200f86f2763be4f8650111eac
不枝雀 <yunxu1019@qq.com>
2021-10-15 21:04:08 +0800

    调整非对象数据返回的数据结构

--- coms/zimoli/getChanges.js
+++ coms/zimoli/getChanges.js
@@ -1,8 +1,8 @@
 function getChanges(current_props, previous_props) {
     if (!isObject(current_props) && !isObject(previous_props)) return !deepEqual.shallow(current_props, previous_props);
-    if (!isObject(current_props)) return true;
-    if (!isObject(previous_props)) return current_props;
     var changes = null;
+    if (!isObject(current_props)) current_props = Object.create(null), changes = {};
+    if (!isObject(previous_props)) previous_props = Object.create(null), changes = {};
     keys(previous_props, current_props).forEach(function (key) {
         if (!deepEqual.shallow(current_props[key], previous_props[key])) {
             if (!changes) changes = {};
