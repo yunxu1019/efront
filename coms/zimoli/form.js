bd92d4f23d354f6c03b3db79f6f57b0ae14ea9fc
不枝雀 <yunxu1019@qq.com>
2021-10-26 04:02:01 +0800

    禁用原生提交的方法从view移动到form组件

--- coms/zimoli/form.js
+++ coms/zimoli/form.js
@@ -2,5 +2,10 @@ function form(elem) {
     if (!elem) {
         elem = document.createElement("form");
     }
+    if (/form/i.test(elem.tagName)) {
+        on("submit")(elem, function (event) {
+            event.preventDefault();
+        });
+    }
     return elem;
 }
