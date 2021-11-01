ef172b2a0e72155f922fc5216be53abaf6be16ad
不枝雀 <yunxu1019@qq.com>
2021-11-01 22:17:10 +0800

    解决无法从主进程往子进程发送消息的问题

--- coms/message/index.js
+++ coms/message/index.js
@@ -69,7 +69,7 @@ var send, __send = send = function (worker, key, params, onsuccess, onerror, onf
         } while (stamp in callback_maps);
         callback_maps[stamp] = [onsuccess, onerror, onfinish];
     }
-    worker.connected && worker.send([key, JSAM.stringify({
+    (worker.process || worker).connected && worker.send([key, JSAM.stringify({
         params, stamp
     })].join(":"));
 };
