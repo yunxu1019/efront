b53ed73c66e762b9ed10d48680984e02fb958276
不枝雀 <yunxu1019@qq.com>
2021-11-03 23:15:15 +0800

    ?.语法兼容有中括号的语句

--- coms/zimoli/render.js
+++ coms/zimoli/render.js
@@ -51,7 +51,7 @@ function rebuild(element) {
         dispatch(event, element);
     }
 }
-var variableReg = /([^\:\,\+\=\-\!%\^\|\/\&\*\!\;\?\>\<~\{\}\s]|\?\s*\.(?=[^\d])|\s*\.\s*)+/g;
+var variableReg = /([^\:\,\+\=\-\!%\^\|\/\&\*\!\;\?\>\<~\{\}\s\[\]\(\)]|\?\s*\.(?=[^\d])|\s*\.\s*)+/g;
 var createGetter = function (search, isprop = true) {
     var [withContext, searchContext] = search;
     if (!searchContext) return function () { };
@@ -62,12 +62,12 @@ var createGetter = function (search, isprop = true) {
             context.split(/\?\s*\.(?=[^\d])/).forEach(function (search) {
                 if (dist) {
                     if (/[\=]/.test(dist)) dist = `(${dist})`;
-                    dist = `${dist}!==void 0&&${dist}!==null?${dist}.${search}:null`
+                    dist = `${dist}!==void 0&&${dist}!==null?${dist}.${search}:''`
                 } else {
                     dist = search;
                 }
             });
-            return context.length > 1 ? `(${dist})` : context;
+            return dist.length > 1 ? `(${dist})` : context;
         });
     }
     if (isprop) {
