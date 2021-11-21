adde503df487bcf1b779e1ce7377dfd778c3dfba
不枝雀 <yunxu1019@qq.com>
2021-11-22 02:15:31 +0800

    降低读取id的优先级

--- coms/zimoli/getValue.js
+++ coms/zimoli/getValue.js
@@ -1,10 +1,11 @@
 var hasOwnProperty = {}.hasOwnProperty;
 function getValue(o = this) {
+    if (!o) return o;
     if (isFunction(o.getValue)) return o.getValue();
     if (hasOwnProperty.call(o, 'valueOf')) return o.valueOf();
-    if ("id" in o) return o.id;
     if ("key" in o) return o.key;
     if ("value" in o) return o.value;
     if (hasOwnProperty.call(o, 'toString')) return o.toString();
+    if (!isEmpty(o.id)) return o.id;
     return o;
 }
