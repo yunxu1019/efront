ff5f8995ce9753fa6eef65c21ff359087fd85458
不枝雀 <yunxu1019@qq.com>
2021-11-23 05:13:41 +0800

    修改样式

--- apps/pivot/task/invoke.js
+++ apps/pivot/task/invoke.js
@@ -14,16 +14,24 @@ function main(a) {
         fields: fields,
         task: a,
         output: '',
+        error: false,
         async run() {
             var id = ++taskid;
             this.output = "正在执行..";
+            this.error = false;
             var params = JSON.stringify(this.data);
-            var res = await data.from("invoke", { key: a.key, params: encode62.timeencode(params) }, function (a) {
-                return a ? encode62.timedecode(a) : '完成！';
-            });
-            if (id !== taskid) return;
-            this.output = res;
+            try {
+                var res = await data.from("invoke", { key: a.key, params: encode62.timeencode(params) }, function (a) {
+                    return a ? encode62.timedecode(a) : '完成！';
+                });
+                if (id !== taskid) return;
+                this.output = res;
+            } catch (e) {
+                this.output = String(e);
+                this.error = true;
+            }
         }
     });
+    resize.on(page);
     return page;
 }
