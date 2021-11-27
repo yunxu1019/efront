56a53541e1e70111980aa662ef906e799d777ddf
不枝雀 <yunxu1019@qq.com>
2021-11-27 08:03:44 +0800

    支持将已创建的任务同步到其他服务器

---
+++ apps/pivot/task/rsync.js
@@ -0,0 +1,38 @@
+function main(task) {
+    var page = view();
+    page.innerHTML = template;
+    drag.on(page.firstChild, page);
+    renderWithDefaults(page, {
+        task,
+        checkbox(e) {
+            e = checkbox(e);
+            cast(e, { options: this.hosts })
+        },
+        base: data.getInstance("base"),
+        hosts: data.getInstance("hosts"),
+        finished: {},
+        checked: {},
+        async flush() {
+            var api = await data.getApi("edit");
+            api = Object.assign({}, api);
+            var finished = this.finished = {};
+            var checked = this.checked;
+            for (var h of this.hosts) {
+                if (!checked[h.key]) continue;
+                api.base = location.protocol + "//" + h.key + "/";
+                cross.addDirect(api.base);
+                await data.from(api, {
+                    type: "task",
+                    key: encode62.timeencode(task.key),
+                    value: encode62.timeencode(JSON.stringify(task))
+                });
+                finished[h.key] = true;
+            }
+            alert('同步完成！');
+        },
+        remove() {
+            remove(page);
+        },
+    });
+    return page;
+}
