deb92102c489184c1c8af4095260e1f015a13d49
不枝雀 <yunxu1019@qq.com>
2021-12-02 22:17:42 +0800

    修改提示信息

--- coms/efront/filebuilder.js
+++ coms/efront/filebuilder.js
@@ -26,7 +26,7 @@ var efronthook = `function (body, window) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             new Function(xhr.responseText).call(window);
-            console.log("%cEfront%c Start", 'color:#6c2', 'color:333');
+            console.log("%cefront%c start", 'color:#6c2', 'color:333');
         }
     };
     xhr.open('POST', 'comm/main');
