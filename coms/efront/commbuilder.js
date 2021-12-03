15824951d0d3c9b593a5138520357b938a5e4c26
不枝雀 <yunxu1019@qq.com>
2021-12-03 13:49:37 +0800

    减少不必要的提示信息

--- coms/efront/commbuilder.js
+++ coms/efront/commbuilder.js
@@ -291,7 +291,7 @@ var loadJsBody = function (data, filename, lessdata, commName, className, htmlDa
                 code_body.push({ type: code_body.EXPRESS, text: commName });
             }
         } else {
-            if (!/\bmain|\bindex/i.test(path.basename(filename))) {
+            if (!/\bmain|\bindex|\.(jsp|asp|php)$/i.test(path.basename(filename))) {
                 if (filename.length > 48) {
                     filename = ".." + filename.slice(filename.length - 46);
                 }
