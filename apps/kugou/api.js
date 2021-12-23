9209a8ff7ba558a2b950f7df833910f53b5db595
不枝雀 <yunxu1019@qq.com>
2021-12-23 11:25:38 +0800

    修复示例项目kugou

--- apps/kugou/api.js
+++ apps/kugou/api.js
@@ -2,15 +2,18 @@
     "https://m.kugou.com/": {
         "song-info": "get                                 app/i/getSongInfo.php?cmd=playInfo&from=mkugou&hash",
         "slider-src": "get:[].mod-slider>.swipe-wrap>div  .#src=img!src&href=a!href",
-        "songs-list": "get:[].panel-songslist>li          .#hash=!id&.panel-songs-item-name>span!innerText",
-        "rank-list": "get:[].panel-img-list>li            rank/list#href=a!href&=a!href\\rank-info&imgurl=img!_src&name=p|innerText",
-        "rank-info": "get:[].panel-songslist>li           rank/info/:id#src=!id&name=.panel-songs-item-name|innerText&data=.panel-songs-item-download/innerText",
-        "plist-index": "get:[].panel-img-list>li          plist/index#href=a!href&=a!href\\plist-info&imgurl=img!_src&name=.panel-img-content-first|innerText&count=.panel-img-content-sub|innerText",
-        "plist-info": "get:[].panel-songslist>li          plist/list/:id#name=.panel-songs-item-name|innerText&data=.panel-songs-item-download/innerText",
+        "songs-list": "get:[].panel-songslist%20li          .#hash=!id&.panel-songs-item-name>span!innerText",
+        "rank-list": "get:[].panel-img-list%20li            rank/list#href=a!href&=a!href\\rank-info&imgurl=img!_src&name=p|innerText",
+        "rank-info": "get:[].panel-songslist%20li           rank/info/:id#src=!id&name=.panel-songs-item-name|innerText&data=.panel-songs-item-download/innerText",
+        "plist-index": "get:[].panel-img-list%20li          plist/index#href=a!href&=a!href\\plist-info&imgurl=img!_src&name=.panel-img-content-first|innerText&count=.panel-img-content-sub|innerText",
+        "plist-info": "get:[].panel-songslist%20li          plist/list/:id#name=.panel-songs-item-name|innerText&data=.panel-songs-item-download/innerText",
         "search-hot": "get                                api/v3/search/hot?format=json&plat=0&count=30",
         "singer-class": "get:[].bd%20li                   singer/class#href=a!href&=a!href\\singer-list&name=a|innerText&group=?parentNode",
-        "singer-list": "get:[].singer-img-list>li         singer/list/:id#href=a!href&=a!href\\singer-info&imgurl=img!_src&name=p|innerText",
-        "singer-info": "mget:[].singer-songs-list>li       singer/info/:id#hash=!id&singer=.singer-name|innerText&name=.song-name|innerText&data=em/innerText"
+        "singer-list": "get:[].singer-img-list>li         singer/list/:id#href=a!href&=a!href\\singer-info0&imgurl=img!_src&name=p|innerText",
+        "singer-info0": "mget:[].singer-songs-list>li       singer/info/:id#hash=!id&singer=.singer-name|innerText&name=.song-name|innerText&data=em/innerText"
+    },
+    "https://www.kugou.com/": {
+        "singer-info": "cget:link[rel]+script       yy/singer/home/:id.html",
     },
     "http://mobilecdn.kugou.com/": {
         "search?keyword": "get:data.info                            api/v3/search/song?format=json&page=1&pagesize=30&showtype=1"
--- coms/kugou/parseSongsList.js
+++ coms/kugou/parseSongsList.js
@@ -1,10 +1,24 @@
-a => a.map(b => {
-    var data = {};
-    if (b.data) extend(data, {
-        name: b.data.filename.replace(/^[\s\S]*?\s*\-\s*/, ''),
-        singer: b.data.filename.replace(/\s*\-\s*[\s\S]*?$/, '')
-    }, b.data);
-    else extend(data, b);
-    if (data.hash) data.hash = data.hash.replace(/^songs\_/i, '');
-    return data;
-});
+a => {
+    if (isElement(a)) {
+        var m = /songsdata\s*=\s*(\[[\s\S]*\])/.exec(a.innerText);
+        if (!m) throw new Error("无法加载数据！");
+        if (m) a = m[1];
+        a = JSON.parse(a);
+        console.log(a)
+    }
+    return a.map(b => {
+        var data = {};
+        if (b.data) extend(data, {
+            name: b.data.filename.replace(/^[\s\S]*?\s*\-\s*/, ''),
+            singer: b.data.filename.replace(/\s*\-\s*[\s\S]*?$/, '')
+        }, b.data);
+        else extend(data, b);
+        if (!data.singer) {
+            data.singer = data.author_name;
+        }
+        if (!data.name) data.name = data.audio_name;
+        if (data.hash) data.hash = data.hash.replace(/^songs\_/i, '');
+        return data;
+    });
+
+}
