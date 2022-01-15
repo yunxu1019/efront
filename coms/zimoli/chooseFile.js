fa0744d743907989034db93e4b1d5621c456e5b7
不枝雀 <yunxu1019@qq.com>
2022-01-16 04:49:50 +0800

    防止文件窗口造成原生事件的中断

--- coms/zimoli/chooseFile.js
+++ coms/zimoli/chooseFile.js
@@ -27,6 +27,8 @@ function chooseFile(accept, multiple, extra) {
             }
         });
     });
-    input.click();
+    setTimeout(function(){
+        input.click();
+    });
     return result;
 }
