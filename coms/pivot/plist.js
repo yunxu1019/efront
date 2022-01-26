e1fa60495c0a4bec4feb9a9321ebe5ee107d021d
不枝雀 <yunxu1019@qq.com>
2022-01-26 08:42:05 +0800

    空数据不再报错

--- coms/pivot/plist.js
+++ coms/pivot/plist.js
@@ -2,7 +2,7 @@
 function main(title, type, fields, edit_ref, options) {
     return frame$list(title, {
         load() {
-            return data.from("list", { type }, a => JSAM.parse(encode62.timedecode(a)));
+            return data.from("list", { type }, a => JSAM.parse(encode62.timedecode(a || '')));
         },
         remove(o) {
             return data.from("edit", { type, key: encode62.timeencode(o.key), value: encode62.timeencode("") }).loading_promise;
