e25b28ab5b0a78b623a18a6b183301024422f3b4
不枝雀 <yunxu1019@qq.com>
2022-02-05 15:23:58 +0800

    不再修改无法识别的isMounted

--- coms/zimoli/isMounted.js
+++ coms/zimoli/isMounted.js
@@ -2,7 +2,10 @@ function isMounted(parent) {
     if ("isMounted" in parent) return parent.isMounted;
     var temp = parent;
     while (temp && temp !== document.documentElement) {
+        if ("isMounted" in temp) {
+            return parent.isMounted = temp.isMounted;
+        }
         temp = temp.parentNode;
     }
-    return parent.isMounted = !!temp;
+    return !!temp;
 }
