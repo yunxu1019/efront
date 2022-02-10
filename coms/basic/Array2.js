c2402290f25fc22aec0f40c7c1355e42320cb7da
不枝雀 <yunxu1019@qq.com>
2022-02-11 00:32:28 +0800

    解决继承Array的class无法通过设置下标修改length的问题

---
+++ coms/basic/Array2.js
@@ -0,0 +1,9 @@
+var setPrototypeOf = Object.setPrototypeOf || function (obj, proto) {
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
+    for (var p in proto) if (!hasOwnProperty.call(obj)) obj[p] = proto[p];
+    return obj;
+}
+function Array2() {
+    return setPrototypeOf([], this.constructor.prototype);
+}
+Array2.prototype = Array.prototype;
--- coms/efront/downLevel.js
+++ coms/efront/downLevel.js
@@ -3,9 +3,7 @@ module.exports = function (data, isAsync, isYield) {
     if (isYield || isAsync) {
         data = `${isAsync ? "async " : ""}function${isYield ? "*" : ""}(){${data}}`;
     }
-    data = data.replace(/(\sextends\s+)Array(\s||\{)/g, function (_, ex, sq) {
-        return `${ex}(A=>(A.prototype=Array.prototype,A))(function(){})${sq}`;
-    });
+    data = data.replace(/(\sextends\s+)Array(\s||\{)/g, `$1Array2$2`);
     data = typescript.transpile(data, { noEmitHelpers: true });
     if (isYield || isAsync) {
         data = data.replace(/^\s*function\s*\(\s*\)\s*\{\s*([\s\S]*?)\s*\}\s*$/, "$1");
