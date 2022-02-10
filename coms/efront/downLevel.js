effcf38cb4affcd0c24c3f1dd61e312a4dd60c12
不枝雀 <yunxu1019@qq.com>
2022-02-10 19:04:53 +0800

    解决typescript转换的代码无法extends Array的问题

--- coms/efront/downLevel.js
+++ coms/efront/downLevel.js
@@ -3,6 +3,9 @@ module.exports = function (data, isAsync, isYield) {
     if (isYield || isAsync) {
         data = `${isAsync ? "async " : ""}function${isYield ? "*" : ""}(){${data}}`;
     }
+    data = data.replace(/(\sextends\s+)Array(\s||\{)/g, function (_, ex, sq) {
+        return `${ex}(A=>(A.prototype=Array.prototype,A))(function(){})${sq}`;
+    });
     data = typescript.transpile(data, { noEmitHelpers: true });
     if (isYield || isAsync) {
         data = data.replace(/^\s*function\s*\(\s*\)\s*\{\s*([\s\S]*?)\s*\}\s*$/, "$1");
