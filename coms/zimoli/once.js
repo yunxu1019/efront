7001667d4477b8f9ad0cb2eabb7759d6025f78da
不枝雀 <yunxu1019@qq.com>
2022-03-03 00:22:26 +0800

    用法与on操持一致

--- coms/zimoli/once.js
+++ coms/zimoli/once.js
@@ -1,15 +1,16 @@
+"use strict";
 var oncetree = {};
 function _once(key) {
     if (oncetree[key]) return oncetree[key];
-    return oncetree[key] = function (target, handler) {
-        var off = on(key)(target, function (event) {
+    return oncetree[key] = function (target, handler, firstmost) {
+        var off = on(key).call(this, target, function (event) {
             off();
             return handler.call(this, event);
-        });
+        }, firstmost);
         return off;
     };
 }
-function once(key, target, handler) {
-    if (isFunction(handler) && isNode(target)) return _once(key)(target, handler);
+function once(key, target, handler, firstmost) {
+    if (isFunction(handler) && isNode(target)) return _once(key).call(this, target, handler, firstmost);
     return _once(key);
 }
