9c48285cd861fb364d97daef2f0b905fa6f27013
不枝雀 <yunxu1019@qq.com>
2022-03-04 16:16:39 +0800

    默认使用第一个字段的key做为主索引

--- coms/pivot/pedit.js
+++ coms/pivot/pedit.js
@@ -1,11 +1,10 @@
-function main(title, type, params) {
+function main(title, type, params, idkey = params.fields[0].key) {
     return frame$edit(title, {
         submit(a, fields) {
-            
             a = submit(fields, a);
             return data.from(params.data ? "edit" : "add", {
                 type,
-                key: encode62.timeencode(a.key),
+                key: encode62.timeencode(a[idkey]),
                 value: encode62.timeencode(JSON.stringify(a)),
             }).loading_promise;
         },
--- coms/pivot/plist.js
+++ coms/pivot/plist.js
@@ -1,11 +1,11 @@
 
-function main(title, type, fields, edit_ref, options) {
+function main(title, type, fields, edit_ref, options, idkey = fields[0].key) {
     return frame$list(title, {
         load() {
             return data.from("list", { type }, a => JSAM.parse(encode62.timedecode(a || '')));
         },
         remove(o) {
-            return data.from("edit", { type, key: encode62.timeencode(o.key), value: encode62.timeencode("") }).loading_promise;
+            return data.from("edit", { type, key: encode62.timeencode(o[idkey]), value: encode62.timeencode("") }).loading_promise;
         },
         fields,
         options,
