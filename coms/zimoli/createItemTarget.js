d9f7a12d714ad42e4ac4e4aa828838703d9ac53b
不枝雀 <yunxu1019@qq.com>
2022-03-08 23:30:12 +0800

    增加几个属性

--- coms/zimoli/createItemTarget.js
+++ coms/zimoli/createItemTarget.js
@@ -1,7 +1,13 @@
-function createItemTarget(item) {
+function createItemTarget(item, target) {
     var $scope = {};
-    var { itemName } = this.$src;
+    var { itemName, indexName, keyName } = this.$src;
     if (itemName) $scope[itemName] = item;
     else $scope.$item = item;
+    if (target && target.$scope) {
+        if (indexName) $scope[indexName] = target.$scope[indexName];
+        if (keyName) $scope[keyName] = target.$scope[keyName];
+        $scope.$index = target.$scope.$index;
+        $scope.$key = target.$scope.$key;
+    }
     return { $scope };
 }
