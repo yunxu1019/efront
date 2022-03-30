64d82a628ce02a851cd5c665f832d21577ecde22
不枝雀 <yunxu1019@qq.com>
2022-03-30 22:38:55 +0800

    完成大数加法和大数乘法

---
+++ coms/basic/BigNumber.js
@@ -0,0 +1,146 @@
+var trim = function (a) {
+    return String(a).replace(/[\s\,]/g, '').replace(/^\+/, '').replace(/[mni]$/i, '');
+};
+var prepare = function (a) {
+    a = trim(a);
+    var neg = /^\-/.test(a);
+    if (neg) a = a.slice(1);
+    var [s1, s2 = ''] = a.split('.');
+    return [neg, s1, s2];
+};
+var repeat = String.prototype.repeat || function (n) {
+    return new Array(n + 1).join(this);
+};
+var repeat0 = function (n) {
+    return repeat.call('0', n);
+};
+var split = function (s, n) {
+    var r = [];
+    var end = s.length;
+    while (end > n) {
+        r.push(s.slice(end - n, end));
+        end -= n;
+    }
+    r.push(s.slice(0, end));
+    return r;
+};
+var fix = function (neg, res, nl) {
+    if (nl > 0) res = res.slice(0, res.length - nl) + "." + res.slice(res.length - nl).replace(/0+$/, '');
+    res = res.replace(/^0+(?!\.)/, '');
+    if (/^\./.test(res)) res = '0' + res;
+    if (neg) res = '-' + res;
+    return res.replace(/\.$/, '');
+};
+class BigNumber {
+    constructor(value) {
+        if (!this || this.constructor !== BigNumber) {
+            return new BigNumber(value);
+        }
+        this.value = value;
+    }
+    toString() {
+        return this.value;
+    };
+    add(bignumber) {
+        return new BigNumber(BigNumber.add(this.value, bignumber));
+    };
+    static add(numstr1, numstr2) {
+        var [neg1, s11, s12] = prepare(numstr1)
+        var [neg2, s21, s22] = prepare(numstr2)
+        var nl = 0;
+        if (s12.length < s22.length) {
+            nl = s22.length;
+            s12 += repeat0(nl - s12.length);
+        }
+        else if (s22.length < s12.length) {
+            nl = s12.length;
+            s22 += repeat0(nl - s22.length);
+        }
+        else {
+            nl = s12.length;
+        }
+        if (s11.length < s21.length) {
+            s11 = repeat0(s21.length - s11.length) + s11;
+        }
+        else if (s11.length > s21.length) {
+            s21 = repeat0(s11.length - s21.length) + s21;
+        }
+        numstr1 = s11 + s12;
+        numstr2 = s21 + s22;
+        var flag = 1;
+        if (neg1 ^ neg2) {
+            if (numstr1 > numstr2) {
+                neg = neg1;
+            }
+            else if (numstr1 < numstr2) {
+                neg = neg2;
+                [numstr1, numstr2] = [numstr2, numstr1];
+            }
+            flag = 0;
+        }
+        else {
+            var neg = neg1 && neg2;
+        }
+        var res = [];
+        var end = (numstr1.length / 15 | 0) * 15;
+        var rest = 0;
+        while (end >= 0) {
+            var s1 = numstr1.slice(end, end + 15);
+            var s2 = numstr2.slice(end, end + 15);
+            var s = +s1 + (flag ? +s2 : -s2) + rest;
+            if (s < 0) {
+                s = +("1" + repeat0(s1.length)) + s;
+                rest = -1;
+            }
+            else {
+                rest = 0;
+            }
+            s = s.toFixed(0);
+            if (s.length > s1.length) {
+                rest = 1;
+                res.push(s.slice(1))
+            }
+            else {
+                if (s.length < s1.length) s = repeat0(s1.length - s.length) + s;
+                res.push(s);
+            }
+            end -= 15;
+        }
+        if (rest) res.push(rest);
+        res = res.reverse().join('');
+        return fix(neg, res, nl);
+    }
+    static prd(numstr1, numstr2) {
+        var [neg1, s11, s12] = prepare(numstr1);
+        var [neg2, s21, s22] = prepare(numstr2);
+        var d = s12.length * s22.length;
+        numstr1 = s11 + s12;
+        numstr2 = s21 + s22;
+        if (numstr1.length < numstr2.length) [numstr1, numstr2] = [numstr2, numstr1];
+        var ns1 = split(numstr1, 7);
+        var ns2 = split(numstr2, 7);
+        var res = [];
+        var i = 0;
+        for (var cx = 0, dx = ns1.length; cx < dx; cx++) {
+            var s1 = ns1[cx];
+            for (var cy = 0, dy = ns2.length; cy < dy; cy++) {
+                var s2 = ns2[cy];
+                var i = cx + cy;
+                var rest = +res[i] || 0;
+                var s = s1 * s2 + rest;
+                s = String(s);
+                if (s.length > 7) {
+                    rest = +s.slice(0, s.length - 7);
+                    res[i] = s.slice(s.length - 7);
+                    res[i + 1] = (+res[i + 1] || 0) + rest;
+                }
+                else {
+                    if (s.length < 7) s = repeat0(7 - s.length) + s;
+                    res[i] = s;
+                }
+            }
+        }
+        res = res.reverse().join('');
+        return fix(neg1 ^ neg2, res, d);
+    }
+}
---
+++ coms/basic/BigNumber_test.js
@@ -0,0 +1,29 @@
+var testAdd = function (a, b, s) {
+    assert(BigNumber.add(a, b), s, `${a} + ${b}`);
+};
+var testPrd = function (a, b, s) {
+    assert(BigNumber.prd(a, b), s, `${a} * ${b}`);
+};
+testAdd(0x1fffffffffffff, 1, '9007199254740992');
+testAdd(0x1fffffffffffff, 2, '9007199254740993');
+testAdd(0x1fffffffffffff, 3, '9007199254740994');
+testAdd(0x1fffffffffffff, 3, '9007199254740994');
+testAdd("900719925474099.2", 0.1, '900719925474099.3');
+testAdd("900719925474099.2", 0.1, '900719925474099.3');
+testAdd("900719925474099.3", -0.2, '900719925474099.1');
+testAdd("900719925474099.39007199254740992", "-900719925474099.39007199254740992", '0');
+testAdd("900719925474099.39007199254740992", 0.2, '900719925474099.59007199254740992');
+testAdd("900719925474099.39007199254740992", "-900719925474099.39007", '0.00000199254740992');
+testAdd("90071992547409939007.199254740992", 0.2, '90071992547409939007.399254740992');
+testAdd("-90071992547409939007.199254740992", 0.2, '-90071992547409939006.999254740992');
+var getrandomint = function () {
+    var n = BigInt(Math.random() * new Date | 0) * BigInt(Math.random() * new Date | 0) * BigInt(Math.random() * new Date | 0);
+    return n;
+}
+for (var cx = 0, dx = 1000; cx < dx; cx++) {
+    var n1 = getrandomint();
+    var n2 = getrandomint();
+    var s = String(n1 + n2);
+    testAdd(n1, n2, s);
+    testPrd(n1, n2, String(n1 * n2));
+}
