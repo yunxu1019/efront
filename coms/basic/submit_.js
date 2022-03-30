ab3290ea67ed799e7743d57a788e9c614d0d347c
不枝雀 <yunxu1019@qq.com>
2022-03-30 20:23:52 +0800

    自动补齐小数位

--- coms/basic/submit_.js
+++ coms/basic/submit_.js
@@ -21,6 +21,25 @@ function submit(fields, data) {
         }
         else {
             params[f.key] = data[f.key];
+            var tmp = /\:(\d+)?(?:\.(\d+))?$/.exec(f.type);
+            if (tmp) var [, , fixed] = tmp;
+            else if (/^(money|price)$/i.test(f.type)) fixed = 2;
+            fixed = +fixed;
+            if (fixed) {
+                var v = params[f.key];
+                if (v) {
+                    var d = v.replace(/^[^\.]*\.?/, '').length;
+                    if (d === v.length - 1) v = "0" + v;
+                    if (d > 0 && d < fixed) {
+                        v = v + new Array(fixed + 1 - d).join("0");
+                    }
+                    else if (d === 0) {
+                        v = v.replace(/\.$/, '') + '.' + new Array(fixed + 1).join("0");
+                    }
+                    params[f.key] = v;
+                }
+            }
+
         }
     }
     if (checks.length + select.length + inputs.length) {
--- coms/basic/valid.js
+++ coms/basic/valid.js
@@ -6,6 +6,27 @@ function valid(field, data) {
         }
         return;
     }
+    var tmp;
+    if (tmp = /\:(\d+)(?:\.(\d+))?$/.exec(field.type)) {
+        var [, a, b] = tmp;
+        var value = data[field.key];
+        a = +a;
+        b = +b;
+        if (a) {
+            if (!b) {
+                if (value.length > a) return `不能超过${a}个字符`;
+            }
+            else {
+                if (value.replace(/\.[\s\S]*$/, '').length > a) return `整数部分不能超过${a}位`;
+            }
+        }
+        if (b) {
+            var digits = value.replace(/^[^\.]+\./, '');
+            if (digits.length > b) {
+                return `小数部分不能超过${b}位`;
+            }
+        }
+    }
     if (field.valid instanceof Function) {
         var e = field.valid(data[field.key]);
         if (e) return e;
