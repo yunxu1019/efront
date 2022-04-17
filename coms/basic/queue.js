c3628e7d8e1d1b9b5f11ff588edb23e3193b7643
不枝雀 <yunxu1019@qq.com>
2022-04-17 17:41:16 +0800

    非异步的队列执行方式修改

--- coms/basic/queue.js
+++ coms/basic/queue.js
@@ -13,39 +13,36 @@ function queue(list, count = 1, context = null) {
     return new Promise(function (ok, oh) {
         var cx = 0;
         var result = [];
-        var loaded_count = 0;
+        var promised = false;
         var error_count = 0;
         var reject = function (e) {
             error_count++;
             oh(e);
         };
         var next = function () {
-            loaded_count++;
             run();
         };
         var run = function () {
-            if (error_count && count === 1) return;
-            if (cx >= list.length) return Promise.all(result).then(ok, oh);
+            if (error_count && count === 1) return promised = null;
+            if (cx >= list.length) return promised = null, count = 0, Promise.all(result).then(ok, oh);
             var saved_cx = cx;
             var args = list[cx];
             try {
                 result[saved_cx] = f.call(context, args, cx++, list);
             } catch (e) {
                 oh(e);
-                return;
+                return promised = null;
             }
-            Promise.resolve(result[saved_cx]).then(next, reject);
+            if (!promised && (!result[saved_cx] || !isFunction(result[saved_cx].then)));
+            else Promise.resolve(result[saved_cx]).then(next, reject), promised = true;
         };
-        if (count > list.length >> 1) {
-            count = list.length >> 1;
+        if (count > list.length) {
+            count = list.length;
         }
         if (!(count >= 1)) {
             count = 1;
         }
-        if (list.length === 0) run();
-        else while (cx < count) {
-            run();
-        }
+        while (promised === false || count-- > 0) run();
     });
 }
 module.exports = queue;
