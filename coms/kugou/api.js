0cdc184dd0f6bc4f732d03e08c5d0f44981e6c1f
不枝雀 <yunxu1019@qq.com>
2022-04-29 11:08:29 +0800

    修改song-mix配置

--- coms/kugou/api.js
+++ coms/kugou/api.js
@@ -4,7 +4,7 @@
         "slider-src": "get:[].mod-slider>.swipe-wrap>div  .#src=img!src&href=a!href",
         "songs-list": "get:[].panel-songslist%20li          .#hash=!id&.panel-songs-item-name>span!innerText",
         "songs-list": "get:[].m_cm_item1warp:nth-child(2)>div .#=a!href\\song-mix&a:nth-child(2)>p:first-child!innerText&singer=a:nth-child(2)>p:nth-child(2)!innerText&imgurl=img!_src",
-        "song-mix;": "get:script:nth-last-child(2)           mixsong/:hashid.html",
+        "song-mix;": "mget:script:nth-last-child(2)           mixsong/:hashid.html",
         "rank-list": "get:[].panel-img-list%20li            rank/list#href=a!href&=a!href\\rank-info&imgurl=img!_src&name=p|innerText",
         "rank-info": "get:[].panel-songslist%20li           rank/info/:id#src=!id&name=.panel-songs-item-name|innerText&data=.panel-songs-item-download/innerText",
         "plist-index": "get:[].panel-img-list%20li          plist/index#href=a!href&=a!href\\plist-info&imgurl=img!_src&name=.panel-img-content-first|innerText&count=.panel-img-content-sub|innerText",
