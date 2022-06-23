1c77ecf90fd5ff7076efa73659ee7855d8cf0c8b
不枝雀 <yunxu1019@qq.com>
2022-06-24 00:23:05 +0800

    serialize支持传入编码工具

--- coms/basic/parseKV.js
+++ coms/basic/parseKV.js
@@ -1,16 +1,27 @@
 "use strict";
 var trim = a => a.trim();
-function parseKV(string, spliter = "&", equals = "=", decode) {
+function parseKV(string, spliter = "&", equals = "=", decode = spliter === "&" && equals === "=" ? decodeURIComponent : trim) {
     var object = {};
-    if (!decode) decode = spliter === "&" && equals === "=" ? decodeURIComponent : trim;
     if (typeof string === "string") {
-        var kvs = string.split(spliter);
-        for (var cx = 0, dx = kvs.length; cx < dx; cx++) {
-            var kv = kvs[cx];
-            if (!kv) continue;
-            var index = kv.indexOf(equals);
-            if (index < 0) index = kv.length;
-            object[decode(kv.slice(0, index))] = index === kv.length ? undefined : decode(kv.slice(index + 1));
+        if (isFunction(decode)) {
+            var kvs = string.split(spliter);
+            for (var cx = 0, dx = kvs.length; cx < dx; cx++) {
+                var kv = kvs[cx];
+                if (!kv) continue;
+                var index = kv.indexOf(equals);
+                if (index < 0) index = kv.length;
+                object[decode(kv.slice(0, index))] = index === kv.length ? undefined : decode(kv.slice(index + 1));
+            }
+        }
+        else{
+            var kvs = string.split(spliter);
+            for (var cx = 0, dx = kvs.length; cx < dx; cx++) {
+                var kv = kvs[cx];
+                if (!kv) continue;
+                var index = kv.indexOf(equals);
+                if (index < 0) index = kv.length;
+                object[kv.slice(0, index)] = index === kv.length ? undefined : kv.slice(index + 1);
+            }
         }
     }
     return object;
--- coms/basic/serialize.js
+++ coms/basic/serialize.js
@@ -1,9 +1,15 @@
-function serialize(map, spliter = "&", equals = "=") {
+var encodeURIComponent = a => a.replace(/[^\w\.\-_~]/g, a => "%" + a.charCodeAt().toString(16).toUpperCase());
+function serialize(map, spliter = "&", equals = "=", encode = spliter === "&" && equals === "=" ? encodeURIComponent : a => a) {
     var result = [];
-    var encode = spliter === "&" && equals === "=" ? encodeURIComponent : a => a;
-    for (var k in map) {
-        if (map[k] !== undefined) result.push(encode(k) + equals + encode(map[k]));
-        else result.push(encode(k));
+    if (isFunction(encode)) {
+        for (var k in map) {
+            if (map[k] !== undefined) result.push(encode(k) + equals + encode(map[k]));
+            else result.push(encode(k));
+        }
+    }
+    else {
+        for (var k in map) if (map[k] !== undefined) result.push(k + equals + map[k]);
+        else result.push(k);
     }
     return result.join(spliter);
 }
