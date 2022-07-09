9e347d734656e172f5285c6f3083c3fa15a82488
不枝雀 <yunxu1019@qq.com>
2022-07-09 18:04:28 +0800

    支持解码arraybuffer类型的数据

--- coms/basic/bdecode.js
+++ coms/basic/bdecode.js
@@ -33,6 +33,39 @@ function decode(str, i) {
     }
     return [, str.length]
 }
+var _indexOf = Array.prototype.indexOf;
+function decodeArrayBuffer(a, i) {
+    var e = i;
+    switch (a[i]) {
+        case 105://i
+            e = _indexOf.call(a, 101/*e*/, i);
+            if (e < 0) e = a.length;
+            return [+decodeUTF8(a.slice(i + 1, e)), e + 1];
+        case 100://d
+            var res = {};
+            i++;
+            while (i < a.length && a[i] !== 101) {
+                var [k, i] = decodeArrayBuffer(a, i);
+                var [v, i] = decodeArrayBuffer(a, i);
+                k = decodeUTF8(k);
+                res[k] = v;
+            }
+            return [res, i + 1];
+        case 108://l
+            var res = [];
+            i++;
+            while (i < a.length && a[i] !== 101) {
+                var [o, i] = decodeArrayBuffer(a, i);
+                res.push(o);
+            }
+            return [res, i + 1];
+        default:
+            e = i;
+            while (a[i] >= 48/*0*/ && a[i] <= 57/*9*/) i++;
+            var l = +String.fromCharCode.apply(String, a.slice(e, i));
+            return [a.slice(++i, i += l), i];
+    }
+}
 function bdecode(str) {
-    return decode(str, 0)[0];
+    return str instanceof ArrayBuffer ? decodeArrayBuffer(new Uint8Array(str), 0)[0] : decode(str, 0)[0];
 }
