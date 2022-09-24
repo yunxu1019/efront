41aba02a3a714b442dc2e749499a3bdcf38ec5df
不枝雀 <yunxu1019@qq.com>
2022-09-25 03:13:18 +0800

    添加awaitable工具

---
+++ coms/basic/awaitable.js
@@ -0,0 +1,41 @@
+/**
+ * 为可能正在加载的元素添加then方法
+ * 占用onload onerror complete then 共4个属性
+ * @param {Element|Object|any} image;
+ */
+function awaitable(image) {
+    if (image.complete) return image;
+    var oks = [];
+    var ohs = [];
+    var resolved = false,
+        errored = false,
+        error = null;
+    var fire = function () {
+        if (resolved || errored) {
+            var _oks = oks.splice(0, oks.length);
+            var _ohs = ohs.splice(0, ohs.length);
+            if (errored) for (var o of _ohs) o(error);
+            delete image.then;
+            if (resolved) for (var o of _oks) o(image);
+            image.then = then;
+        }
+    };
+    var then = image.then = function (ok, oh) {
+        if (ok) oks.push(ok);
+        if (oh) ohs.push(oh);
+        fire();
+    };
+    image.onload = function () {
+        if (resolved || errored) return;
+        resolved = true;
+        if (!image.complete) image.complete = true;
+        fire();
+    };
+    image.onerror = function (e) {
+        if (resolved || errored) return;
+        errored = true;
+        error = e;
+        fire();
+    };
+    return image;
+}
