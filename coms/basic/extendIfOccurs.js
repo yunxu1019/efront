1f45987ea55f48c33444290bec662248d39434f6
不枝雀 <yunxu1019@qq.com>
2022-09-30 14:14:07 +0800

    extendIfOccurs忽略只读属性

--- coms/basic/extendIfOccurs.js
+++ coms/basic/extendIfOccurs.js
@@ -1,7 +1,15 @@
 var isObject = require("./isObject");
 function extendIfOccurs(o1) {
+    var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;// ie8 存在此方法
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
     o1 = Object(o1);
-    var ks = Object.keys(o1);
+    var ks = [];
+    for (var k in o1) {
+        if (!hasOwnProperty.call(o1, k)) continue;
+        var f = getOwnPropertyDescriptor(o1, k);
+        if (!f.writable && !f.set) continue;
+        ks.push(k);
+    }
     for (var cx = 1, dx = arguments.length; cx < dx; cx++) {
         var o2 = arguments[cx];
         if (isObject(o2)) for (var k of ks) {
