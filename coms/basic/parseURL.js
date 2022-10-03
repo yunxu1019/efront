7b22975f60b8311a3b16ef7f514ca74cc91cae3a
不枝雀 <yunxu1019@qq.com>
2022-10-04 00:43:12 +0800

    自动重组href属性

--- coms/basic/parseURL.js
+++ coms/basic/parseURL.js
@@ -19,9 +19,28 @@
 
 // -------/// ---------------1---------------------------------///////////////////2-----3--------//////// ------4----2/////5---------------------------------------------------6-----------------------------------------------------------------------------//////////////-7--5///8-------9----------//10--11---10/8/--12----////
 var reg = /^([^\:\/\\\?#\[]+\:(?![^\:\/\\\?\#]*@|[\/\\][^\/\\]))?(?:\/\/|\\\\)?(?:(([^\:\/\\\?#]+)?(?:\:([^\/\\\?#]+))?)@)?(([^\/\\@\?\#\.]*?[^\/\\@\:\?\#\.\d][^\/\\@\:\?\#\.]*?|[^\/\\@\:\?\#\.]+(?:\:[\@\/\\\?#\.]*[^\d\@\:\/\\\?#\.]+|\.[^\/\\@\:\?\#]+?))?(?:(?:\:|^)(\d+))?)(((?:\/|\\|^)[^\?#]*)?(\?([^#]*))?)(#[\s\S]*)?$/;
-function parseURL(url) {
+var hrefDescriptor = {
+    get() {
+        var href = '';
+        if (this.protocol) href += this.protocol;
+        if (this.auth) href += "//" + this.auth + "@" + this.host;
+        else if (this.host) href += "//" + this.host;
+        if (this.path) href += this.path;
+        if (this.hash) href += this.hash;
+        return href;
+    },
+    set(href) {
+        Object.assign(this, parseURL(href));
+    }
+};
+function parse(url) {
     if (url === undefined || url === null) url = '';
     var [__, protocol, auth, username, password, host, hostname, port, path, pathname, search, query, hash] = reg.exec(url);
-    return { protocol, auth, username, password, host, hostname, port, path, pathname, search, query, hash, href: __ };
+    return { protocol, auth, username, password, host, hostname, port, path, pathname, search, query, hash };
+}
+function parseURL(url) {
+    var obj = parse(url);
+    Object.defineProperty(obj, "href", hrefDescriptor);
+    return obj;
 }
 module.exports = parseURL;
