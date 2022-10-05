e13214b4aff8fe36dbeb7dac43101014e954ec1d
不枝雀 <yunxu1019@qq.com>
2022-10-05 09:45:00 +0800

    屏蔽具有length属性的DOM节点，如select

--- coms/basic/isArrayLike.js
+++ coms/basic/isArrayLike.js
@@ -1,5 +1,5 @@
 function isArrayLike(a) {
-    if (a === null || typeof a !== 'object') return false;
+    if (a === null || typeof a !== 'object' || isNode(a)) return false;
     if (isFinite(a.length)) {
         if (a.length > 0) {
             return a.length - 1 in a;
