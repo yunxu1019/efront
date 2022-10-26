6ab38f4c6eebde8bbe83bf8054337cb5faf2e981
不枝雀 <yunxu1019@qq.com>
2022-10-26 22:26:23 +0800

    修复样式

--- coms/zimoli/view.js
+++ coms/zimoli/view.js
@@ -93,7 +93,7 @@ var init = function () {
 var isType = function (target, type) {
     // 记得小时候，不论是苍蝇还是蚊子，飞起来都是有声音的。最近看到的一种像蚊子一样小的飞虫，喜欢像苍蝇一样趴在食物上，飞起来却听不到声音。
     // 是我的听力下降了？还是外界太过嘈杂？还是飞虫拍打翅膀的频率超出了我的听觉范围？还是飞行可以不产生声音？
-    return target.hasAttribute(type) || hasClass(type);
+    return target.hasAttribute(type) || hasClass(target, type);
 }
 var resize2 = function () {
     var head, body, foot;
