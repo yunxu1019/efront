4858a2ef8a6d74964d87afa8d7730bca306ee98c
不枝雀 <yunxu1019@qq.com>
2022-10-28 17:20:41 +0800

    isArrayLike忽略window对象

--- coms/basic/isArrayLike.js
+++ coms/basic/isArrayLike.js
@@ -1,5 +1,5 @@
 function isArrayLike(a) {
-    if (a === null || typeof a !== 'object' || isNode(a)) return false;
+    if (a === null || a === window || typeof a !== 'object' || isNode(a)) return false;
     if (isFinite(a.length)) {
         if (a.length > 0) {
             return a.length - 1 in a;
