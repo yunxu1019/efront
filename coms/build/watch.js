f6122892fbe525e4cf7fd8084c8ac69b58d19968
不枝雀 <yunxu1019@qq.com>
2022-11-06 18:48:48 +0800

    删除无用的代码

--- coms/build/watch.js
+++ coms/build/watch.js
@@ -4,8 +4,6 @@ require("../efront/console");
 var lazy = require("../basic/lazy");
 var environment = require("./environment");
 var fs = require("fs");
-var path = require("path");
-if (!process.cwd() === path.dirname(__dirname)) throw new Error("请在项目根目录启动！");
 var progress = require("./progress");
 var {
     pages_root,
@@ -17,7 +15,6 @@ var listener = lazy(() => progress(true), 1000);
     var recursive = /^(darwin|win32)$/.test(process.platform);
     if (!recursive) console.warn("watch功能在当前操作系统可能无法使用！");
     if (fs.existsSync(rootpath)) fs.watch(rootpath, { recursive }, listener);
-
 });
 progress(true);
 console.info("efront watch ..");
