000befc486f2506163e70ac40db919c9003d06af
不枝雀 <yunxu1019@qq.com>
2022-12-15 00:13:00 +0800

    修改removeFromList的返回值

--- coms/basic/removeFromList.js
+++ coms/basic/removeFromList.js
@@ -1,8 +1,10 @@
 function removeFromList(list, item) {
+    var count = 0;
     for (var cx = list.length - 1; cx >= 0; cx--) {
         if (list[cx] === item) {
+            count++;
             list.splice(cx, 1);
         }
     }
-    return list;
+    return count;
 }
