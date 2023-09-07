b3887acbf359c31829ccb03cf185c011cefc3235
不枝雀 <yunxu1019@qq.com>
2023-09-07 12:53:38 +0800

    空属性时不再显示undefined

--- coms/zimoli/marker.js
+++ coms/zimoli/marker.js
@@ -1,7 +1,7 @@
 function marker(e) {
     if (!e) e = document.createElement("marker");
     on("changes")(e, function () {
-        e.innerHTML = mark(this.source, this.search);
+        e.innerHTML = isEmpty(this.source) ? "" : mark(this.source, this.search);
     });
     return e;
 }
