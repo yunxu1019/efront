81739045d95f0ab541a752fcc6d0528daa86a012
不枝雀 <yunxu1019@qq.com>
2023-09-18 16:58:34 +0800

    修改判断NaN的方法

--- coms/basic/isSame.js
+++ coms/basic/isSame.js
@@ -1,3 +1,4 @@
+var isNaN = Number.isNaN;
 function isSame(o1, o2) {
-    return o1 === o2 || isNumber(o1) && isNumber(o2) && isNaN(o1) && isNaN(o2);
+    return o1 === o2 || isNaN(o1) && isNaN(o2);
 }
