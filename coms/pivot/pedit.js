c98cadd65ebc25f89abff450bdc1dfcc80e7d1a5
不枝雀 <yunxu1019@qq.com>
2023-09-19 13:05:58 +0800

    拆分两个功能

--- coms/pivot/pedit.js
+++ coms/pivot/pedit.js
@@ -1,13 +1,22 @@
-function main(title, type, params, idkey = params.fields[0].key) {
+function send(type, key, value, origin) {
+    return data.from(origin ? "edit" : "add", {
+        type,
+        key: encode62.timeencode(key),
+        value: encode62.timeencode(JSON.stringify(value)),
+    }).loading_promise;
+}
+function pedit(title, type, params, idkey = params.fields[0].key) {
     return frame$edit(title, {
         submit(a, fields) {
             a = submit(fields, a);
-            return data.from(params.data ? "edit" : "add", {
-                type,
-                key: encode62.timeencode(a[idkey]),
-                value: encode62.timeencode(JSON.stringify(a)),
-            }).loading_promise;
+            return send(type, a[idkey], a, params.data);
         },
 
     }, params);
+}
+pedit.create = function (type, key, value) {
+    return send(type, key, value, false);
+}
+pedit.update = function (type, key, value) {
+    return send(type, key, value, true);
 }
