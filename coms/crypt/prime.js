abf2cf7d66879184b08c71e0570846bbc377c60e
不枝雀 <yunxu1019@qq.com>
2024-03-05 09:29:40 +0800

    添加几个工具函数

--- coms/crypt/prime.js
+++ coms/crypt/prime.js
@@ -11,14 +11,72 @@ var updatePrime = function (m) {
         prime.push(n);
     }
 }
-var isPrime = function (s) {
+var isPrime = function (s, int) {
+    return 最小素约数(s, int) === s;
+};
+
+var 分解因数 = function (s) {
+    var r = [];
+    do {
+        var d = 最小素约数(s);
+        r.push(d);
+        s = s / d;
+    } while (s >= d);
+    return r;
+};
+var 公约数 = function (a, b) {
+    var r1 = 分解因数(a);
+    var r2 = 分解因数(b);
+    var i1 = 0, i2 = 0;
+    var s = [];
+    while (i1 < r1.length && i2 < r2.length) {
+        var a1 = r1[i1], a2 = r2[i2];
+        if (a1 === a2) {
+            i1++, i2++;
+            s.push(a1);
+            continue;
+        }
+        if (a1 < a2) {
+            i1++;
+        }
+        else {
+            i2++;
+        }
+    }
+    return s;
+};
+
+var 最小素约数 = function (s, int = typeof s === 'bigint' ? BigInt : Number) {
     var n = Math.sqrt(Number(s));
-    s = BigInt(s);
+    var 零 = int(0);
     if (n > prime[prime.length - 1]) updatePrime(n);
-    for (var cx = 0; prime[cx] < n; cx++)if (s % BigInt(prime[cx]) === 0n) return false;
-    return true;
+    for (var cx = 0; prime[cx] <= n; cx++)if (s % int(prime[cx]) === 零) return int(prime[cx]);
+    return s;
+};
+
+var 测试梅森素数 = function (p) {
+    // 仅bigint;
+    if (!isPrime(p)) return false;
+    if (typeof p !== 'bigint') p = BigInt(p);
+    var 二 = BigInt(2);
+    var 一 = BigInt(1);
+    var 零 = BigInt(0);
+    var Mp = 二 ** p - 一;
+    var 四 = BigInt(4);
+    var L0 = 四;
+    var Lm = n => (n * n - 二) % Mp;
+    var Ln = L0;
+    for (var cx = 一, dx = p - 一; cx < dx; cx++) {
+        Ln = Lm(Ln);
+    }
+    return Ln === 零;
 };
 prime.isPrime = isPrime;
+prime.mtest = 测试梅森素数;
+prime.minFactor = 最小素约数;
+prime.factorize = 分解因数;
+prime.cd = 公约数;
+
 prime.find = function (start, delta, end) {
     var isBigInt = typeof start === 'bigint' || typeof end === 'bigint';
     var n = [0, 1, 2];
--- coms/crypt/prime_test.js
+++ coms/crypt/prime_test.js
@@ -1,10 +1,37 @@
 var test = function (a, b, c) {
     var start = new Date;
-    prime.find(a, b, c)
+    var r = prime.find(a, b, c)
     var end = new Date;
-    console.log(a, b, c, '用时', end - start);
+    var n = console.format("<yellow>;</yellow>").split(';');
+    var p = console.format("<green>;</green>").split(';');
+
+    if (r && r.length) console.log(console.format(`从${n.join(a)}每次加${n.join(b)}到${n.join(c)}，用时${n.join(end - start)}毫秒，共找到${n.join(r.length)}个素数，最后一个是${p.join(r[r.length - 1])}`));
+    else console.log(`从${n.join(a)}每次加${n.join(b)}到${n.join(c)}，用时${n.join(end - start)}毫秒，没找到素数`);
+};
+var testFactorize = function (s) {
+    var is = prime.factorize(s);
+    console.log(s, "因数分解为：", is);
+};
+testFactorize(2n * 3n * 5n * 7n * 11n * 13n + 1n);
+testFactorize(2n * 3n * 5n * 7n * 11n * 13n);
+testFactorize(2n ** 4n * 3n * 5n * 7n ** 2n * 11n * 13n);
+testFactorize(2n * 3n * 5n * 7n * 11n * 13n - 1n);
+testFactorize(2n * 3n * 5n * 7n * 11n * 13n * 17n * 19n * 23n * 29n * 31n - 1n);
+testFactorize(2n * 3n * 5n * 7n * 11n * 13n * 17n * 19n * 23n * 29n * 31n + 1n);
+testFactorize(2n * 3n * 5n * 7n * 11n * 13n * 17n * 19n * 23n * 29n * 31n * 37n * 41n - 1n);
+testFactorize(2n * 3n * 5n * 7n * 11n * 13n * 17n * 19n * 23n * 29n * 31n * 37n * 41n + 1n);
+console.log(prime.cd(9, 9))
+console.log(prime.cd(9, 27))
+console.log(prime.cd(9, 12))
+console.log(prime.cd(2024, 1992))
+test(19000000n, 1, 21000000n);
+test(19000000, 1, 21000000);
+test(0x1ffffffffff0ff, 2, 0x1fffffffffffff);
+test(0x1f202403050355n, 2, 0x1f2024030503f5n);
+console.log("已缓存的素数的个数", prime.length);
+for (var p of prime) {
+    console.test("正在测试：", console.format(`<yellow>M(<gray>${p}</gray>)</yellow>`));
+    var a = prime.mtest(p);
+    if (a) console.pass("发现梅森素数：", console.format(`<yellow>M(<green>${p}</green>)</yellow>`));
 }
-test(19000000n, 1, 21000000n);
-test(19000000, 1, 21000000);
-test(19000000, 1, 21000000);
-test(19000000n, 1, 21000000n);
+
