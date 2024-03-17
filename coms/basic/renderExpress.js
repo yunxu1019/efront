5f5ba98e097f519424a43192fe06ec1d1394386c
不枝雀 <yunxu1019@qq.com>
2024-03-18 06:51:47 +0800

    a.b?.()支持在函数中访问this

--- coms/basic/renderExpress.js
+++ coms/basic/renderExpress.js
@@ -1,13 +1,18 @@
 function splitSeek(express) {
     var dist = [];
     var x = 0;
+    var called = false;
     express.forEach((e, i) => {
+        if (isArray(e)) called = true;
         if (i > x && /^\?/.test(e)) {
             var exp = express.slice(x, x = i);
+            exp.called = called;
             dist.push(exp);
+            called = false;
         }
     });
     if (x < express.length) dist.push(express.slice(x, express.length));
+    dist[dist.length - 1].called = called;
     dist.forEach(d => d[0] = d[0].replace(/^\?/, ''))
     return dist;
 }
@@ -24,31 +29,114 @@ function getTmpvar(explist) {
     return tmpvar;
 }
 var autoDefine = false;
-function createSeek(express, split = true) {
-    var tmpvar = 'a', undef = '_';
+function joinsharp(sharped, ctx) {
+    var willcall = [], call = false;
+    for (var cx = 1, dx = sharped.length; cx < dx; cx++) {
+        if (/^\(/.test(sharped[cx])) {
+            var p = sharped[cx - 1];
+            if (/\)$/.test(p)) continue;
+            if (cx > 1 || /\./.test(p)) willcall[cx] = true;
+        }
+    }
+    var result = [];
+    var will = false, call = false;
+    if (sharped[sharped.length - 1] === '.') sharped[sharped.length - 1] = "";
+    for (var cx = 0, dx = sharped.length; cx < dx; cx++) {
+        var s = sharped[cx];
+        var plus = /^\//.test(s);
+        if (plus) s = s.slice(1);
+        call = will;
+        will = willcall[cx + 1];
+        if (will) {
+            if (/\]/.test(s)) {
+                s = [s];
+                s.unshift('');
+            }
+            else {
+                s = s.split('.');
+                if (s.length > 1) {
+                    s.unshift(s.splice(0, s.length - 1).join('.'));
+                    s[1] = '.' + s[1];
+                }
+                else {
+                    s.unshift("");
+                }
+            }
+            if (plus) {
+                s[0] = result[result.length - 1] + s[0];
+                result[result.length - 1] = s;
+            }
+            else result.push(s);
+        }
+        else if (call) {
+            var params = s.replace(/^\(\s*([\s\S]*)\s*\)$/, '$1');
+            result.push(`["call"](${ctx}${params ? ',' + params : ''})`);
+        }
+        else {
+            if (plus) {
+                result[result.length - 1] += s;
+            }
+            else result.push(s);
+        }
+    }
+    return result;
+}
+var wrapquote = false;
+function createSeek(sharped, splited = true) {
+    if (splited) wrapquote = false;
+    if (sharped.length <= 1) return sharped[0];
+    var tmpvar = 'a', undef = '_', tmpctx = 'c';
+    var express = splited ? joinsharp(sharped, tmpctx) : sharped;
     if (express.length <= 1) return express[0];
-    var notmp = !/[\.\[\(\{]/.test(express[0]);
+    if (splited) wrapquote = true;
+    var [exp0, exp1] = express;
+    var notmp = typeof exp0 === 'string' && !/[\.\[\(\{]/.test(exp0);
     if (express.length === 2) {
-        if (split && /^\?/.test(express[1])) {
-            if (notmp) return `typeof ${tmpvar}!=='undefined'&&${tmpvar}!==null?${tmpvar}:${express[1].slice(1)}`;
-            return `function(${tmpvar},${undef}){return ${tmpvar}==${undef}?${express[1].slice(1)}:${tmpvar}}(${express[0]})`;
+        if (splited && /^\?/.test(exp1)) {
+            if (notmp) return `typeof ${exp0}!=='undefined'&&${exp0}!==null?${exp0}:${exp1.slice(1)}`;
+            return `function(${tmpvar},${undef}){return ${tmpvar}==${undef}?${exp1.slice(1)}:${tmpvar}}(${exp0})`;
         }
         if (notmp) {
-            tmpvar = express[0];
             if (autoDefine) {
-                return `typeof ${tmpvar}==='undefined'||${tmpvar}===null?void 0:${tmpvar}${express[1]}`;
+                return `typeof ${exp0}==='undefined'||${exp0}===null?void 0:${exp0}${exp1}`;
             }
-            return `${tmpvar}==null?void 0:${tmpvar}${express[1]}`;
+            return `${exp0}==null?void 0:${exp0}${exp1}`;
         };
-        return `function(${tmpvar},${undef}){return ${tmpvar}==${undef}?${undef}:${tmpvar}${express[1]}}(${express[0]})`;
+        if (isArray(exp0)) {
+            return `function(${tmpctx},${tmpvar},${undef}){${tmpvar}=${tmpctx}${exp0[exp0.length - 1]};return ${tmpvar}==${undef}?${undef}:${tmpvar}${exp1}}(${exp0.slice(0, exp0.length - 1)})`;
+        }
+        return `function(${tmpvar},${undef}){return ${tmpvar}==${undef}?${undef}:${tmpvar}${exp1}}(${exp0})`;
     }
-    if (!split) {
-        var dist = express.slice(1).map((search, i, a) => {
-            return `if(${tmpvar}==${undef})return;${tmpvar}=${tmpvar}${search};`;
+    if (!splited) {
+        var called = express.called;
+        var prop = isArray(exp0) ? exp0[exp0.length - 1] : '';
+        var prop0 = prop;
+        var dist = express.slice(1).map(search => {
+            var res = prop ? `${tmpvar}=${tmpctx}${prop}` : '';
+            res += `if(${tmpvar}==${undef})return;`;
+            if (isArray(search)) {
+                prop = search[search.length - 1];
+                res += `${tmpctx}=${tmpvar}${search.slice(0, search.length - 1)},${tmpvar}=${tmpctx}${prop};`;
+                prop = '';
+            }
+            else {
+                res += `${tmpvar}=${tmpvar}${search};`;
+            }
+            return res;
         }).join('');
-        dist = `function(${tmpvar},${undef}){${dist}return ${tmpvar}}(${express[0]})`;
+        if (called) {
+            if (prop0) {
+                dist = `function(${tmpctx},${tmpvar},${undef}){${dist}return ${tmpvar}}(${exp0.slice(0, exp0.length - 1)})`;
+            }
+            else {
+                dist = `function(${tmpvar},${tmpctx},${undef}){${dist}return ${tmpvar}}(${exp0})`;
+            }
+        }
+        else {
+            dist = `function(${tmpvar},${undef}){${dist}return ${tmpvar}}(${exp0})`;
+        }
         if (notmp && autoDefine) {
-            dist = `typeof ${express[0]}==='undefined'&&${express[0]}===null?void 0:${dist}`;
+            dist = `typeof ${exp0}==='undefined'||${exp0}===null?void 0:${dist}`;
         }
         return dist;
     }
@@ -65,8 +153,8 @@ function createSeek(express, split = true) {
 }
 function main(express, autodef = true) {
     autoDefine = autodef;
-    if (!/\?\s*\.(?=[^\d])|\?\s*[\?\]\}\)\:\,=|%&;\>\<]|\?\s*$/.test(express)) return express;
-    var reg = /\\[\s\S]|\?\s*([\.](?!\d)|\?|$|(?=[\?\]\}\)\:\,=|%&;\>\<\*\/]))|[\:\,\+\=\-\!%\^\|\/\&\*\!\;\?\>\<~\{\}\[\]\(\)'"`\s]/g;
+    if (!/\?\s*\.(?=[^\d])|\?\s*[\?\]\}\)\:\,=|%&;\>\<]|\?\.?\s*$/.test(express)) return express;
+    var reg = /\\[\s\S]|\?\s*([\.](?!\d)|\?|$|(?=[\?\]\}\)\:\,\=\|%&;\>\<\*\/]))|[\:\,\+\=\-\!%\^\|\/\&\*\!\;\?\>\<\~\{\}\[\]\(\)'"`\s]/g;
     var cache = [], queue = [];
     var exp = [];
     var instr = false;
@@ -80,8 +168,11 @@ function main(express, autodef = true) {
                 exp.splice(0, exp.length, '');
                 e = '';
             }
-            if (/\.$/.test(e) && /^[\[\("'`]/.test(s)) {
-                e = e.replace(/\.$/, '');
+            if (/\.$/.test(e)) {
+                if (/^[\[\("'`]/.test(s)) e = e.replace(/\.$/, '');
+            }
+            else if (!cache.length) {
+                return exp.push('/' + s);
             }
             e += s;
             exp[exp.length - 1] = e;
@@ -103,7 +194,7 @@ function main(express, autodef = true) {
     var add_punc = function (m) {
         if (exp.length) {
             var temp = createSeek(exp);
-            queue.push(exp.length > 1 ? `(${temp})` : temp, m);
+            queue.push(wrapquote ? `(${temp})` : temp, m);
             exp = [];
         } else if (queue.length) {
             queue[queue.length - 1] += m;
@@ -129,7 +220,6 @@ function main(express, autodef = true) {
             isstr = true;
             index++;
         }
-
         var str = express.slice(lastIndex, index);
         if (str) {
             add_exp(str);
--- coms/basic/renderExpress_test.js
+++ coms/basic/renderExpress_test.js
@@ -1,8 +1,14 @@
 assert(renderExpress("[].a"), "[].a")
 assert(renderExpress("a?b:c"), "a?b:c")
 assert(renderExpress("a?"), "(typeof a==='undefined'||a===null?void 0:a)")
-assert(renderExpress("[].a?.()"), "(function(a,_){return a==_?_:a()}([].a))")// 暂不支持在?.()的函数中访问this，此为临时方案
-assert(renderExpress("[].a?.b?.c?.()"), "(function(a,_){if(a==_)return;a=a.b;if(a==_)return;a=a.c;if(a==_)return;a=a();return a}([].a))")
+assert(renderExpress("b+a?."), "b+(typeof a==='undefined'||a===null?void 0:a)")
+assert(renderExpress("a?.+b"), "(typeof a==='undefined'||a===null?void 0:a)+b")
+assert(renderExpress("a?.()"), "(typeof a==='undefined'||a===null?void 0:a())")
+assert(renderExpress("a.b?.()"), `(function(c,a,_){a=c.b;return a==_?_:a["call"](c)}(a))`)
+assert(renderExpress("a[['a[']][]?.()"), `(function(c,a,_){a=c[];return a==_?_:a["call"](c)}(a[['a[']]))`)
+assert(renderExpress("a?.b?.()"), `(typeof a==='undefined'||a===null?void 0:function(a,c,_){if(a==_)return;c=a,a=c.b;if(a==_)return;a=a["call"](c);return a}(a))`)
+assert(renderExpress("[].a?.()"), `(function(c,a,_){a=c.a;return a==_?_:a["call"](c)}([]))`)// 已支持在?.()的函数中访问this，此为临时方案
+assert(renderExpress("[].a?.b?.c?.()"), `(function(a,c,_){if(a==_)return;a=a.b;if(a==_)return;c=a,a=c.c;if(a==_)return;a=a["call"](c);return a}([].a))`)
 assert(renderExpress("a??b"), "(typeof a!=='undefined'&&a!==null?a:b)")
 assert(renderExpress("a?.b??b?.c", false), "(function(b0){b0=a==null?void 0:a.b;if(b0!=null)return b0;return b==null?void 0:b.c}())")
 
@@ -10,4 +16,4 @@ assert(renderExpress("[]?.a"), "(function(a,_){return a==_?_:a.a}([]))")
 assert(renderExpress("[]?.()"), "(function(a,_){return a==_?_:a()}([]))")
 assert(renderExpress("[]?.[]"), "(function(a,_){return a==_?_:a[]}([]))")
 assert(renderExpress("a['a?.a']"), "a['a?.a']")
-assert(renderExpress("a?.['a?.a']?.b"), "(typeof a==='undefined'&&a===null?void 0:function(a,_){if(a==_)return;a=a['a?.a'];if(a==_)return;a=a.b;return a}(a))")
+assert(renderExpress("a?.['a?.a']?.b"), "(typeof a==='undefined'||a===null?void 0:function(a,_){if(a==_)return;a=a['a?.a'];if(a==_)return;a=a.b;return a}(a))")
