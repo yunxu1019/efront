05a19e7aca60170d0399bc4d1fe24bc8d5b5121d
不枝雀 <yunxu1019@qq.com>
2024-03-22 04:43:29 +0800

    调整linux下源码运行的脚本

--- tools/run-efront.bat
+++ tools/run-efront.bat
@@ -1,7 +1,11 @@
 :<<a
 goto :win
 a
+if test -L $0; then
 dp=`cd $(dirname $0);cd $(dirname $(readlink $0)); pwd -P`
+else
+dp=`cd $(dirname $0); pwd -P`
+fi
 export libs_path=$dp/../coms
 #export coms_path=./coms
 export libs=typescript,esprima,escodegen,esmangle,pngjs,less-node
