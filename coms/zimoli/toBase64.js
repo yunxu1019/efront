50c90f406dc0b4ac41af4b5da27ab92f69e4a726
不枝雀 <yunxu1019@qq.com>
2024-03-28 12:17:01 +0800

    支持编码为base64url格式

--- coms/zimoli/toBase64.js
+++ coms/zimoli/toBase64.js
@@ -1,14 +1,24 @@
 var encoding = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split('');
-function toBase64(input) {
+function toBase64(input, url) {
+    var encoding1 = encoding;
+    if (url) {
+        encoding1[62] = "-";
+        encoding1[63] = "_";
+    }
     for (var cx = 0, block, output = [], dx = input.length; cx < dx;) {
         block = input[cx++] << 16 | input[cx++] << 8 | input[cx++];
         output.push(
-            encoding[block >> 18],
-            encoding[block >> 12 & 0b111111],
-            encoding[block >> 6 & 0b111111],
-            encoding[block & 0b111111]
+            encoding1[block >> 18],
+            encoding1[block >> 12 & 0b111111],
+            encoding1[block >> 6 & 0b111111],
+            encoding1[block & 0b111111]
         );
     }
-    for (cx = output.length + dx - cx, dx = output.length; cx < dx; cx++) output[cx] = '=';
+    if (url) {
+        encoding1[62] = "+";
+        encoding1[63] = "/";
+        while (cx-- > dx) output.pop();
+    }
+    else for (cx = output.length + dx - cx, dx = output.length; cx < dx; cx++) output[cx] = '=';
     return output.join("");
 }
