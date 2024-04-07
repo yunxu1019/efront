c0ebcc8fb2b3a919b7d9e7cc410ce4af5278023d
不枝雀 <yunxu1019@qq.com>
2024-04-07 11:42:10 +0800

    追加actived事件

--- coms/zimoli/active.js
+++ coms/zimoli/active.js
@@ -1,12 +1,21 @@
 // 激活 自定义的 active 事件
 function active(target, value, item = value, srcElem) {
-    var activeEvent = createEvent("active");
-    activeEvent.item = item;
-    activeEvent.value = value;
-    if (srcElem) {
-        if (Object.defineProperty) Object.defineProperty(activeEvent, 'active', { value: srcElem });
-        else activeEvent.active = srcElem;
+    var dispatchEvent = function (eventname) {
+        var activeEvent = createEvent(eventname);
+        activeEvent.item = item;
+        activeEvent.value = value;
+        if (srcElem) {
+            if (Object.defineProperty) Object.defineProperty(activeEvent, 'active', { value: srcElem });
+            else activeEvent.active = srcElem;
+        }
+        activeEvent = dispatch(target, activeEvent);
+    };
+    var activeEvent = dispatchEvent("active");
+    var actived = activeEvent && !activeEvent.defaultPrevented;
+    if (actived !== false) {
+        Promise.resolve().then(function () {
+            dispatchEvent("actived");
+        });
     }
-    activeEvent = dispatch(target, activeEvent);
-    return activeEvent && !activeEvent.defaultPrevented;
+    return actived;
 }
