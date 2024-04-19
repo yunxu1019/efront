4f502af636f0d4c72fd4f6fe962fcdb145ba9924
不枝雀 <yunxu1019@qq.com>
2024-04-20 02:02:49 +0800

    编译完成后控制台不再显示子进程编号

--- coms/message/log.js
+++ coms/message/log.js
@@ -1,6 +1,6 @@
 "use strict";
 var colors = require("../reptile/colors");
 module.exports = function ({ log, args }) {
-    args.unshift(`${colors.FgPurple}<${colors.FgRed2}< ${colors.FgYellow}${this.threadId}${colors.FgGreen2} >${colors.FgCyan}>${colors.Reset}`);
+    if (args.length) args.unshift(`${colors.FgPurple}<${colors.FgRed2}< ${colors.FgYellow}${this.threadId}${colors.FgGreen2} >${colors.FgCyan}>${colors.Reset}`);
     console[log].apply(console, args);
 }
