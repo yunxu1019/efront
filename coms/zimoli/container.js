21d1905dae4a28803f0c3395d9031ed5c3a510ce
不枝雀 <yunxu1019@qq.com>
2024-04-22 09:48:44 +0800

    解决container在列表中使用时只渲染最后一个元素的问题

--- coms/zimoli/container.js
+++ coms/zimoli/container.js
@@ -1,36 +1,36 @@
-var change = lazy(async function (src) {
-    if (src !== this.$src) return;
-    if (!src) remove(this.with);
-    else if (isNode(src)) {
-        if (isElement(src) && this.$struct.copys) {
-            for (var c of this.$struct.copys) {
-                if (c.name === 'class') {
-                    addClass(src, c.value);
-                }
-                else if (c.name === 'style') {
-                    css(src, c.value);
-                }
-                else src.setAttribute(c.name, c.value);
-            }
-        }
-        zimoli.global(src, this);
-    }
-    else if (isString(src) || +src < 0) {
-        watch(this, 'params', onparams);
-        onparams.call(this, this.params);
-    }
-});
-var gosrc = function (src) {
-    if (!isHandled(this.$src) && !isHandled(src)) return;
-    if (src !== this.$src) {
-        this.$src = src;
-        change.call(this, src);
-    }
-};
-var onparams = lazy(function (params) {
-    zimoli.go(this.$src, params, this);
-});
 function container(element) {
+    var change = lazy(async function (src) {
+        if (src !== this.$src) return;
+        if (!src) remove(this.with);
+        else if (isNode(src)) {
+            if (isElement(src) && this.$struct.copys) {
+                for (var c of this.$struct.copys) {
+                    if (c.name === 'class') {
+                        addClass(src, c.value);
+                    }
+                    else if (c.name === 'style') {
+                        css(src, c.value);
+                    }
+                    else src.setAttribute(c.name, c.value);
+                }
+            }
+            zimoli.global(src, this);
+        }
+        else if (isString(src) || +src < 0) {
+            watch(this, 'params', onparams);
+            onparams.call(this, this.params);
+        }
+    });
+    var gosrc = function (src) {
+        if (!isHandled(this.$src) && !isHandled(src)) return;
+        if (src !== this.$src) {
+            this.$src = src;
+            change.call(this, src);
+        }
+    };
+    var onparams = lazy(function (params) {
+        zimoli.go(this.$src, params, this);
+    });
     var comment = document.createComment('container');
     comment.$struct = element.$struct;
     care(comment, gosrc);
