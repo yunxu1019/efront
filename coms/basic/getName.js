7fcb303f51abb6dcac3010452532ba596e1994ef
不枝雀 <yunxu1019@qq.com>
2024-05-17 11:07:23 +0800

    解决菜单项名称显示错误的问题

--- coms/basic/getName.js
+++ coms/basic/getName.js
@@ -9,11 +9,11 @@ function getName(o) {
         if (!isEmpty(name)) return String(name);
     }
     for (var k of keys) {
-        if (!hasOwnProperty.call(o, k)) continue;
-        if (!isEmpty(o[k])) return String(o[k]);
-        if (isString(o[k])) name = o[k];
+        var v = o[k];
+        if (!isEmpty(v) && hasOwnProperty.call(o, k)) return String(v);
+        if (isString(v)) name = v;
     }
-    if (hasOwnProperty.call(o, 'valueOf')) name = o.valueOf();
+    if (!name && hasOwnProperty.call(o, 'valueOf')) name = o.valueOf();
     if (isString(name)) return name;
     return String(o);
 }
