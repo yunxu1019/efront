f6e24f2e5a46d41d77c328a3ce91276fb9905f02
不枝雀 <yunxu1019@qq.com>
2024-06-04 09:46:50 +0800

    支持手动更改证书

--- apps/pivot/cert/list.js
+++ apps/pivot/cert/list.js
@@ -1,9 +1,5 @@
 plist.bind(null, '接口管理', "cert", refilm`
 *域名/hostname input/253
-公钥/private gen/600 ${async function (data) {
-        var [private_key, public_key] = await acme2.createKeyPair();
-        data.private = private_key;
-        data.public = public_key;
-    }}
--私钥/public text
-$证书/cert text`, '/cert/edit');
+公钥/private text
+私钥/public text
+证书/cert text`, '/cert/edit');
