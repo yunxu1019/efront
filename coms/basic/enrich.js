1a065d8b92ff7990c84c0cf93e6f32236d55a1c9
不枝雀 <yunxu1019@qq.com>
2024-08-07 06:07:53 +0800

    重写两个小工具enrich,zimoli/user

--- coms/basic/enrich.js
+++ coms/basic/enrich.js
@@ -1,47 +1,36 @@
 "use strict";
+var bindObjK = function (obj, k, resolve, _object) {
+    Object.defineProperty(_object, k, {
+        get() {
+            var method = obj[k];
+            if (typeof method === 'function') {
+                return function () {
+                    var r = method.apply(obj, arguments);
+                    return resolve(r);
+                };
+            }
+            return method;
+        },
+        set(v) {
+            obj[k] = v;
+        },
+    });
+};
 var enrich = function enrich(obj) {
     var _promise = Promise.resolve();
-    var Base = function () {
-    };
-    Base.prototype = obj;
-    var _object = new Base;
-    for (var k in obj) {
-        if (obj[k] instanceof Function) {
-            _promise[k] = function () {
-                var method = obj[k];
-                var caller = function () {
-                    "use strict";
-                    var args = arguments;
-                    return this.then(function () {
-                        return method.apply(_object, args);
-                    });
-                };
-                _object[k] = function () {
-                    return caller.apply(_promise, arguments);
-                };
-                return caller;
-            }();
-        }
-    }
-    _promise._enrich = function (promise) {
-        if (!promise._enrich) {
-            for (var k in _promise) promise[k] = _promise[k];
-        }
-        return promise;
-    };
+    var _object = Object.create(null);
     var _then = _promise.then;
-    var _catch = _promise.catch;
-    _promise.then = function () {
+    var pthen = function () {
         var promise = _then.apply(this, arguments);
-        return this._enrich(promise);
+        return resolve(promise);
     };
-    _promise.catch = function () {
-        var promise = _catch.apply(this, arguments);
-        return this._enrich(promise);
-    }
+    var resolve = function (result) {
+        var _promise = Promise.resolve(result);
+        for (var k in obj) if (k !== 'then') bindObjK(obj, k, resolve, _promise);
+        _promise.then = pthen;
+        return _promise;
+    };
+    for (var k in obj) bindObjK(obj, k, resolve, _object);
     return _object;
 };
 module.exports = enrich;
-
-
-// var Promise = new Function(require("fs").readFileSync("coms/zimoli/promise.js").toString()+"\r\nreturn Promise;").apply(global);
--- coms/zimoli/user.js
+++ coms/zimoli/user.js
@@ -15,11 +15,12 @@ var proto = {
     loginPath: null,
     session_time: 300000,
     session: "",
+    clean: null,
     loadSession: function () {
         var object = userInstance;
         if (object.session && !(new Date() > object.session_time)) {
-            proto.session_time = object.session_time;
-            proto.session = object.session;
+            this.session_time = object.session_time;
+            this.session = object.session;
             this.Login(object);
         }
         return object.session || "";
@@ -27,58 +28,57 @@ var proto = {
     saveSession: function (session = this.session) {
         this.session = session;
         data.setInstance(USERINFO, {
-            roles: proto.roles,
-            name: proto.name,
-            _id: proto._id,
+            roles: this.roles,
+            name: this.name,
+            _id: this._id,
             session: session,
             _passport: this._passport,
             isLogin: !!session,
-            session_time: +new Date() + proto.session_time
+            session_time: +new Date() + this.session_time
         });
+    },
+    setLoginPath(pathname) {
+        this.loginPath = pathname;
+    },
+    setUserDataLoader(method) {
+        loadUserData = method;
+    },
+    setSessionTime(session_time) {
+        this.session_time = session_time;
+    },
+    refresh() {
+        loadUserData instanceof Function && loadUserData();
+    },
+    getPassport() {
+        if (!passport) {
+            passport = encode62.timeencode(encode62.decode62(user._passport, user.session));
+        }
+        return passport = encode62.timeupdate(passport);
     },
     Login(userinfo) {
         this.login(userinfo);
     },
+    login(userinfo) {
+        this.roles = userinfo.roles || [];
+        this.isLogin = true;
+        this.name = userinfo.name;
+        this._id = "org.couchdb.user:" + userinfo.name;
+        this.clean?.(this.loginPath);
+        this.refresh();
+    },
     Logout() {
         this.logout();
-    }
+    },
+    logout() {
+        delete user.isLogin;
+        delete user.roles;
+        delete user._passport;
+        this._passport = "";
+        this.roles = null;
+        this.isLogin = false;
+        passport = '';
+        this.saveSession("");
+    },
 };
 extend(proto, emptyProto, userInstance);
 var user = enrich(proto);
-user.setLoginPath = function (pathname) {
-    proto.loginPath = pathname;
-};
-user.setUserDataLoader = function (method) {
-    loadUserData = method;
-};
-user.setSessionTime = function (session_time) {
-    proto.session_time = session_time;
-};
-user.refresh = function () {
-    loadUserData instanceof Function && loadUserData();
-};
-
-user.getPassport = function () {
-    if (!passport) {
-        passport = encode62.timeencode(encode62.decode62(user._passport, user.session));
-    }
-    return passport = encode62.timeupdate(passport);
-};
-user.login = function (userinfo) {
-    proto.roles = userinfo.roles || [];
-    proto.isLogin = true;
-    proto.name = userinfo.name;
-    proto._id = "org.couchdb.user:" + userinfo.name;
-    user.clean && user.clean(proto.loginPath);
-    this.refresh();
-};
-user.logout = function () {
-    delete user.isLogin;
-    delete user.roles;
-    delete user._passport;
-    proto._passport = "";
-    proto.roles = null;
-    proto.isLogin = false;
-    passport = '';
-    this.saveSession("");
-};
