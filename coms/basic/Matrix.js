7ab9b01b1c2c8f28d1a2b13ebbd0ccf7eebb2e93
不枝雀 <yunxu1019@qq.com>
2024-08-21 18:13:47 +0800

    删除矩阵的转置实现

--- coms/basic/Matrix.js
+++ coms/basic/Matrix.js
@@ -1,10 +1,175 @@
 var notMatchLength = new Error("矩阵长度不一致");
+
+function matrix2d(theta) {
+    var cosa = Math.cos(theta);
+    var sina = Math.sin(theta);
+    return new Matrix(
+        cosa, sina, 0,
+        -sina, cosa, 0,
+        0, 0, 1
+    );
+}
+
+function matrix3d(factor) {
+    var n = norm(factor.slice(0, 3));
+    var theta = factor.length === 4 ? factor[3] : n;
+    var vec = times(factor, 1 / n);
+    var cosa = Math.cos(theta);
+    var sina = Math.sin(theta);
+    var vera = 1 - cosa;
+    var [x_u, y_u, z_u] = !n ? [0, 0, 1] : vec;
+    return new Matrix(
+        cosa + x_u * x_u * vera, x_u * y_u * vera - z_u * sina, x_u * z_u * vera + y_u * sina, 0,
+        x_u * y_u * vera + z_u * sina, cosa + y_u * y_u * vera, y_u * z_u * vera - x_u * sina, 0,
+        x_u * z_u * vera - y_u * sina, y_u * z_u * vera + x_u * sina, cosa + z_u * z_u * vera, 0,
+        0, 0, 0, 1
+    );
+}
+
+function transform(B, dots) {
+    var dimention = Math.sqrt(B.length - 1) | 0;
+    if (dots.length % dimention !== 0) throw notMatchLength;
+    if (dots === B) B = B.slice(0);
+    var ds = dots.slice(0);
+    var dim2 = B.length % dimention !== 0 ? dimention + 1 : dimention;
+    for (var cx = 0, dx = dots.length; cx < dx; cx += dimention) {
+        for (var cy = 0, dy = dimention; cy < dy; cy++) {
+            var sum = 0;
+            for (var ct = 0, dt = dimention; ct < dt; ct++) {
+                sum += ds[cx + ct] * B[ct * dim2 + cy];
+            }
+            sum += B[dimention * dim2 + cy];
+            dots[cx + cy] = sum;
+        }
+    }
+    return dots;
+}
+
+function translate(A, delta) {
+    var [dim2, dim] = size(A);
+    var inc = 0;
+    for (var cx = dim * dim2, dx = cx + dim; cx < dx; cx++) {
+        A[cx] = (A[cx] || 0) + delta[inc++];
+    }
+    return A;
+}
+
+function 逆(A) {
+    var dim = Math.sqrt(A.length) | 0;
+    if (dim * dim !== A.length) throw notMatchLength;
+    var E = new Array(A.length).fill(0);
+    for (var cx = 0, dx = dim; cx < dx; cx++)E[cx * dim + cx] = 1;
+    var X = A.splice(0, A.length);
+    A.push.apply(A, E);
+    for (var cx = 0, dx = dim; cx < dx; cx++) {
+        var start = cx * dim + cx;
+        var max_ct, v = 0;
+        for (var ct = start, dt = X.length; ct < dt; ct += dim) {
+            var v0 = Math.abs(X[ct]);
+            if (v0 > v) {
+                max_ct = ct;
+                v = v0;
+            }
+        }
+        ct = max_ct;
+        if (ct !== start) {
+            var delta = ct - start;
+            var ratio = (1 - X[start]) / X[ct];
+            for (var cy = cx * dim, dy = cy + dim; cy < dy; cy++) {
+                X[cy] += X[cy + delta] * ratio;
+                A[cy] += A[cy + delta] * ratio;
+            }
+        }
+        if (X[start] !== 1) {
+            var ratio = 1 / X[start];
+            for (var cy = cx * dim, dy = cy + dim; cy < dy; cy++) {
+                X[cy] *= ratio;
+                A[cy] *= ratio;
+            }
+        }
+        for (var ct = start + dim, dt = X.length; ct < dt; ct += dim) {
+            if (X[ct] === 0) continue;
+            var ratio = -X[ct];
+            var delta = start - ct;
+            for (var cy = ct - cx, dy = cy + dim; cy < dy; cy++) {
+                X[cy] += X[cy + delta] * ratio;
+                A[cy] += A[cy + delta] * ratio;
+            }
+        }
+    }
+    for (var cx = dim - 1; cx > 0; cx--) {
+        var start = cx * dim + cx;
+        for (var ct = start - dim; ct >= 0; ct -= dim) {
+            if (X[ct] === 0) continue;
+            var ratio = -X[ct];
+            var delta = start - ct;
+            for (var cy = ct - cx, dy = cy + dim; cy < dy; cy++) {
+                X[cy] += X[cy + delta] * ratio;
+                A[cy] += A[cy + delta] * ratio;
+            }
+        }
+    }
+    return A;
+};
+
+function multiply(A, B) {
+    if (A.length !== B.length) throw notMatchLength;
+    var dim = Math.sqrt(A.length) | 0;
+    if (dim * dim !== A.length) throw notMatchLength;
+    var X = A.slice(0);
+    for (var cx = 0, dx = A.length; cx < dx; cx += dim) {
+        for (var cy = 0, dy = dim; cy < dy; cy++) {
+            var sum = 0;
+            for (var ct = 0, dt = dim; ct < dt; ct++) {
+                sum += X[cx + ct] * B[ct * dim + cy];
+            }
+            A[cx + cy] = sum;
+        }
+    }
+    return A;
+}
+
+function resolve(A, s) {
+    var dim = s.length;
+    if (dim * dim !== A.length) throw notMatchLength;
+    var r = [];
+    for (var cx = 0, cy = 0, dx = dim; cx < dx; cx++) {
+        var sum = 0;
+        for (var dy = cy + dim; cy < dy; cy++) {
+            sum += A[dy] * s[cx];
+        }
+        r[cx] = sum;
+    }
+    return r;
+}
+
+function transpose(A) {
+    var [size] = size(A);
+    for (var cx = 0, dx = size; cx < dx; cx++) {
+        for (var cy = cx + 1, dy = dx; cy < dy; cy++) {
+            var i = cx * size + cy;
+            var j = cy * size + cx;
+            var tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+        }
+    }
+    return A;
+}
+
 class Matrix extends Array {
+    static multiply = multiply;
+    static transform = transform;
+    static translate = translate;
+    static matrix2d = matrix2d;
+    static matrix3d = matrix3d;
+    static resolve = resolve;
+    static 逆 = 逆;
     static create2d(theta = 0) {
-        return MathMatrix.matrix2d(theta);
+        return matrix2d(theta);
     }
     static create3d(factor = [0, 0, 0]) {
-        return MathMatrix.matrix3d(factor);
+        return matrix3d(factor);
     }
     size() {
         return size(this);
@@ -18,41 +183,21 @@ class Matrix extends Array {
         return this._invert = this.slice().inverse();
     }
     transpose() {
-        if (this.constructor === MathMatrix) {
-            var m = new MatrixTransposed;
-        }
-        else {
-            var m = new MathMatrix;
-        }
-        m.push.apply(m, this);
-        m.transpose_();
-        return m;
-    }
-    transpose_() {
-        var [size] = this.size();
-        for (var cx = 0, dx = size; cx < dx; cx++) {
-            for (var cy = cx + 1, dy = dx; cy < dy; cy++) {
-                var i = cx * size + cy;
-                var j = cy * size + cx;
-                var tmp = this[i];
-                this[i] = this[j];
-                this[j] = tmp;
-            }
-        }
-        return this;
+        this.dirty();
+        return transpose(this);
     }
     inverse() {
         this.dirty();
-        return this.constructor.逆(this);
+        return 逆(this);
     }
     rotate(factor, center) {
         this.dirty();
         if (center) this.translate(center.map(负));
         if (factor.length) {
-            this.multiply(this.constructor.matrix3d(factor));
+            this.multiply(matrix3d(factor));
         }
         else {
-            this.multiply(this.constructor.matrix2d(factor));
+            this.multiply(matrix2d(factor));
         }
         if (center) this.translate(center);
         return this;
@@ -61,7 +206,7 @@ class Matrix extends Array {
         var v = vector;
         if (!isFinite(v.length)) v = arguments;
         this.dirty();
-        return this.constructor.translate(this, v);
+        return translate(this, v);
     }
     scale(ratio, center) {
         this.dirty();
@@ -73,298 +218,27 @@ class Matrix extends Array {
     }
     multiply(a) {
         this.dirty();
-        return this.constructor.multiply(this, a);
+        return multiply(this, a);
     }
     transform(dots) {
-        if (dots instanceof Array) return this.constructor.transform(this, dots);
+        if (dots instanceof Array) return transform(this, dots);
         if (arguments.length > 1) {
             var a = Array.prototype.slice.apply(arguments, 0);
-            return this.constructor.transform(this, a);
+            return transform(this, a);
         }
         return dots;
     }
-}
-class MathMatrix extends Matrix {
-    getTransform() {
-        return [this[0], this[1], this[3], this[4], this[6], this[7]];
+    resolve(s) {
+        var A = this.slice().inverse();
+        return resolve(A, s);
     }
     toDOMString() {
-        if (this.size()[1] === 2) return `matrix(${this.getTransform()})`;
+        if (this.size()[1] === 2) return `matrix(${[this[0], this[1], this[3], this[4], this[6], this[7]]})`;
         return `matrix(${this})`;
     }
-
-    static transform(B, dots) {
-        var dimention = Math.sqrt(B.length - 1) | 0;
-        if (dots.length % dimention !== 0) throw notMatchLength;
-        if (dots === B) B = B.slice(0);
-        var ds = dots.slice(0);
-        var dim2 = B.length % dimention !== 0 ? dimention + 1 : dimention;
-        for (var cx = 0, dx = dots.length; cx < dx; cx += dimention) {
-            for (var cy = 0, dy = dimention; cy < dy; cy++) {
-                var sum = 0;
-                for (var ct = 0, dt = dimention; ct < dt; ct++) {
-                    sum += ds[cx + ct] * B[ct * dim2 + cy];
-                }
-                sum += B[dimention * dim2 + cy];
-                dots[cx + cy] = sum;
-            }
-        }
-        return dots;
-    };
-
-
-    static translate(A, delta) {
-        var [dim2, dim] = size(A);
-        var inc = 0;
-        for (var cx = dim * dim2, dx = cx + dim; cx < dx; cx++) {
-            A[cx] = (A[cx] || 0) + delta[inc++];
-        }
-        return A;
-    }
-
-    static multiply(A, B) {
-        if (A.length !== B.length) throw notMatchLength;
-        var dim = Math.sqrt(A.length) | 0;
-        if (dim * dim !== A.length) throw notMatchLength;
-        var X = A.slice(0);
-        for (var cx = 0, dx = A.length; cx < dx; cx += dim) {
-            for (var cy = 0, dy = dim; cy < dy; cy++) {
-                var sum = 0;
-                for (var ct = 0, dt = dim; ct < dt; ct++) {
-                    sum += X[cx + ct] * B[ct * dim + cy];
-                }
-                A[cx + cy] = sum;
-            }
-        }
-        return A;
-    }
-
-
-    static matrix2d(theta) {
-        var cosa = Math.cos(theta);
-        var sina = Math.sin(theta);
-        return new MathMatrix(
-            cosa, sina, 0,
-            -sina, cosa, 0,
-            0, 0, 1
-        );
-    };
-
-    static matrix3d(factor) {
-        var n = norm(factor.slice(0, 3));
-        var theta = factor.length === 4 ? factor[3] : n;
-        var vec = times(factor, 1 / n);
-        var cosa = Math.cos(theta);
-        var sina = Math.sin(theta);
-        var vera = 1 - cosa;
-        var [x_u, y_u, z_u] = !n ? [0, 0, 1] : vec;
-        return new MathMatrix(
-            cosa + x_u * x_u * vera, x_u * y_u * vera - z_u * sina, x_u * z_u * vera + y_u * sina, 0,
-            x_u * y_u * vera + z_u * sina, cosa + y_u * y_u * vera, y_u * z_u * vera - x_u * sina, 0,
-            x_u * z_u * vera - y_u * sina, y_u * z_u * vera + x_u * sina, cosa + z_u * z_u * vera, 0,
-            0, 0, 0, 1
-        );
-    }
-
-    static 逆 = function (A) {
-        var dim = Math.sqrt(A.length) | 0;
-        if (dim * dim !== A.length) throw notMatchLength;
-        var E = new Array(A.length).fill(0);
-        for (var cx = 0, dx = dim; cx < dx; cx++)E[cx * dim + cx] = 1;
-        var X = A.splice(0, A.length);
-        A.push.apply(A, E);
-        for (var cx = 0, dx = dim; cx < dx; cx++) {
-            var start = cx * dim + cx;
-            var max_ct, v = 0;
-            for (var ct = start, dt = X.length; ct < dt; ct += dim) {
-                var v0 = Math.abs(X[ct]);
-                if (v0 > v) {
-                    max_ct = ct;
-                    v = v0;
-                }
-            }
-            ct = max_ct;
-            if (ct !== start) {
-                var delta = ct - start;
-                var ratio = (1 - X[start]) / X[ct];
-                for (var cy = cx * dim, dy = cy + dim; cy < dy; cy++) {
-                    X[cy] += X[cy + delta] * ratio;
-                    A[cy] += A[cy + delta] * ratio;
-                }
-            }
-            if (X[start] !== 1) {
-                var ratio = 1 / X[start];
-                for (var cy = cx * dim, dy = cy + dim; cy < dy; cy++) {
-                    X[cy] *= ratio;
-                    A[cy] *= ratio;
-                }
-            }
-            for (var ct = start + dim, dt = X.length; ct < dt; ct += dim) {
-                if (X[ct] === 0) continue;
-                var ratio = -X[ct];
-                var delta = start - ct;
-                for (var cy = ct - cx, dy = cy + dim; cy < dy; cy++) {
-                    X[cy] += X[cy + delta] * ratio;
-                    A[cy] += A[cy + delta] * ratio;
-                }
-            }
-        }
-        for (var cx = dim - 1; cx > 0; cx--) {
-            var start = cx * dim + cx;
-            for (var ct = start - dim; ct >= 0; ct -= dim) {
-                if (X[ct] === 0) continue;
-                var ratio = -X[ct];
-                var delta = start - ct;
-                for (var cy = ct - cx, dy = cy + dim; cy < dy; cy++) {
-                    X[cy] += X[cy + delta] * ratio;
-                    A[cy] += A[cy + delta] * ratio;
-                }
-            }
-        }
-        return A;
-    };
-
 }
-class MatrixTransposed extends Matrix {
-    getTransform() {
-        return [this[0], this[3], this[1], this[4], this[2], this[5]];
-    }
-    toDOMString() {
-        if (this.size()[1] === 2) return `matrix(${this.getTransform()})`;
-        return `matrix(${this.transpose()})`;
-    }
-    static transform(B, dots) {
-        var dimention = Math.sqrt(B.length - 1) | 0;
-        if (dots.length % dimention !== 0) throw notMatchLength;
-        if (dots === B) B = B.slice(0);
-        var ds = dots.slice(0);
-        var dim2 = B.length % dimention !== 0 ? dimention + 1 : dimention;
-        for (var cx = 0, dx = dots.length; cx < dx; cx += dimention) {
-            for (var cy = 0, dy = dimention; cy < dy; cy++) {
-                var sum = 0;
-                var start = dim2 * cy;
-                for (var ct = 0, dt = dimention; ct < dt; ct++) {
-                    sum += ds[cx + ct] * B[start + ct];
-                }
-                sum += B[start + dimention];
-                dots[cx + cy] = sum;
-            }
-        }
-        return dots;
-    };
 
-    static translate(A, delta) {
-        var [dim2, dim] = size(A);
-        var inc = 0;
-        for (var cx = dim, dx = dim * dim2; cx < dx; cx += dim2) {
-            A[cx] = (A[cx] || 0) + delta[inc++];
-        }
-        return A;
-    }
-
-    static multiply(A, B) {
-        if (A.length !== B.length) throw notMatchLength;
-        var dim = Math.sqrt(A.length) | 0;
-        if (dim * dim !== A.length) throw notMatchLength;
-        var X = A.slice(0);
-        for (var cx = 0, dx = A.length; cx < dx; cx += dim) {
-            for (var cy = 0, dy = dim; cy < dy; cy++) {
-                var sum = 0;
-                for (var ct = 0, dt = dim; ct < dt; ct++) {
-                    sum += B[cx + ct] * X[ct * dim + cy];
-                }
-                A[cx + cy] = sum;
-            }
-        }
-        return A;
-    }
-
-    static matrix2d(theta) {
-        var cosa = Math.cos(theta);
-        var sina = Math.sin(theta);
-        return new MatrixTransposed(
-            cosa, -sina, 0,
-            sina, cosa, 0,
-            0, 0, 1
-        );
-    }
-
-    static matrix3d(factor) {
-        var n = norm(factor.slice(0, 3));
-        var theta = factor.length === 4 ? factor[3] : n;
-        var vec = times(factor, 1 / n);
-        var cosa = Math.cos(theta);
-        var sina = Math.sin(theta);
-        var vera = 1 - cosa;
-        var [x_u, y_u, z_u] = !n ? [0, 0, 1] : vec;
-        return new MatrixTransposed(
-            cosa + x_u * x_u * vera, x_u * y_u * vera + z_u * sina, x_u * z_u * vera - y_u * sina, 0,
-            x_u * y_u * vera - z_u * sina, cosa + y_u * y_u * vera, y_u * z_u * vera + x_u * sina, 0,
-            x_u * z_u * vera + y_u * sina, y_u * z_u * vera - x_u * sina, cosa + z_u * z_u * vera, 0,
-            0, 0, 0, 1
-        );
-    }
-    static 逆 = function (A) {
-        var dim = Math.sqrt(A.length) | 0;
-        if (dim * dim !== A.length) throw notMatchLength;
-        var E = new Array(A.length).fill(0);
-        for (var cx = 0, dx = dim; cx < dx; cx++)E[cx * dim + cx] = 1;
-        var X = A.splice(0, A.length);
-        A.push.apply(A, E);
-        for (var cx = 0, dx = dim; cx < dx; cx++) {
-            var start = cx * dim + cx;
-            var max_ct, v = 0;
-            for (var ct = start, dt = cx * dim + dim; ct < dt; ct++) {
-                var v0 = Math.abs(X[ct]);
-                if (v0 > v) {
-                    max_ct = ct;
-                    v = v0;
-                }
-            }
-            ct = max_ct;
-            if (ct !== start) {
-                var delta = ct - start;
-                var ratio = (1 - X[start]) / X[ct];
-                for (var cy = cx, dy = X.length; cy < dy; cy += dim) {
-                    X[cy] += X[cy + delta] * ratio;
-                    A[cy] += A[cy + delta] * ratio;
-                }
-            }
-            if (X[start] !== 1) {
-                var ratio = 1 / X[start];
-                for (var cy = cx, dy = X.length; cy < dy; cy += dim) {
-                    X[cy] *= ratio;
-                    A[cy] *= ratio;
-                }
-            }
-            for (var ct = start + 1, dt = start + dim - cx; ct < dt; ct++) {
-                if (X[ct] === 0) continue;
-                var ratio = -X[ct];
-                var delta = start - ct;
-                for (var cy = cx + ct - start, dy = X.length; cy < dy; cy += dim) {
-                    X[cy] += X[cy + delta] * ratio;
-                    A[cy] += A[cy + delta] * ratio;
-                }
-            }
-        }
-        for (var cx = dim - 1; cx >= 0; cx--) {
-            var start = cx * dim + cx;
-            for (var ct = start - 1, dt = start - cx; ct >= dt; ct--) {
-                if (X[ct] === 0) continue;
-                var ratio = -X[ct];
-                var delta = start - ct;
-                for (var cy = cx + ct - start, dy = X.length; cy < dy; cy += dim) {
-                    X[cy] += X[cy + delta] * ratio;
-                    A[cy] += A[cy + delta] * ratio;
-                }
-            }
-        }
-        return A;
-    };
-
-}
-Matrix.MathMatrix = MathMatrix;
-Matrix.MatrixTransposed = MatrixTransposed;
+Matrix.MathMatrix = Matrix;
 
 
 function inner(vec1, vec2) {
