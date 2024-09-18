3917900600b6e461dbc954957de2a861e8ef6173
不枝雀 <yunxu1019@qq.com>
2024-09-18 14:59:35 +0800

    修复用色号定制按钮的功能

--- coms/zimoli/button.js
+++ coms/zimoli/button.js
@@ -96,9 +96,9 @@ function button(texter, type) {
             if (!type) {
                 texter = texter.replace(/#\w*/, function (w) {
                     if (/^#([a-f\d]{3,4}){1,2}$/.test(w)) {
-                        type = color.parse(w);
+                        type = w;
                     }
-                    type = w.slice(1);
+                    else type = w.slice(1);
                     return '';
                 }).trim();
             }
@@ -138,6 +138,16 @@ function button(texter, type) {
         if (parseFloat(type)) {
             var size = isFinite(type) ? type + "px" : type;
             css(button, `height:${size};`);
+        }
+        else if (color.isColor(type)) {
+            var c = color.format(type);
+            if (button.getAttribute('type') === 'anchor' || button.tagName === 'A') {
+                css(button, { color: c })
+            }
+            else {
+                var b = color.pair(c);
+                css(button, { background: c, color: b });
+            }
         }
         else if (isString(type)) button.setAttribute("type", type);
     }
