174de43b395218f30fcf5d510e37c89eb784edb6
不枝雀 <yunxu1019@qq.com>
2024-10-16 12:25:34 +0800

    onmounted的逻辑移动到on.js

--- coms/zimoli/on.js
+++ coms/zimoli/on.js
@@ -545,3 +545,18 @@ var invoke = function (event, type, pointerType) {
     }
 
 }());
+
+/**
+ * 在挂载后执行
+ * 如果是已挂载的元素，立即执行传入的函数
+ * 如果是未挂载的元素，等到挂载时执行
+ * @param {Element} target 
+ * @param {Function} handle 
+ */
+var onmounted = on('mounted');
+handlersMap["onmounted"] = function (target, handle) {
+    if (isMounted(target)) {
+        handle.call(target);
+    }
+    onmounted(target, handle);
+};
--- coms/zimoli/onmounted.js
+++ coms/zimoli/onmounted.js
@@ -1,13 +1,1 @@
-/**
- * 在挂载后执行
- * 如果是已挂载的元素，立即执行传入的函数
- * 如果是未挂载的元素，等到挂载时执行
- * @param {Element} target 
- * @param {Function} handle 
- */
-function onmounted(target, handle) {
-    if (isMounted(target)) {
-        handle.call(target);
-    }
-    return on("mounted")(target, handle);
-}
+return on("mounted");
