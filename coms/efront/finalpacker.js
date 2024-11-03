cc7f5fdde6c1f4249d09e229dbca8d851d6c17c6
不枝雀 <yunxu1019@qq.com>
2024-11-03 16:42:14 +0800

    对不存在入口文件的路径主动报错

--- coms/efront/finalpacker.js
+++ coms/efront/finalpacker.js
@@ -118,6 +118,7 @@ var responseFromCache = function (data) {
     if (data instanceof Promise) return data.then(responseFromCache, responseFromCache);
     if (data instanceof Buffer || data instanceof Function) return data;
     if (typeof data === "string") {
+        if (/\/$/.test(data)) return console.error(i18n`路径${data}未找到入口文件`);
         data = commbuilder(`require("${data}")`, 'index.js', 'index.js', []);
         return data;
     }
