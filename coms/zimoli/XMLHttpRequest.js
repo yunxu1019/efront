6d70ef664d4d5fbbc0b62b2d0c76ae06d2fcf61f
不枝雀 <yunxu1019@qq.com>
2024-11-08 07:24:15 +0800

    解决低版本浏览器无法调试的问题

--- coms/zimoli/XMLHttpRequest.js
+++ coms/zimoli/XMLHttpRequest.js
@@ -2,29 +2,33 @@ if (this.XMLHttpRequest) return this.XMLHttpRequest;
 if (this.ActiveXObject) return this.ActiveXObject.bind(null, 'Microsoft.XMLHTTP');
 if (!this.fetch) return;
 var window = this;
-return class XMLHttpRequest {
-    onload = null;
-    onerror = null;
-    onreadystatechange = null;
-    readyState = 0;
-    fetch = window.fetch;
-    open(method, url) {
-        this.readyState = 1;
-        this.method = method;
-        this.url = url;
-    }
-    send(data) {
-        this.fetch(this.url, { method: this.method, headers: { referer: document.location.href.replace(/^#[\s\S]*$/g, '') } }).then(async d => {
-            this.fetched = d;
-            this.readyState = 4;
-            this.status = d.status;
-            this.responseText = await d.text();
-            if (this.onreadystatechange) this.onreadystatechange({ target: this });
-            if (this.onload) this.onload({ target: this });
-        }, e => {
-            this.readyState = 4;
-            if (this.onreadystatechange) this.onreadystatechange({ target: this });
-            if (this.onerror) this.onerror({ target: this });
-        });
-    }
+function XMLHttpRequest() {
+    this.onload = null;
+    this.onerror = null;
+    this.onreadystatechange = null;
+    this.readyState = 0;
+    this.fetch = window.fetch;
+}
+XMLHttpRequest.prototype.open = function (method, url) {
+    this.readyState = 1;
+    this.method = method;
+    this.url = url;
+};
+XMLHttpRequest.prototype.send = function (data) {
+    var params = { method: this.method, headers: { referer: document.location.href.replace(/^#[\s\S]*$/g, '') } };
+    if (data != null) params.body = data;
+    this.fetch(this.url, params).then(function (d) {
+        this.fetched = d;
+        this.readyState = 4;
+        this.status = d.status;
+        return d.text();
+    }).then(function (d) {
+        this.responseText = d;
+        if (this.onreadystatechange) this.onreadystatechange({ target: this });
+        if (this.onload) this.onload({ target: this });
+    }, function (e) {
+        this.readyState = 4;
+        if (this.onreadystatechange) this.onreadystatechange({ target: this });
+        if (this.onerror) this.onerror({ target: this });
+    });
 }
