7bfb149ed00d2f5da6c4c8b00e5c6b62a2eeed72
不枝雀 <yunxu1019@qq.com>
2025-03-03 18:29:57 +0800

    启动时不再转换文案，更新到此版本时国际化文案要重新扫描一遍

--- coms/efront/国际化.js
+++ coms/efront/国际化.js
@@ -9,8 +9,6 @@ var loadData = async function (fullpath, i18nMap) {
         var keys = Object.keys(d);
         keys.forEach(k => {
             if (!d[k]) return;
-            d[k] = d[k].replace(/^\s*([\s\S]*?)([：\:]?\s*)$/, '$1');
-
             if (!i18nMap[k]) i18nMap[k] = Object.create(null);
         });
     }
