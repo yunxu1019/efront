837596c7889e350a47f09ef37e6dbd7979c5a17b
不枝雀 <yunxu1019@qq.com>
2025-03-13 04:49:17 +0800

    添加字段编辑的功能

--- coms/zimoli/design.js
+++ coms/zimoli/design.js
@@ -1,12 +1,35 @@
 var field_Id = 0;
 function main(fields, types) {
-    var page = form();
-    page.innerHTML = design;
-    var scope = render(page, {
+    var page = fields;
+    if (isElement(fields)) {
+        fields = [];
+        types = [
+            { name: '字符串', key: 'string' },
+            { name: '布尔值', key: 'boolean' },
+            { name: '列表', key: 'array' },
+            { name: '攻举', key: 'enum' },
+            { name: '对象', key: 'object' },
+            { name: '双精度数值', key: 'number' },
+            { name: "时间戳", key: "timestamp" }
+        ];
+    }
+    else {
+        page = form();
+    }
+    var avoid = [].concat(fields);
+    var defines = refilm`
+    /name/显示名 input ^${avoid}
+    */key/键名 input ^${avoid}
+    /type/选择类型 select ${types}
+    `;
+    var scope = {
         select,
         fields,
-        list,
+        field,
+        defines,
+        dlist: list,
         btn: button,
+        drop,
         padding,
         focus(a) {
             addClass(a, 'focus');
@@ -15,23 +38,37 @@ function main(fields, types) {
             removeClass(a, 'focus');
         },
         add() {
-            this.fields.push({
+            var field = {
                 id: ++field_Id
-            });
+            }
+            this.fields.push(field);
+            avoid.push(field);
             setTimeout(() => {
                 fieldsContainer.go(this.fields.length);
             })
         },
         remove(field) {
-            var fields = this.fields;
-            for (var cx = fields.length - 1; cx >= 0; cx--) {
-                if (fields[cx] === field) {
-                    fields.splice(cx, 1);
-                }
-            }
+            removeFromList(this.fields, field);
+            removeFromList(avoid, field);
         },
         types
-    }).$scope;
+    };
+    care(page, function (fields1) {
+        if (isArray(fields1[0])) {
+            [fields1, types] = fields1;
+        }
+        else {
+            fields1 = fields1;
+        }
+        field_Id = fields1.length;
+        fields = scope.fields = fields1;
+        avoid.splice(0, avoid.length, ...fields1);
+        defines[2].options = types;
+        console.log(page.avoid, 'avoid')
+    });
+    page.innerHTML = design;
+    render(page, scope);
+
     var [options, fieldsContainer] = page.children;
     // vbox(fieldsContainer);
     autodragchildren(fieldsContainer, fieldsContainer, function (src, dst, dst1) {
