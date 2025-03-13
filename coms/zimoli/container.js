64888aec3d2e9450f5f99cd3df8f7594396e3929
不枝雀 <yunxu1019@qq.com>
2025-03-13 22:50:44 +0800

    降低container的内存占用

--- coms/zimoli/container.js
+++ coms/zimoli/container.js
@@ -1,48 +1,46 @@
-function container(element) {
-    var change = lazy(async function (src) {
-        if (src !== this.$src) return;
-        if (!src) remove(this.with);
-        else if (isNode(src)) {
-            if (isElement(src) && this.$struct.copys) {
-                for (var c of this.$struct.copys) {
-                    if (c.name === 'class') {
-                        addClass(src, c.value);
-                    }
-                    else if (c.name === 'style') {
-                        css(src, c.value);
-                    }
-                    else src.setAttribute(c.name, c.value);
+var change = async function (src) {
+    if (src !== this.$src) return;
+    if (!src) remove(this.with);
+    else if (isNode(src)) {
+        if (isElement(src) && this.$struct.copys) {
+            for (var c of this.$struct.copys) {
+                if (c.name === 'class') {
+                    addClass(src, c.value);
                 }
+                else if (c.name === 'style') {
+                    css(src, c.value);
+                }
+                else src.setAttribute(c.name, c.value);
             }
-            zimoli.global(src, this);
         }
-        else if (isString(src) || +src < 0) {
-            watch(this, 'params', onparams);
-            onparams.call(this, this.params);
-        }
-        else {
-            remove(this.with);
-        }
-    });
-    var gosrc = function (src) {
-        if (!isHandled(this.$src) && !isHandled(src)) return;
-        if (src !== this.$src) {
-            this.$src = src;
-            change.call(this, src);
-        }
-    };
-    var onparams = lazy(function (params) {
-        zimoli.go(this.$src, params, this);
-    });
+        zimoli.global(src, this);
+    }
+    else if (isString(src) || +src < 0) {
+        watch(this, 'params', onparams);
+        onparams.call(this, this.params);
+    }
+};
+var gosrc = function (src) {
+    if (!isHandled(this.$src) && !isHandled(src)) return;
+    if (src !== this.$src) {
+        this.$src = src;
+        change.call(this, src);
+    }
+};
+var goone = function (src) {
+    if ("$src" in this) return;
+    gosrc.call(this, src);
+};
+var onparams = lazy(function (params) {
+    zimoli.go(this.$src, params, this);
+});
+function container(element) {
     var comment = document.createComment('container');
     comment.$struct = element.$struct;
     care(comment, gosrc);
     if (element.hasAttribute && element.hasAttribute('src')) {
         var src = element.getAttribute('src');
-        if (src) oncemount(comment, function () {
-            if ("$src" in this) return;
-            gosrc.call(this, src);
-        });
+        if (src) oncemount(comment, goone);
     }
     return comment;
 }
