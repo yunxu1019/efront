c11c3702c1891b3c0b3e7472d773a56162eaee49
不枝雀 <yunxu1019@qq.com>
2025-03-22 12:30:52 +0800

    添加一处非空判断

--- coms/pivot/checkGeo.js
+++ coms/pivot/checkGeo.js
@@ -4,5 +4,8 @@ return async function (a) {
     var res = await data.from("iplocation", { ip: m ? m[0] : ip });
     if (res.data) res = res.data;
     if (res.address) res = res.address;
+    else if (res.country || res.isp) {
+        res = [res.country, res.province, res.city, res.district, res.isp].join(' ');
+    }
     a.address = res;
 }
