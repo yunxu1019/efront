cc1d2148cf2ddeba34fbff76e38ca4c17d461264
不枝雀 <yunxu1019@qq.com>
2025-03-23 20:24:40 +0800

    添加百度和高德的ip定位接口，在ip.cn抓取失败时启用

--- coms/pivot/checkGeo.js
+++ coms/pivot/checkGeo.js
@@ -1,11 +1,38 @@
+var md5 = docs$md5;
+var baidu = function (ip) {
+    var query = {
+        ip,
+        ak: "lPiUBf6CCdZtLzrbzYREa6liU0NycJyr"
+    };
+    query.sn = md5(encodeURIComponent("/location/ip?" + serialize(query)) + "dZtaNhMkjlDmxAot0uYs2fEJKvTi3REe");
+    return data.from("ipbaidu", query);
+};
+var gaode = function (ip) {
+    var query = {
+        ip,
+        key: 'e360c7487575c81c617a42d00edea1fb'
+    };
+    query.sig = md5(serialize(query) + "a9b7a9509b47169bde38dfa825898ff8");
+    return data.from("ipgaode", query);
+};
 return async function (a) {
     var ip = a.ip || a.remote;
     var m = /(\d+\.){3}\d+$/.exec(ip);
-    var res = await data.from("iplocation", { ip: m ? m[0] : ip });
-    if (res.data) res = res.data;
-    if (res.address) res = res.address;
-    else if (res.country || res.isp) {
-        res = [res.country, res.province, res.city, res.district, res.isp].join(' ');
+    ip = m ? m[0] : ip;
+    var res = await data.from("ipcn", { ip });
+    console.log(res)
+    if (!res.address && !res.data) res = await baidu(ip);
+    var msg = '加载错误';
+    if (res.message && !res.data && !res.content && !res.address) {
+        msg = res.message;
+        res = await gaode(ip);
     }
-    a.address = res;
+    if (res.data) res = res.data;
+    if (res.content) res = res.content;
+    if (res.address) res = res.address;
+    else if (res.country || res.province || res.city || res.isp) {
+        res = [res.country, res.province, res.city, res.district, res.isp].join('');
+    }
+    if (!res) alert(msg, 'warn');
+    a.address = res || msg;
 }
