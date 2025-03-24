2c5d170b427d3eb4842531d806ce08b23206a94e
不枝雀 <yunxu1019@qq.com>
2025-03-24 11:42:01 +0800

    兼容0x80000000与0xffffffff之间的数字

--- coms/basic/writeLEB128.js
+++ coms/basic/writeLEB128.js
@@ -3,7 +3,7 @@ function writeLEB128(dist, offset, value) {
     while (b > 127) {
         var t = b & 0x7f;
         dist[offset++] = t | 0b10000000;
-        b = b >> 7;
+        b = b >>> 7;
     }
     dist[offset++] = b;
     return offset;
