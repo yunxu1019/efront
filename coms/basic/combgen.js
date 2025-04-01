424a70745a2e2b9b6c141f3bda2de87b763ad4b7
不枝雀 <yunxu1019@qq.com>
2025-04-02 00:46:54 +0800

    修复combgen

--- coms/basic/combgen.js
+++ coms/basic/combgen.js
@@ -1,12 +1,13 @@
 function* combgen() {
-    var [f, args] = Comb.geta(arguments);
+    var [f, args] = Comb.args(arguments);
     var arr = new Comb(f, args);
+    var i = 0;
     for (var cx = 0, dx = arr.length; cx < dx; cx++) {
         var res = yield arr.get(cx);
         if (res === false) {
             i++;
-            if (i > argsList.length) break;
-            var tx = argsList[argsList.length - i].length;
+            if (i > args.length) break;
+            var tx = args[args.length - i].length;
             cx += tx - cx % tx - 1;
         }
         else {
