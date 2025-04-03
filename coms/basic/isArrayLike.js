f301363a0c633744385257109e034396e5ca1ff3
不枝雀 <yunxu1019@qq.com>
2025-04-04 03:20:13 +0800

    修复isArrayLike

--- coms/basic/isArrayLike.js
+++ coms/basic/isArrayLike.js
@@ -1,6 +1,6 @@
 var window = this;
 function isArrayLike(a) {
-    if (a === null || a === window || typeof a !== 'object' || isNode(a)) return false;
+    if (a == null || typeof a !== 'object' || a.window === a || isNode(a)) return false;
     if (isFinite(a.length)) {
         if (a.length > 0) {
             return a.length - 1 in a;
