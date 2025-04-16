28f1d0c2bba8f71cf72e58059e9ff1f7c28d0e99
不枝雀 <yunxu1019@qq.com>
2025-04-16 11:46:57 +0800

    解决添加或删除行时代码错乱的问题

--- coms/茨菰/list.js
+++ coms/茨菰/list.js
@@ -1,34 +1,54 @@
 // 一个简化版的list，不支持回弹，且每行高度一致
 function list(elem) {
     var g = getGenerator(elem);
-    var s = [];
-    care(elem, function (src, old) {
-        s = src;
-        if (src !== old) remove(this.children);
-        if (isMounted(elem)) refresh();
+    var src = [];
+    var childrenMap = [];
+    var collectChildren = function (src, old) {
+        var children = Array.apply(null, elem.children);
+        childrenMap = [];
+        var inc = 0;
+        for (var cx = 0, dx = src.length; cx < dx; cx++) {
+            switch (old[cx]) {
+                case old[inc]:
+                    childrenMap[cx] = children[inc];
+                    delete children[inc];
+                    inc++;
+                    continue;
+                case old[inc + 1]:
+                    childrenMap[cx] = children[++inc];
+                    delete children[inc];
+                    inc++;
+                    continue;
+            }
+        }
+        remove(children.filter(a => !!a));
+        console.log(src)
+    }
+    care(elem, function (src1, old) {
+        src = src1;
+        if (src1 !== old) remove(elem.children);
+        else collectChildren(src, old);
+        if (isMounted(elem)) mount();
+        remove(childrenMap.filter(a => !!a));
     });
-    var refresh = function () {
+    var mount = function () {
         var fsize = parseFloat(getComputedStyle(elem).lineHeight);
         var { top, height } = getScreenPosition(elem);
         var sheight = screen.height;
         var i = 0;
-        var childrenMap = Object.create(null);
-        for (var c of elem.children) {
-            if (isFinite(c.index)) childrenMap[c.index] = c;
-        }
         if (top < -sheight) {
             i = (-top - sheight) / fsize | 0;
         }
+        elem.start = i;
         css(elem, { paddingTop: i * 1.36 + 'em' });
         top += i * fsize;
         sheight += sheight;
         var p = null;
         while (top < sheight) {
             var e = childrenMap[i];
-            if (!e) e = g(i, s[i]);
+            if (!e) e = g(i, src[i]);
             else delete childrenMap[i];
             if (!e) break;
-            e.index = i;
             top += fsize;
             i++;
             if (isMounted(e) && (!p || e.previousSibling === p));
@@ -36,8 +56,14 @@ function list(elem) {
             else appendChild(elem, e);
             p = e;
         }
-        for (var k in childrenMap) remove(childrenMap[k]);
+        elem.end = i;
     };
+    var refresh = function () {
+        childrenMap = Array.apply(null, elem.children);
+        mount();
+        remove(childrenMap.filter(a => !!a));
+        childrenMap = [];
+    }
     on("mounted")(elem, refresh);
     elem.refresh = refresh;
     return elem;
