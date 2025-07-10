5682997ecf90ccff21ef5b12b4ec7d228ff6c27a
不枝雀 <yunxu1019@qq.com>
2025-07-11 00:53:51 +0800

    编码时绕开空zip文件的标记

--- coms/pkware/decodePack_test.js
+++ coms/pkware/decodePack_test.js
@@ -1,2 +1,6 @@
 var buff = new Uint32Array([0x04034b50]);
 decodePack(Buffer.from(buff.buffer, buff.byteOffset, buff.byteLength))
+var buff = new Uint32Array([0x02014b50]);
+decodePack(Buffer.from(buff.buffer, buff.byteOffset, buff.byteLength))
+var buff = new Uint32Array([0x06054b50]);
+decodePack(Buffer.from(buff.buffer, buff.byteOffset, buff.byteLength))
