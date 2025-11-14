713b87870d6e85f4c03078972bf660f4d55618bd
不枝雀 <yunxu1019@qq.com>
2025-11-14 13:12:16 +0800

    区间增删工具

---
+++ coms/basic/SliceRange.js
@@ -0,0 +1,65 @@
+function checkRange(from, to) {
+    if (!isFinit(from) || !isFinit(to)) throw Error('边界异常！');
+    if (from > to) throw new Error(`左边界${from}大于右边界${to}！`);
+}
+function copyCaped(copyto, caped) {
+    copyto.length = caped.length;
+    for (var cx = 0, dx = copyto.length; cx < dx; cx++) {
+        copyto[cx] = caped[cx];
+    }
+}
+// 包含from不包含to，步进单位是1
+function SliceRange(from, to) {
+    console.log(from, to)
+    checkRange(from, to);
+    if (!this || this.constructor !== SliceRange) return new SliceRange(from, to);
+    this[0] = [from, to];
+    this.length = 1;
+}
+
+SliceRange.prototype.delete = function (from, to) {
+    checkRange(from, to);
+    var caped = [];
+    for (var cx = 0, dx = this.length; cx < dx; cx++) {
+        var [m, n] = this[cx];
+        delete this[cx];
+        if (m >= from && n <= to) continue;
+        if (m < from) {
+            caped.push([m, from]);
+        }
+        if (n > to) {
+            caped.push([to + 1, n]);
+        }
+    }
+    copyCaped(this, caped);
+};
+
+SliceRange.prototype.add = function (from, to) {
+    checkRange(from, to);
+    var caped = [];
+    var pushed = false;
+    for (var cx = 0, dx = this.length; cx < dx; cx++) {
+        var [m, n] = this[cx];
+        delete this[cx];
+        if (n < from) {
+            caped.push([m, n]);
+            continue;
+        }
+        if (m > to) {
+            if (!pushed) {
+                caped.push([from, to]);
+                pushed = true;
+            }
+            caped.push([m, n]);
+            continue;
+        }
+        if (m <= from) {
+            from = m;
+        }
+        if (n >= to) {
+            caped.push([from, to]);
+            pushed = true;
+        }
+    }
+    copyCaped(this, caped);
+}
