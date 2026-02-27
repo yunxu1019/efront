615df820482f157b36066230c1d1d5a003cf3b8b
不枝雀 <yunxu1019@qq.com>
2026-02-27 22:11:09 +0800

    防止Symbol转换成属性时变成相同的值

--- coms/basic_/Symbol.js
+++ coms/basic_/Symbol.js
@@ -1,24 +1,29 @@
 var Symbol = this.Symbol || function () {
+    var symbolid = 0;
     function Symbol(a) {
-        if (this instanceof Symbol) {
-            this.symbol = String(a);
+        this.id = ++symbolid;
+        this.name = String(a);
+    }
+    function Symbol1(a) {
+        if (this instanceof Symbol1) {
+            throw new Error('Symbol不是构造方法!');
         } else if (a instanceof Symbol) {
-            return a;
+            throw new Error('无法将Symbol类型转换成字符串!');
         } else {
             return new Symbol(a);
         }
     }
     var prototype = new Symbol;
     prototype.toString = function () {
-        return 'Symbol(' + this.symbol + ")";
+        return `Symbol(${this.name}/${this.id})`;
     };
     prototype.valueOf = function () {
         return this;
     };
 
     Symbol.prototype = prototype;
-    Symbol.iterator = Symbol('iterator');
-    Symbol.asyncIterator = Symbol('asyncIterator');
+    Symbol.iterator = Symbol1('iterator');
+    Symbol.asyncIterator = Symbol1('asyncIterator');
     var iterator = function () {
         var arr = this, cx = 0;
         return {
@@ -33,5 +38,5 @@ var Symbol = this.Symbol || function () {
     } catch {
         Array.prototype[Symbol.iterator] = iterator;
     }
-    return Symbol;
+    return Symbol1;
 }();
