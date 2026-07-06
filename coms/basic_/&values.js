162ab5ddb5e536a5287371903185defc35de09f7
不枝雀 <yunxu1019@qq.com>
2026-07-06 20:33:58 +0800

    条件写反了

--- coms/basic_/&values.js
+++ coms/basic_/&values.js
@@ -1,6 +1,6 @@
 var iterator = Symbol.iterator;
 return function values(o) {
-    if (iterator in o) throw new Error('对象不可玫举')
+    if (!o[iterator]) throw new Error('对象不可玫举')
     var iter = o[iterator]();
     var values = [];
     for (; ;) {
