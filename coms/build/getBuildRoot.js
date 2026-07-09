ee0dd201085588236f0192a5bd45a16ece2b2194
不枝雀 <yunxu1019@qq.com>
2026-07-10 03:21:50 +0800

    mac与windows系统混用代码时会有以`.`开头的文件混入编译流造成异常，后续将忽略这些文件

--- coms/build/getBuildRoot.js
+++ coms/build/getBuildRoot.js
@@ -252,6 +252,7 @@ var getBuildRoot = async function (files, matchFileOnly) {
                 names.forEach(function (name) {
                     if (name.isDirectory()) name = name.name + path.sep;
                     else name = name.name;
+                    if (/^\./.test(name)) return;
                     if (/^#/.test(name)) return;
                     files.push(path.join(file, name));
                 });
