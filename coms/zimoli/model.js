812ea912050406c215869e3e4936eb48c02fe08e
不枝雀 <yunxu1019@qq.com>
2026-07-17 17:52:34 +0800

    适配新版chrome

--- coms/zimoli/model.js
+++ coms/zimoli/model.js
@@ -13,17 +13,20 @@ unmark(checkbox);
 unmark(swap);
 unmark(image);
 unmark(success);
+var amp_encode = function (str) {
+    return `${String(str).replace(/["]/g, "&quot;")}`;
+};
 var renderOption = function (o, index) {
     if (typeof o === 'string') return o;
     if (isFunction(o)) {
         var name = typeof index === 'number' ? o.name : index;
-        return `<a href="javascript:;" @click=field.options[${strings.encode(index)}](data)>${name}</a>`
+        return `<a href="javascript:;" @click="field.options[&quot;${amp_encode(index)}&quot;](data)">${name}</a>`
     }
     if (isObject(o)) {
         if (o.href) {
-            return `<a href=${strings.encode(o.href)}>${o.name}</a>`;
+            return `<a href="${amp_encode(o.href)}">${o.name}</a>`;
         }
-        return `<a href='javascript:;' @click=field.options[${strings.encode(index)}](data)>${o.name}</a>`
+        return `<a href='javascript:;' @click="field.options[&quot;${amp_encode(index)}&quot;](data)">${o.name}</a>`
     }
     return o;
 }
